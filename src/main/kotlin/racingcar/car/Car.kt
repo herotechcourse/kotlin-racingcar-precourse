3d2f7ac295703d2bd64d2b2abb333784e3967332
Junyou Park <pjy008008@gmail.com>
2025-04-16 07:16:36 +0900

    feat(car): add drive method, getPosition method in Car class
    Added new functions to Car
    - drive() function:
      - Generates a random integer between 0 and 9
      - Increments the 'position' of the car by 1 if the generated random number is greater than 3
    - getPosition() function:
      - Returns the current 'position' of the car as an 'Int'

--- src/main/kotlin/racingcar/car/Car.kt
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -1,8 +1,19 @@
 package racingcar.car
 
+import kotlin.random.Random
+
 class Car(val carName: String) {
     init {
         require(carName.length < 5)
     }
-    //drive code
+    private var position: Int = 0
+    fun drive() {
+        val randomNumber = Random.nextInt(10)
+        if (randomNumber > 3) {
+            position++
+        }
+    }
+    fun getPosition(): Int{
+        return position
+    }
 }
