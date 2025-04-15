5f2c257c097954accb8c02a3510b8c89fdb613a7
Fabio Niglio <fabio_niglio@hotmail.com>
2025-04-15 19:04:05 +0200

    feat: implement Cars class to manage movement and determine winners

---
+++ src/main/kotlin/racingcar/Cars.kt
@@ -0,0 +1,32 @@
+package racingcar
+
+class Cars(private val cars: List<Car>) {
+
+    fun moveAll() {
+        cars.forEach { car ->
+            val number = camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9)
+            car.move(number >= 4)
+        }
+    }
+
+    fun printRound() {
+        cars.forEach { car ->
+            println(car.printProgress())
+        }
+        println()
+    }
+
+    fun getWinners(): List<Car> {
+        val maxPosition = cars.maxOf { it.position }
+        return cars.filter { it.position == maxPosition }
+    }
+
+    companion object {
+        fun fromNames(input: String): Cars {
+            val carList = input.split(",")
+                .map { it.trim() }
+                .map { Car(it) }
+            return Cars(carList)
+        }
+    }
+}
