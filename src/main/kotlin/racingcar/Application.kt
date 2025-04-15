9b5523c93709ae49daa3a94ef82a2a9af8483067
Ugochi <amakomitchell5@gmail.com>
2025-04-16 00:24:27 +0200

    create function to getCarNames

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,4 +2,22 @@ package racingcar
 
 fun main() {
     // TODO: Implement the program
+
+}
+
+fun getCarNames(): List<String> {
+    var isFinished = false;
+
+    do {
+        println("Enter car names, must be separated by commas!:")
+        val carNamesInput = readLine()?.trim() ?: continue
+        val carNames = carNamesInput.split(",").map { it.trim() }
+
+        if(carNames.all { it.isNotEmpty() && it.length <= 5 }) {
+            return carNames
+        } else {
+            throw IllegalArgumentException()
+        }
+
+    } while (!isFinished)
 }
