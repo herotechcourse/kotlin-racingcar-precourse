3f4638081a272afb95cd9e0f75cd67ad53871170
LeeJaeHyeok97 <hazardous10@naver.com>
2025-04-16 01:41:01 +0900

    feat(Games): add game start message and implement input for players and rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,33 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+class Games(var challengers: String, var rounds: String) {
+
+
+}
+
 fun main() {
-    // TODO: Implement the program
+    // racing start introducing comment
+    println("Enter the names of the cars (comma-separated):")
+    val challengers = Console.readLine()
+    println("How many rounds will be played?")
+    val rounds = Console.readLine()
+
+    println()
+    println("Race Results")
+
+    val challengerList = challengers.split(",") // [pobi, woni, jun]
+
+//    Games()
+    var currentRound = 0
+    while (currentRound != rounds.toInt()) {
+        for (i in 1..challengerList.size) {
+            val diceNumber = Randoms.pickNumberInRange(0, 9)
+            if (diceNumber >= 4) {
+                // todo 전진한 count 저장 필요
+            }
+        }
+    }
 }
