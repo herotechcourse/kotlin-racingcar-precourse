26a8012ca490855708112c4793f24f3ae0668978
Fabio Niglio <fabio_niglio@hotmail.com>
2025-04-15 19:10:07 +0200

    feat: implement main application logic to run the race

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,7 +1,22 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    try {
+        val carNames = InputHandler.readCarNames()
+        val numberOfRounds = InputHandler.readNumberOfRounds()
 
-    //First commit
+        val cars = Cars.fromNames(carNames)
+
+        OutputView.printRaceStart()
+        repeat(numberOfRounds) {
+            cars.moveAll()
+            OutputView.printRound(cars)
+        }
+
+        val winners = cars.getWinners()
+        OutputView.printWinners(winners)
+
+    } catch (e: IllegalArgumentException) {
+        println("Error: ${e.message}")
+    }
 }
