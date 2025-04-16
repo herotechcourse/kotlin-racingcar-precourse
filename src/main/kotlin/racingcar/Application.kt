9d093ea6ad32e2cb7d8333d539ccc41b407d60d0
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-16 22:22:16 +0200

    feat(main): implement game entry point

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import racingcar.Utils.parseCars
+import racingcar.Utils.printResults
+import racingcar.Utils.printWinners
+
 fun main() {
-    // TODO: Implement the program
+
+  print("Enter car names (comma-separated): ")
+  val carsString = Console.readLine()
+  print("How many rounds will be played?\n")
+  //TODO  make sure rounds is int
+  val rounds = Console.readLine().toInt()
+
+  val parsedCars = parseCars(carsString)
+  val game = Game(parsedCars, rounds)
+  game.startRace()
+
+  printResults(parsedCars)
+  printWinners(parsedCars)
+
+
 }
