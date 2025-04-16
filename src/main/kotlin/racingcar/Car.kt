49d163c8b557697ec803768050d15d0637cc575c
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-16 12:08:08 +0200

    feat(car): create Car data class with movement and progress methods

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,3 +1,10 @@
 package racingcar
 
-data class Car()
+data class Car(val name: String, var currLevel: Int=0) {
+  fun move() {
+    this.currLevel++;
+  }
+  fun getProgress(): String {
+    return "-".repeat(currLevel)
+  }
+}
