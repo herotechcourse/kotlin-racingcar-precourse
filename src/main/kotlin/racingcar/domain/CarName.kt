790ab54f8b9a440f39c0d9705dadfc71fb9b71c7
권장순 <jsoonworld@gmail.com>
2025-04-16 20:40:28 +0900

    refactor(CarName) : extract error message into constant ERROR_EXCEED_LENGTH

--- src/main/kotlin/racingcar/domain/CarName.kt
+++ src/main/kotlin/racingcar/domain/CarName.kt
@@ -5,8 +5,10 @@ class CarName private constructor(private val value: String) {
     fun value(): String = value
 
     companion object {
+        const val ERROR_EXCEED_LENGTH = "[ERROR] Car Name cannot exceed 5 characters."
+
         fun from(value: String): CarName {
-            require(value.length <= 5) {"[ERROR] Car Name cannot exceed 5 characters."}
+            require(value.length <= 5) { ERROR_EXCEED_LENGTH }
             return CarName(value)
         }
     }
