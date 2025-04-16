b8c1551299b651785e1f3fea5223af3d63fb68fa
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-16 15:48:03 +0200

    test: retrieve original test code

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -9,37 +9,30 @@ import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
-    fun `test for input`() {
+    fun `feature test`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni", "1")
+                assertThat(output()).contains("pobi : -", "woni : ", "Winners : pobi")
+            },
+            MOVING_FORWARD,
+            STOP,
+        )
+    }
+
+    @Test
+    fun `exception test`() {
         assertSimpleTest {
-            run("pobi,woni", "1")
-            assertThat(output()).contains("1")
+            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
         }
     }
-    // @Test
-    // fun `feature test`() {
-    //     assertRandomNumberInRangeTest(
-    //         {
-    //             run("pobi,woni", "1")
-    //             assertThat(output()).contains("pobi : -", "woni : ", "Winners : pobi")
-    //         },
-    //         MOVING_FORWARD,
-    //         STOP,
-    //     )
-    // }
-
-    // @Test
-    // fun `exception test`() {
-    //     assertSimpleTest {
-    //         assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
-    //     }
-    // }
 
     override fun runMain() {
         main()
     }
 
-    // companion object {
-    //     private const val MOVING_FORWARD: Int = 4
-    //     private const val STOP: Int = 3
-    // }
+    companion object {
+        private const val MOVING_FORWARD: Int = 4
+        private const val STOP: Int = 3
+    }
 }
