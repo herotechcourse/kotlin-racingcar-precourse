6510d2636cf999f5caff28fbc2e2dd57b3a5b17d
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-16 12:29:59 +0200

    feat: add user input for car names and race rounds
    Implement functionality to accept user input for:
    - Multiple car names via console input
    - Number of race rounds via console input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println(
+        buildString {
+            append("Enter the names of the cars. Make sure to:\n- Separate them by a comma and no spaces.")
+            append("\n- Use less than five characters per car name.")
+        },)
+    val carNames = Console.readLine()
+    // To do: Call a function that validates the input.
+    // To do: Call the function to populate an array of strings containing the car names.
+    println("Enter the number of rounds to race:")
+    val numberOfRounds = Console.readLine()
 }
+
