4b2263a516238fa5941c5a03108d88cb254fefb6
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-16 12:05:47 +0200

    feat: add move and canMove methods to Car for random movement

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,7 +1,20 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(val name: String) {
 
     var position = 0
+
+    fun move() {
+        if (canMove())
+        position++
+    }
+
+    private fun canMove(): Boolean {
+        val num = Randoms.pickNumberInRange(0, 9)
+        return num >= 4
+    }
+
 
 }
