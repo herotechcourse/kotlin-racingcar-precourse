e264b5d1193c33cadd7771fdac1bd445a7f02340
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2025-04-16 21:25:58 +0200

    feat(racingcar): add car names read functionality
    Added new function to read comma seperated car names. And raise
    IllegalArgumentException if the car name exceeds limit of 5 characters

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    val carNames = readCarName()
+
+}
+
+fun readCarName(): List<String>{
+    print("Enter the names of the cars (comma-separated): ")
+    val input = Console.readLine();
+    val carNames = input.split(",")
+    val invalidNames = carNames.filter { name -> name.length > 5 }
+            if (invalidNames.isNotEmpty()) {
+            throw IllegalArgumentException("Car name(s) '${invalidNames.joinToString(", ")}' exceed 5 character limit.")
+        }
+
+
+    return carNames
 }
