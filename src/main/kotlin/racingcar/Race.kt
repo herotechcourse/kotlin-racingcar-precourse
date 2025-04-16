a27f3cebd9529f4d436778dbd2a5c414670f02c8
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-16 23:00:37 +0200

    feat(Race): add Race with setters and methods

---
+++ src/main/kotlin/racingcar/Race.kt
@@ -0,0 +1,52 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Race {
+    var carNames: List<String> = emptyList()
+        private set
+    var rounds: Int = 0
+        private set
+    var cars = mutableListOf<Car>()
+        private set
+
+    fun setCarNames(carNames: List<String>) {
+        this.carNames = carNames
+    }
+
+    fun setRounds(rounds: Int) {
+        this.rounds = rounds
+    }
+
+    fun initializeCars() {
+        for (carName in carNames) {
+            val car = Car()
+            car.setCarName(carName)
+            cars.add(car)
+        }
+    }
+
+    fun advanceRounds() {
+        for (car in cars) {
+            if (Randoms.pickNumberInRange(0, 9) >= 4) {
+                car.increaseProgress()
+            }
+
+            var progressBar = ""
+            for (i in 1..car.progress) {
+                progressBar += "-"
+            }
+
+            println(car.name + " : " + progressBar)
+        }
+        println()
+        rounds--
+    }
+
+    fun announceWinners() {
+        val maxProgress = cars.maxOf { it.progress }
+        val winners: String = cars.filter { it.progress == maxProgress }
+                                    .map { it.name }.joinToString(", ")
+        println("Winners : $winners")
+    }
+}
