79f5540e5fb6d6a7fd3159648b4dcc2cfe1dd8ea
Alex <alexis.mar.schmidt@gmail.com>
2025-04-16 11:56:42 +0200

    feat(validation): validate car names and number of rounds input
    Added validation to ensure the car names and numbers are correct.
    - Car names: ensures names are comma-separated, not empty, unique,
    and no longer than 5 characters.
    - Rounds: ensures the input is a valid integer.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,7 @@ import racingcar.view.InputView
 
 fun main() {
     val carNames = InputView.readNames()
-    val rounds =InputView.readRounds()
+    val rounds =InputView.readRoundNumber()
     println("Car names: $carNames")
     println("Number of rounds: $rounds")
 }
--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.util.Validator
 
 object InputView {
 
@@ -8,12 +9,13 @@ object InputView {
         println("Enter the names of the cars (comma-separated):")
         val input = Console.readLine()
         val names = input.split(",").map { it.trim() }
+        Validator.validateNames(names)
         return names
     }
 
-    fun readRounds(): Int{
+    fun readRoundNumber(): Int{
         println("How many rounds will be played?")
         val input = Console.readLine()
-        return input.toInt()
+        return Validator.validateRoundNumber(input)
     }
 }
