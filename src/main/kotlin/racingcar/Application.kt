36117142ada5fade971174d635a48140354b2e81
HyeonminKim <61939519+kimpeter2018@users.noreply.github.com>
2025-04-16 15:17:01 +0200

    feat: add simulateRace() and determineWinners() functions, call functions in the main()
    simulateRace(): call move method and print the progress for the number of given rounds
    determineWinners(): among the cars determine which has the most progress and return all that has the max amount through filter

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -15,6 +15,19 @@ class Car(val name: String, var progress: Int = 0) {
     }
 }
 
+fun simulateRace(Cars: List<Car>, rounds: Int) {
+    for (i in 0 until rounds) {
+        Cars.forEach { it.move() }
+        Cars.forEach { println("${it.name} : ${it.getProgress()}") }
+        println()
+    }
+}
+
+fun determineWinners(cars: List<Car>): List<Car> {
+    val maxProgress = cars.maxOf { it.progress }
+    return cars.filter { it.progress == maxProgress }
+}
+
 fun main() {
     // receive car names from user
     println("Enter the names of the cars (comma-separated):")
@@ -24,8 +37,17 @@ fun main() {
     // receive round count from user
     val roundCount = Console.readLine().toInt()
     println("How many rounds will be played?")
-
+    
+    // generate Cars
     val cars = carNamesList.map { Car(it) }
 
+    // start race
+    println("Race Results")
+    simulateRace(cars, roundCount)
+
+    // determine and declare winners
+    println("Winner(s):")
+    val winners = determineWinners(cars)
+    println(winners.joinToString(", ") { it.name }) 
 
 }
