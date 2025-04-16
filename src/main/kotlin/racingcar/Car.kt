47676308cea11be51255f4275013a276f7325d32
hyungjunn <hotfirebulldog@gmail.com>
2025-04-16 16:37:10 +0900

    add random number feature in move function

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,5 +1,7 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(name: String) {
     init {
         fun isValidNameLength(nameLength : Int) : Boolean = nameLength <= 5
@@ -11,8 +13,10 @@ class Car(name: String) {
 
     private var position = 0
 
-    fun move() {
-        position++
+    fun move(randomNumber: Int = Randoms.pickNumberInRange(0, 9)) {
+        if (randomNumber >= 4) {
+          position++
+        }
     }
 
     fun position() : Int = position
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -3,6 +3,8 @@ package racingcar
 import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
     @Test
@@ -13,11 +15,11 @@ class CarTest {
             .hasMessage("Car name cannot exceed 5 characters, current name length: ${name.length}")
     }
 
-    @Test
-    fun `car can move forward`() {
+    @ParameterizedTest
+    @ValueSource(ints = [4, 9])
+    fun `car can move forward`(randomNumber: Int) {
         val car  = Car("hero")
-        car.move()
+        car.move(randomNumber)
         assertThat(car.position()).isEqualTo(1)
     }
-
 }
