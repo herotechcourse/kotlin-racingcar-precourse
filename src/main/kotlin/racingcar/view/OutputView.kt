9773bdbfea7aba97dab8d7398175b5e7aa8d1980
권장순 <jsoonworld@gmail.com>
2025-04-16 18:34:59 +0900

    feat(OutputView) : Add prompt message for round input

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -6,7 +6,12 @@ class OutputView {
         println(INTRO_INPUT_CARNAMES_MESSAGE)
     }
 
+    fun printAskPlayRoundPrompt() {
+        println(ASK_PLAY_ROUND_PROMPT)
+    }
+
     companion object {
         private val INTRO_INPUT_CARNAMES_MESSAGE = "Enter the names of the cars (comma-separated):"
+        private val ASK_PLAY_ROUND_PROMPT = "How many rounds will be played?"
     }
 }
