bdb7d17e68d8ffdbaca587f51c27a2fe6d38301c
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-16 10:16:28 +0200

    feat: validate and parse car name strings from user input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,7 +3,20 @@ package racingcar
 fun main() {
 
     val carNamesInput = readCarNamesInput()
+    val carsNames = parseCarNamesInput(carNamesInput)
 
+}
+
+fun parseCarNamesInput(carNamesInput: String): List<String> {
+    val names = carNamesInput.split(",").map { it.trim() }
+
+    if (names.isEmpty()) throw IllegalArgumentException("At least one car name must be provided")
+
+    names.forEach{ name ->
+        if (name.isEmpty()) throw IllegalArgumentException("Cars name cannot be empty")
+        if (name.length > 5) throw IllegalArgumentException("Car names cannot exceed 5 characters. Car '$name' has ${name.length} characters")
+    }
+    return names
 }
 
 fun readCarNamesInput(): String {
