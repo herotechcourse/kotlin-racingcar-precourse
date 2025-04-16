d111e4c9d991ef7aa95a0ed7096d3b5370ab7a4b
Taekeundo <pleasepose@gmail.com>
2025-04-16 18:44:55 +0200

    test: Add failing test for car-name validation

---
+++ src/test/kotlin/racingcar/validation/ValidateCarNamesTest.kt
@@ -0,0 +1,11 @@
+fun validateCarNames(input: String): Boolean {
+    return true
+}
+
+fun main () {
+    val car1 = "pobi"
+    if (validateCarNames(car1))
+        println("OK")
+    else
+        println("KO")
+}
