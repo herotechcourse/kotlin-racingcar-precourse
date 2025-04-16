f5ccb2398668cc1b175245f00fe0c4a3d1be44d8
Atash1986 <msc.ashourzadeh@gmail.com>
2025-04-16 22:18:20 +0000

    fix(input): replace standard readLines with custom readLines

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,27 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     println("Enter the names of the cars (comma-separated):")
-    val carsStr = readLine() ?: ""
+    val carsStr = Console.readLine() ?: ""
     val carList = carsStr.split(',').map { it.trim() }
-    val cars = carList.map { carName -> 
-        if (Car.isValidName(carName)) {
-            Car(carName)
-        } else {
-            throw IllegalArgumentException()
+    val cars =
+        carList.map { carName ->
+            if (Car.isValidName(carName)) {
+                Car(carName)
+            } else {
+                throw IllegalArgumentException()
+            }
         }
-    }
 
     println("How many rounds will be played?")
-    val roundStr=readLine()
-    val rounds=roundStr?.toIntOrNull()
-    if(rounds==null)
-    {
+    val roundStr = Console.readLine()
+    val rounds = roundStr?.toIntOrNull()
+    if (rounds == null) {
         throw IllegalArgumentException()
     }
-    
-    val race=Race(rounds,cars)
+
+    val race = Race(rounds, cars)
     race.run()
     race.displayWinners()
-
 }
