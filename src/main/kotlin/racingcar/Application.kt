664942053138c867e9782413934eb15cfbdad999
Shilpa Pullayikodi <shilpapullayikodi@gmail.com>
2025-04-16 22:25:24 +0200

    feat(racingcar): add car movement logic
    Added a new function to simulate car movements. Generated a random
    number and marked movement based on the input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,12 +2,40 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 
+// add a constant for forward movement and stay still
+const val FORWARD = "Forward"
+const val STAY = "Stay"
+
+// add a model for the car to store the name and position
+data class Car(val name: String, var position: Int = 0)
+
 fun main() {
-    val carNames = readCarNames()
+    val carList = readCars()
     val numberOfRounds =  readNumberOfRounds()
+
+    repeat(numberOfRounds) {
+        moveCars(carList)
+    }
 }
 
-fun readCarNames(): List<String>{
+fun moveCars(carList: List<Car>) {
+    carList.forEach { car ->
+        if (randomCarMovement() == FORWARD) {
+            car.position++
+        }
+    }
+}
+
+fun randomCarMovement(): String{
+    val randomNumber = (0..9).random()
+    return if (randomNumber >= 4) {
+        FORWARD
+    } else {
+        STAY
+    }
+}
+
+fun readCars(): List<Car>{
     print("Enter the names of the cars (comma-separated): ")
     val input = Console.readLine();
     val carNames = input.split(",")
@@ -16,8 +44,15 @@ fun readCarNames(): List<String>{
             throw IllegalArgumentException("Car name(s) '${invalidNames.joinToString(", ")}' exceed 5 character limit.")
         }
 
+    val carList =  carNames.map { name ->
+        if (name.isBlank()) {
+            throw IllegalArgumentException("Car name cannot be empty.")
+        }
 
-    return carNames
+        Car(name.trim())
+    }
+
+    return carList
 }
 
 fun readNumberOfRounds(): Int{
