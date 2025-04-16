4476f800cb00d3b097a53d098ac418cc67cdec0c
hongseokpyo <hongseokpyou@gmail.com>
2025-04-16 22:59:00 +0900

    feat(input): receive car names and lap count from user

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
 package racingcar
 
-fun main() {
-    // TODO: Implement the program
+object Application {
+    @JvmStatic
+    fun main(args: Array<String>) {
+        print("Enter the names of the cars (comma-separated):\n")
+        val players = readlnOrNull() ?: ""
+        val names = players.split(",").map {it.trim()}
+
+        print("How many rounds will be played?\n")
+        val rounds = readlnOrNull() ?: ""
+        val laps = rounds.toInt()
+    }
 }
+
+
