cf1d2459d213bbe0e73d3fe2212af79d86a7b4b2
moonseonghui <msh9980@naver.com>
2025-04-17 00:38:22 +0900

    feat(car): add Car class with name and position properties

---
+++ src/main/kotlin/racingcar/car/Car.kt
@@ -0,0 +1,10 @@
+package racingcar.car
+
+class Car(val name: String) {
+    var position: Int = 0
+        private set
+
+    fun moveForward() {
+        position++
+    }
+}
--- src/main/kotlin/racingcar/io/OutputPrinter.kt
+++ src/main/kotlin/racingcar/io/OutputPrinter.kt
@@ -1,2 +1,16 @@
 package racingcar.io
 
+object OutputPrinter {
+    fun printCarPosition(carName: String, position: Int) {
+        val movement = "-".repeat(position)
+        println("$carName : $movement")
+    }
+
+    fun printWinners(winners: List<String>) {
+        println("Winners : ${winners.joinToString(", ")}")
+    }
+
+    fun printRoundSeparator() {
+        println()
+    }
+}
