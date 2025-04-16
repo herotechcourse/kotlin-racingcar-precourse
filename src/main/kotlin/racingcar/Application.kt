cf9dbe8f5997980ebc8da23ad218cfed8ad56293
vketteni <vketteni@gmail.com>
2025-04-16 13:52:09 +0000

    feat(user input): prompt user to enter car names and number of rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,34 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars (comma-separated):")
+    val carNamesInput = Console.readLine();
+    println("How many rounds will be played?")
+    val raceRoundsInput = Console.readLine();
+
+    val carNames = carNamesInput
+        .split(",")
+        .map { it.trim() };
+    val raceRounds = raceRoundsInput.toInt();
+    runRace(carNames, raceRounds);
+}
+
+fun runRace(carNames: List<String>, raceRounds: Int) {
+    val announcer1 = """
+        PREPARING ANOTHER EPIC CAR RACE
+        THE RACE WILL TAKE
+         => $raceRounds ROUND(S)      
+        TODAY'S CARS ARE
+    """.trimIndent()
+    val announcer2 = """
+        GET READY!!
+        AND..
+        GOOOO##        
+    """.trimIndent()
+
+    println(announcer1);
+    for (car in carNames) println(" => $car");
+    println(announcer2);
 }
