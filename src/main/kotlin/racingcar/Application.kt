6e09934125299a5a854ce5bb23ff36b309fdf232
Santiago Tena Hernandez <stenahe@gmail.com>
2025-04-16 23:01:12 +0200

    feat(logic): add overarching logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    // UserInput
+    val userInput = UserInput()
+    println("Enter the names of the cars (comma-separated):")
+    val carNames = Console.readLine()
+        userInput.setCarNamesFromInput(carNames)
+    println("How many rounds will be played?")
+    val rounds = Console.readLine()
+        userInput.setRoundsFromInput(rounds)
+    println()
+
+    //Race
+    println("Race Results")
+    val race = Race()
+    race.setCarNames(userInput.carNames)
+    race.setRounds(userInput.rounds)
+    race.initializeCars()
+
+    while (race.rounds > 0) {
+        race.advanceRounds()
+    }
+
+    race.announceWinners()
 }
