d7d46dcfb842d055766bf7032ae543974b9a832e
wjunseok <wminsoo1@naver.com>
2025-04-16 16:26:57 +0900

    feat(car): add name length validation
    - Car names can be up to 5 characters
    - Throws IllegalArgumentException if the name exceeds MAX_NAME_LENGTH

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,4 +4,14 @@ class Car(private val name: String) {
 
     private var position = 0
 
+    companion object {
+        const val MAX_NAME_LENGTH = 5
+    }
+
+    init{
+        if(name.length > MAX_NAME_LENGTH) {
+            throw IllegalArgumentException("Car name must have at least 5 characters")
+        }
+    }
+
 }
