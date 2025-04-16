f5b6d70d3b0b69422ddab609c78e49e79310be46
Taekeundo <pleasepose@gmail.com>
2025-04-16 15:26:03 +0200

    test: Add input-request test function
    TDD: test structure to support input request flow for both two inputs(carNames and rounds)

---
+++ src/test/kotlin/racingcar/input/InputRequestTest.kt
@@ -0,0 +1,12 @@
+fun requestCarNames() {
+    println("Enter the names of the cars (comma-separated):")
+}
+
+fun requestRounds() {
+    println("How many rounds will be played?")
+}
+
+fun main() {
+    requestCarNames()
+    requestRounds()
+}
