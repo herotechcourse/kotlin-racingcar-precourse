79d2a82b05de4f6e4ca9c2c2ab28a23f577ae438
Irene Rivero <irene.rivero.casal@gmail.com>
2025-04-16 17:55:00 +0200

    feat: create Application class entry point

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,15 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
-    val name = "car"
-    val gameType = "racing"
-    println("This is a $name $gameType game")
-    println("----------")
+    Application().run()
+}
+
+class Application {
+    fun run() {
+        // This will be implemented later
+        val name = "car"
+        val gameType = "racing"
+        println("This is a $name $gameType game")
+        println("----------")
+    }
 }
