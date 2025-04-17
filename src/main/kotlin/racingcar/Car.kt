1fc791b913c7bc0235590da1451fb5469e0ad92b
Urbano Jr <urbanobazz@gmail.com>
2025-04-17 20:51:10 +0200

    fix($Car): fix printing format in toString()

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -8,6 +8,6 @@ data class Car(val name: String, var position: Int = 0) {
     }
 
     override fun toString(): String {
-        return "$name: ${"-".repeat(position)}"
+        return "$name : ${"-".repeat(position)}"
     }
 }
