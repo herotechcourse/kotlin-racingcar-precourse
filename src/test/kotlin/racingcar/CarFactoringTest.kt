8461b5e6e796439a54c28d4ca87a0b71b610f233
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-17 11:41:10 +0200

    test(factory): add unit test for CarFactoring.createCars
    Verified that CarFactoring correctly creates a list of Car instances
    with the expected names using AssertJ.

---
+++ src/test/kotlin/racingcar/CarFactoringTest.kt
@@ -0,0 +1,16 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class CarFactoringTest {
+
+     @Test
+     fun testCreateCars() {
+         val carList = CarFactoring.createCars(
+             listOf("pobi", "woni", "jun"))
+         assertThat(carList.map { it.name })
+             .containsExactly("pobi", "woni", "jun")
+     }
+ }
