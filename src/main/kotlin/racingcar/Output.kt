b74f68bc60a31bd9fee9824aba6f50357bb11659
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-04-18 02:27:42 +0900

    feat(racingcar): implement Output object

---
+++ src/main/kotlin/racingcar/Output.kt
@@ -0,0 +1,13 @@
+package racingcar
+
+object Output {
+    fun printCarNameInputMessage() = println("Enter the names of the cars (comma-separated):")
+
+    fun printRoundInputMessage() = println("How many rounds will be played?")
+
+    fun printRaceStartMessage() = println("Race Results")
+
+    fun printProgress(cars: List<Car>) = println(cars.joinToString("\n") + "\n")
+
+    fun printWinners(winners: List<Car>) = println("Winners : ${winners.joinToString(", ") { it.name }}")
+}
