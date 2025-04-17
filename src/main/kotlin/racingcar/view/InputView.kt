bc183ebf85423e86bdf72c1dd50342f204051574
권장순 <jsoonworld@gmail.com>
2025-04-17 18:23:57 +0900

    refactor(InputView) : convert class to interface for improved testability

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,13 +1,6 @@
 package racingcar.view
 
-import camp.nextstep.edu.missionutils.Console
-
-class InputView {
-    fun readCarNames(): String {
-        return Console.readLine()
-    }
-
-    fun readNumberOfPlayRound(): String {
-        return Console.readLine()
-    }
+interface InputView {
+    fun readCarNames(): String
+    fun readNumberOfPlayRound(): String
 }
