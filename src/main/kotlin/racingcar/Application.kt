372fda465a0a64eadc824c6965f8fc6e1e912561
ShrutiK23 <shruti.kulkarni@st.ovgu.de>
2025-04-17 09:46:52 +0200

    feat(game): car race logic implemented
    - Cars move forward during each round if the randomly generated number between 0 and 9 is 4 or greater. This adds randomness.
    - The race results for each round is displayed by showing the car name and if it has moved forward or not.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,21 +1,64 @@
 package racingcar
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main()
 {
     //Read car names and number of rounds from user
     println("Enter the names of the cars (comma-separated):")
     val carNames = readLine()
-    SeparateCarNames(carNames)
+    var names = SeparateCarNames(carNames)
 
     println("How many rounds will be played?")
     val rounds = Console.readLine()
     println("Number of rounds: $rounds")
 
+    //Car racing logic
+    racing_func(names,rounds)
+}
+
+//This function is where the car racing is programmed
+fun racing_func(names:List<String>,rounds:String)
+{
+    val size = names.size //size = number of cars
+    // carPos shows the car name and its position at each round.
+    var carPos = mutableMapOf<String, String>()
+    for(i in 0 until size){
+        carPos[names[i]] = ""
+    }
+    println("Race Results")
+    for (i in 0 until rounds.toInt()) {
+        carPos = Calculate_Car_Position(carPos,size,names)
+        //Print each round results
+        PrintRoundResults(carPos)
+        println()
+    }
+}
+
+//This function prints every round results
+fun PrintRoundResults(carPos:MutableMap<String, String>) {
+    for (key in carPos.keys) {
+        println(key + " : " + carPos[key])
+    }
+}
+
+//Position is calculated as :
+//     A random number for each car is generated. If the random number
+//     is greater that 4, that car moves one step forward. So, in the
+//     position for that car, a '-' id appended.
+fun Calculate_Car_Position(carPos: MutableMap<String, String>, size:Int,names:List<String>) : MutableMap<String, String>{
+    for(j in 0 until size) {
+        val no = Randoms.pickNumberInRange(0, 9)
+        if (no >= 4) {
+            carPos[names[j]] = carPos[names[j]] + '-' //append a '-' if number is greater than or equal to 4
+        }
+    }
+    return carPos
 }
 
 //Separate names into list of strings based on "," as delimiter
-fun SeparateCarNames(carNames:String?){
+fun SeparateCarNames(carNames:String?) : List<String>
+{
     val names = carNames!!.split(",")
-    println("Car names are: $names")
+    return names
 }
