ff8ea3a0d97ccb49561b1f10020fe97037a70aa2
Taekeundo <pleasepose@gmail.com>
2025-04-17 12:06:08 +0200

    feat: Implement inValidCarNames() function

---
+++ src/main/kotlin/racingcar/validation/ValidateCarNames.kt
@@ -0,0 +1,24 @@
+package racingcar.validation
+
+/**
+ *  [ Condition ]
+ *  1. Not exceed 5 characters.     // "abcdef"
+ *  2. Not null or empty (= blank)  // "" or " "
+ *  3. Comma-separated format.      // "pobi,woni,jun"
+ */
+fun isNotExceed5Chars(input: String): Boolean {
+    return input.length <= 5
+}
+
+fun isNotBlankInput(input: String): Boolean {
+    return input.isNotBlank()
+}
+
+fun isCommaSeparated(input: String): Boolean {
+    val carNames = input.split(",")
+    return carNames.all { isNotBlankInput(it) && isNotExceed5Chars(it) }
+}
+
+fun isValidCarNames(input: String): Boolean {
+    return isCommaSeparated(input) && isNotBlankInput(input)
+}
