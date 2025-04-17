a2b4501b9c6f0cdc86b4e06844c0c152aabf4358
Jack <gengar@Jacks-MacBook-Pro.local>
2025-04-17 14:26:32 +0200

    feat: let users input car names

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: Implement the program
+    print("Enter names of cars (max 5 chars):e.g mike,mary,mo")
+    val input = Console.readLine()
+    val names = input.split(",").map { it.trim() }
+    names.forEach { name ->
+        println(name)
+    }
 }
