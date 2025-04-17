64a158d8cf4199bc7b91213db486aeb130790e63
2taezeat <xorudfl772@naver.com>
2025-04-17 20:42:29 +0900

    feat: print, winners

--- src/main/kotlin/racingcar/OutputController.kt
+++ src/main/kotlin/racingcar/OutputController.kt
@@ -4,7 +4,7 @@ object OutputController {
     private const val ENTER_THE_NAMES_OF_CARS_OUTPUT = "Enter the names of the cars (comma-separated):"
     private const val PLAY_ROUND_OUTPUT = "How many rounds will be played?"
     private const val RACE_RESULTS = "Race Results"
-    private const val WINNERS = "Winners"
+    private const val WINNERS = "Winners : "
     private const val DISTANCE = "-"
 
     fun printNamesOfCars() {
@@ -17,14 +17,23 @@ object OutputController {
 
     fun printPlayerCarState(playerCar: PlayerCar) {
         print(playerCar.name + " : ")
-        repeat(playerCar.distance) {
-            print("-")
-        }
+        repeat(playerCar.distance) { print(DISTANCE) }
         println()
     }
 
     fun printBlankLine() {
         println()
+    }
+
+    fun printRaceResults() {
+        printBlankLine()
+        println(RACE_RESULTS)
+    }
+
+    fun printWinners(winners: List<PlayerCar>) {
+        printBlankLine()
+        print(WINNERS)
+        println(winners.joinToString(", ") { it.name })
     }
 
 
