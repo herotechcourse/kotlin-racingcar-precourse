aeb84447cc70c76c881d541028b315549e71c0dd
sunishae <sunishae44@gmail.com>
2025-04-17 13:42:12 +0900

    feat: implement RacingGame to control race and print results

--- src/main/kotlin/racingcar/model/RacingGame.kt
+++ src/main/kotlin/racingcar/model/RacingGame.kt
@@ -1,4 +1,28 @@
 package racingcar.model
 
-class RacingGame {
+import racingcar.util.NumberGenerator
+import racingcar.view.OutputView
+
+// class that runs the whole racing game
+// takes in cars and how many rounds to play
+class RacingGame(
+    private val cars: Cars,
+    private val roundCount: Int
+) {
+
+    // this starts the race
+    // calls moveCars() roundCount times
+    fun start() {
+        for (i in 1..roundCount) {
+            val numbers = NumberGenerator.generate(carsSize())
+            cars.moveCars(numbers)
+
+            OutputView.printRoundResult(cars.getCars())
+        }
+    }
+
+    // gets how many cars are in the race
+    private fun carsSize(): Int {
+        return cars.getCars().size
+    }
 }
