b80cc4889022ac78f829277e9d21d7ce004a7f61
2taezeat <xorudfl772@naver.com>
2025-04-17 19:43:12 +0900

    feat: InputController, read userInput

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,3 +3,14 @@ package racingcar
 fun main() {
     // TODO: Implement the program
 }
+
+
+object InputController {
+    fun readNamesOfCars(): List<String>? {
+        return readlnOrNull()?.split(",")
+    }
+
+    fun readRounds(): Int? {
+        return readlnOrNull()?.toInt()
+    }
+}
