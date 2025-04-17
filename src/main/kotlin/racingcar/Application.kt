8029dfd14a14d5465564572bbdf44b8336a1b2b4
2taezeat <xorudfl772@naver.com>
2025-04-17 19:44:05 +0900

    feat: printing output

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -14,3 +14,19 @@ object InputController {
         return readlnOrNull()?.toInt()
     }
 }
+
+
+object OutputController {
+    private const val ENTER_THE_NAMES_OF_CARS_OUTPUT = "Enter the names of the cars (comma-separated):"
+    private const val PLAY_ROUND_OUTPUT = "How many rounds will be played?"
+    private const val RACE_RESULTS = "Race Results"
+    private const val WINNERS = "Winners"
+
+    fun printNamesOfCars() {
+        println(ENTER_THE_NAMES_OF_CARS_OUTPUT)
+    }
+
+    fun printPlayerRound() {
+        println(PLAY_ROUND_OUTPUT)
+    }
+}
