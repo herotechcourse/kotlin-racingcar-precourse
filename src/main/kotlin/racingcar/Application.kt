c7ad4b7ed69f87409ae52bda3ae66213fb71c232
DOL-DOM <haesolj@naver.com>
2025-04-17 23:33:41 +0900

    fix: add exceptions

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -25,8 +25,19 @@ fun readCarNames(): List<String>? {
     println("Enter the names of the cars (comma-separated):")
     val input = Console.readLine()
 
-    val carNames = input?.split(",")?.map { it.trim() }
+    val carNames = input.split(",").map { it.trim() }
 
+    if (carNames.isEmpty() || carNames.any { it.isEmpty() }) {
+        throw IllegalArgumentException("It can't be empty.")
+    }
+
+    if (carNames.size < 2) {
+        throw IllegalArgumentException("At least two cars are required.")
+    }
+
+    if (carNames.any { it.length > 5 }) {
+        throw IllegalArgumentException("Only up to 5characters are allowed.")
+    }
 
     return carNames
 }
