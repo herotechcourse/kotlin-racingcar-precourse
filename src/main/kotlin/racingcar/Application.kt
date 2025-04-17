9fe539635f7f4ab9b4aad87449d2876b23722e95
knopgm <knop.gm@gmail.com>
2025-04-17 16:31:17 +0200

    feat(user input): add a prompt asking for user to enter the car names and how many round the race will take.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,16 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     // TODO: Implement the program
+
+    // Interactive form for user's car name input:
+    println("Welcome to Racing Cars! Enter the car names that will compete together(comma-separated):")
+    val cars = Console.readLine()
+    println("The competitors are: $cars")
+
+    // Interactive form for the number of round the race should take:
+    println("Now, choose how many rounds the competitors should run:")
+    val rounds = Console.readLine()
+    println("The competitors will run $rounds rounds")
 }
