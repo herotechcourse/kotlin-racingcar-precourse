1a1030f5ee52c63182582a5d37157ca79177ba05
DOL-DOM <haesolj@naver.com>
2025-04-17 22:48:28 +0900

    chore: plan project structure

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,4 +2,16 @@ package racingcar
 
 fun main() {
     // TODO: Implement the program
+
+    //1. get inputs of player names
+
+    //2. get inputs of rounds
+
+    //3. function: make random result
+
+    //4. print racing result (player names, random function)
+
+    //5. calculate who wins
+
+    //6. print winner
 }
