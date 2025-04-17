77704d3a78ba34d4accea2877d7b7ac2917c7a35
권장순 <jsoonworld@gmail.com>
2025-04-17 18:24:08 +0900

    refactor(OutputView) : convert class to interface for flexible output handling

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,31 +1,9 @@
 package racingcar.view
 
-class OutputView {
-
-    fun printCarNamesPrompt() {
-        println(CAR_NAME_PROMPT)
-    }
-
-    fun printRoundCountPrompt() {
-        println(ROUND_COUNT_PROMPT)
-    }
-
-    fun printRaceResultPrompt() {
-        println(RACE_RESULT_PROMPT)
-    }
-
-    fun printProgress(lines: List<String>) {
-        lines.forEach { println(it) }
-        println()
-    }
-
-    fun printWinners(winnerMessage: String) {
-        println(winnerMessage)
-    }
-
-    companion object {
-        private const val CAR_NAME_PROMPT = "Enter the names of the cars (comma-separated):"
-        private const val ROUND_COUNT_PROMPT = "How many rounds will be played?"
-        private const val RACE_RESULT_PROMPT = "Race Results"
-    }
+interface OutputView {
+    fun printCarNamesPrompt()
+    fun printRoundCountPrompt()
+    fun printRaceResultPrompt()
+    fun printProgress(message: List<String>)
+    fun printWinners(message: String)
 }
