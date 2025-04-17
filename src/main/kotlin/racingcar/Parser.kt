318de4a968341b4a0b1e7dfda46c19b646c7ed79
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-17 12:47:06 +0200

    feat(parser):moving the logic of parsing to separate file and validation

---
+++ src/main/kotlin/racingcar/Parser.kt
@@ -0,0 +1,14 @@
+package racingcar
+
+object Parser {
+  fun parseCars(input: String): MutableList<Car> {
+    return input.split(",")
+      .map { it.trim() }
+      .onEach {
+        require(it.isNotEmpty()) { ErrorMessages.ERROR_EMPTY_CAR_NAME }
+        require(it.length <= 5) { ErrorMessages.ERROR_LONG_CAR_NAME }
+      }
+      .map { Car(it) }
+      .toMutableList()
+  }
+}
