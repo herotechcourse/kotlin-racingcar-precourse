e9dbddea9bd1b323b57aaf65f164e6551794a363
ShrutiK23 <shruti.kulkarni@st.ovgu.de>
2025-04-17 08:46:29 +0200

    feat(game): user input implemented
    The user inputs car names and the number of rounds. The car names, entered as a comma-separated string, is split into a list of names.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
-fun main() {
-    // TODO: Implement the program
+fun main()
+{
+    //Read car names and number of rounds from user
+    println("Enter the names of the cars (comma-separated):")
+    val carNames = readLine()
+    SeparateCarNames(carNames)
+
+    println("How many rounds will be played?")
+    val rounds = Console.readLine()
+    println("Number of rounds: $rounds")
+
+}
+
+//Separate names into list of strings based on "," as delimiter
+fun SeparateCarNames(carNames:String?){
+    val names = carNames!!.split(",")
+    println("Car names are: $names")
 }
