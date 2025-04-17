6ae0150ead397f72f329c121e7047cb5e34e65c5
DOL-DOM <haesolj@naver.com>
2025-04-17 23:23:12 +0900

    feat: add readCarNames()

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,14 @@
 package racingcar
 
+
 fun main() {
     // TODO: Implement the program
 
-    //1. get inputs of player names
+    //1. get inputs of player names - readCarNames()
+
+
+
+
 
     //2. get inputs of rounds
 
@@ -14,4 +19,15 @@ fun main() {
     //5. calculate who wins
 
     //6. print winner
+}
+
+fun readCarNames(): List<String>? {
+    println("Enter the names of the cars (comma-separated):")
+    val input = readLine()
+
+    val carNames = input?.split(",")?.map { it.trim() }
+
+
+
+    return carNames
 }
