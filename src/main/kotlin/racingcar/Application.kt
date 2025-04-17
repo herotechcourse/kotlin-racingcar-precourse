db85d5b48d1ddc39a8681d30ba8bf0e8f40535fc
ShrutiK23 <shruti.kulkarni@st.ovgu.de>
2025-04-17 12:07:29 +0200

    feat(game):validate the input strings
    - Each car name is trimmed of leading and trailing white spaces since it
     could be entered by the user by mistake
    
    - For car name entered, an IllegalArgumentException is thrown if
    	- The string is null or empty
    	- If the car names are duplicates
    	- If there is an empty car name (comma followed by nothing or just a
      	  space)
    	- If the car name length exceeds 5 characters
    
    - Number of rounds input by the user is also validated. An
      IllegalArgumentException is thrown if
    	- The number to be entered is null or empty
    	- The number enetered is 0

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,11 +7,21 @@ fun main()
     //Read car names and number of rounds from user
     println("Enter the names of the cars (comma-separated):")
     val carNames = readLine()
-    var names = SeparateCarNames(carNames)
+    var car_Names = SeparateCarNames(carNames)
+    CarNamesValidation(carNames)
+    CarNamesLengthValidation(car_Names)
+    CarNamesDuplicateValidation(car_Names)
+    CarNamesEmptyValidation(car_Names)
+
+    //trim leading and trailing white spaces which may be entered by mistake
+    var names = mutableListOf<String>()
+    for(i in 0 until car_Names.size){
+        names.add(car_Names[i].trim())
+    }
 
     println("How many rounds will be played?")
     val rounds = Console.readLine()
-    println("Number of rounds: $rounds")
+    RoundsValidation(rounds)
 
     //Car racing logic
     racing_func(names,rounds)
@@ -83,4 +93,37 @@ fun SeparateCarNames(carNames:String?) : List<String>
 {
     val names = carNames!!.split(",")
     return names
+}
+
+
+//A function to check if the car name that is entered is null
+fun CarNamesValidation(names:String?)
+{
+    if (names.isNullOrEmpty()) throw IllegalArgumentException("Atleast one car name is required")
+}
+
+//A function to check if the car name has duplicates
+fun CarNamesDuplicateValidation(names:List<String>)
+{
+    if (names.size != names.toSet().size) throw IllegalArgumentException("Cars must not have duplicate names")
+}
+
+//A function to check if the car name is empty
+fun CarNamesEmptyValidation(names:List<String>)
+{
+    if (names.contains("")) throw IllegalArgumentException("A car name cannot be empty")
+    for(i in 0 until names.size){
+        if(names[i].trim().isNullOrEmpty()) throw IllegalArgumentException("A car name cannot be empty")
+    }
+}
+
+//A function to check if the car names length exceeds 5
+fun CarNamesLengthValidation(names:List<String>){
+    if (names.any { it.length > 5 }) throw IllegalArgumentException("One or more names exceed 5 characters.")
+}
+
+//A function to check if the number of rounds is an integer
+fun RoundsValidation(rounds:String){
+    if(rounds.isNullOrEmpty()) throw IllegalArgumentException("Number of rounds must be an integer")
+    if(rounds.toInt() == 0) throw IllegalArgumentException("Number of rounds should be a number greater than 0")
 }
