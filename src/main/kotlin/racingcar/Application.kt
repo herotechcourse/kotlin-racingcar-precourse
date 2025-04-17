2e6bada9a418b18193b388685b29fbc68af8d4f2
LeeJaeHyeok97 <hazardous10@naver.com>
2025-04-17 16:20:40 +0900

    feat(exception): car name's length limit add

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -11,67 +11,72 @@ class Car(val name: String) {
 }
 
 fun main() {
-    // racing start introducing comment
-    println("Enter the names of the cars (comma-separated):")
-    val challengers = Console.readLine()
-    if (challengers.isEmpty()) {
-        throw IllegalArgumentException("Car name can't be empty.")
-    }
+    try {
+        // racing start introducing comment
+        println("Enter the names of the cars (comma-separated):")
+        val challengers = Console.readLine()
+        if (challengers.isEmpty()) {
+            throw IllegalArgumentException("Car name can't be empty.")
+        }
 
-    println("How many rounds will be played?")
-    val rounds = Console.readLine()
-    if (rounds.toInt() < 2) {
-        throw IllegalArgumentException("Race round is more than 1")
-    }
+        println("How many rounds will be played?")
+        val rounds = Console.readLine()
+//    if (rounds.toInt() < 2) {
+//        throw IllegalArgumentException("Race round is more than 1")
+//    }
 
-    println()
-    println("Race Results")
+        println()
+        println("Race Results")
 
-    val challengerList = challengers.split(",") // [pobi, woni, jun]
-    // make Car object
-    var challengerGroup = mutableListOf<Car>()
-    for (challenger in challengerList) {
-        challengerGroup.add(Car(challenger))
-    }
+        val challengerList = challengers.split(",") // [pobi, woni, jun]
+        // make Car object
+        var challengerGroup = mutableListOf<Car>()
+        for (challenger in challengerList) {
+            if (challenger.length > 5) {
+                throw IllegalArgumentException("names cannot exceed 5 characters.")
+            }
+            challengerGroup.add(Car(challenger))
+        }
 
 //    Games()
-    var currentRound = 0
-    while (currentRound != rounds.toInt()) {
-        for (challenger in challengerGroup) {
-            val diceNumber = Randoms.pickNumberInRange(0, 9)
-            if (diceNumber >= 4) {
-                challenger.currentPosition += 1
+        var currentRound = 0
+        while (currentRound != rounds.toInt()) {
+            for (challenger in challengerGroup) {
+                val diceNumber = Randoms.pickNumberInRange(0, 9)
+                if (diceNumber >= 4) {
+                    challenger.currentPosition += 1
+                }
             }
-        }
 
 //        print current racing status
-        for (challenger in challengerGroup) {
-            print("${challenger.name} : ")
-            for (p in 1..challenger.currentPosition) {
-                print("-")
+            for (challenger in challengerGroup) {
+                print("${challenger.name} : ")
+                for (p in 1..challenger.currentPosition) {
+                    print("-")
+                }
+                println()
             }
             println()
+            currentRound += 1
         }
-        println()
-        currentRound += 1
-    }
 
-    // search winners
-    var winnerGroup = mutableListOf<String>()
-    var tmp = 0
-    challengerGroup.forEach {
-        challenger ->
-        if (challenger.currentPosition > tmp) {
-            winnerGroup = mutableListOf<String>()
-            winnerGroup.add(challenger.name)
-            tmp = challenger.currentPosition
+        // search winners
+        var winnerGroup = mutableListOf<String>()
+        var tmp = 0
+        challengerGroup.forEach { challenger ->
+            if (challenger.currentPosition > tmp) {
+                winnerGroup = mutableListOf<String>()
+                winnerGroup.add(challenger.name)
+                tmp = challenger.currentPosition
+            } else if (challenger.currentPosition == tmp) {
+                winnerGroup.add(challenger.name)
+            }
         }
-        else if (challenger.currentPosition == tmp) {
-            winnerGroup.add(challenger.name)
-        }
-    }
 
-    // print winners
-    print("Winners : ")
-    println(winnerGroup.joinToString(", "))
+        // print winners
+        print("Winners : ")
+        println(winnerGroup.joinToString(", "))
+    } catch (e: IllegalArgumentException) {
+        throw IllegalArgumentException("test")
+    }
 }
