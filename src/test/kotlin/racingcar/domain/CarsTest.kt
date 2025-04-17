f37f3a6848b0ede78d9b2b80850b4e36d10acfa4
권장순 <jsoonworld@gmail.com>
2025-04-17 11:09:32 +0900

    refactor(Cars): throw exception when winners() is called on empty car list

--- src/test/kotlin/racingcar/domain/CarsTest.kt
+++ src/test/kotlin/racingcar/domain/CarsTest.kt
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
+import racingcar.domain.Cars.Companion.ERROR_NO_CARS
 
 class CarsTest {
 
