09355480355cf3cb88cb25216781ea52b7fd7287
jslusark <slusark.jessica@gmail.com>
2025-04-17 18:49:33 +0200

    feat(input): add validation functions for car names and round count
    - Enforced minimum 2 cars, non-empty names, and max length of 5 characters
    - Ordered round count validation in its own function
    - Added detailed error messages including index/position of invalid inputs
    - Updated TODOs to explore input constraits to prevent misuse and system overload (car name format, max rounds, max cars)

--- src/main/kotlin/racingcar/InputHandler.kt
+++ src/main/kotlin/racingcar/InputHandler.kt
@@ -8,15 +8,46 @@ object InputHandler {
   fun getCarNames(): List<String> {
     println("Enter the names of the cars (comma-separated):")
     val carList = Console.readLine().split(",").map { it.trim() }
-    return carList // TODO: validate names with required format and other edgecases
+    validateCarNames(carList)
+    return carList
   }
 
   fun getTotalRounds(): Int {
     println("How many rounds will be played?")
-    val totalRounds = Console.readLine().toIntOrNull()
-    if (totalRounds == null || totalRounds <= 0)
-        throw IllegalArgumentException(
-            "Input failed at getTotalRounds(): '$totalRounds' is not a valid round count.")
+    val totalRounds =
+        Console.readLine().toIntOrNull()
+            ?: throw IllegalArgumentException("Input failed at getTotalRounds(): found null.")
+    validateTotalRounds(totalRounds)
     return totalRounds
+  }
+
+  private fun validateCarNames(carList: List<String>) {
+    /*
+      TODO: could be useful to throw an error if a name is divided by spaces and force user to not use only symbols as names and at least come letters  (regex-check?)
+      TODO: could be useful to also put a limit on the number of cars to avoid overloading the system
+    */
+    throwErrorIf(
+        carList.size < 2,
+        "Input failed at getCarNames(): more than 1 car is required for the race to start.")
+    throwErrorIf(
+        carList.any { it.isEmpty() },
+        "Input failed at getCarNames(): empty car names found at index ${
+          carList.mapIndexedNotNull { index, name -> if (name.isEmpty()) index else null }
+          }.")
+    throwErrorIf(
+        carList.any { it.length > 5 },
+        "Input failed at getCarNames(): car names longer than 5 characters found at positions ${ carList.mapIndexedNotNull { index, name -> if (name.length > 5) index else null }}.")
+  }
+
+  private fun validateTotalRounds(totalRounds: Int) {
+    // TODO: could be useful to put a limit on total rounds to avoid overloading the system - limit
+    // could be 100 rounds?
+    throwErrorIf(
+        totalRounds <= 0,
+        "Input failed at getTotalRounds(): '$totalRounds' is not a valid round number.")
+  }
+
+  private fun throwErrorIf(error: Boolean, message: String) {
+    if (error) throw IllegalArgumentException(message)
   }
 }
