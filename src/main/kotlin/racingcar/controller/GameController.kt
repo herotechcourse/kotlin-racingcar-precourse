9b78e5baaecb709578cfe0bd18952c9fb41295b7
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2025-04-17 16:52:20 +0900

    feat: Implement basic game loop for racing rounds

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -1,11 +1,25 @@
 package racingcar.controller
 
+import racingcar.domain.Cars
+import racingcar.view.InputCarNames
+import racingcar.view.InputRoundCount
+import racingcar.view.OutputRaceProgress
+
 class GameController {
+
     fun startGame() {
+        val carNames = InputCarNames.invoke()
+        val cars = Cars.from(carNames)
+        val roundCount = InputRoundCount.invoke()
+
+        repeat(roundCount) { // repoeat number of rounds
+            startTurn(cars)
+        }
 
     }
 
-    fun startTurn(tryCount: Int) {
-
+    private fun startTurn(cars: Cars) {
+        cars.raceOnce() // Race the car once
+        OutputRaceProgress.showRace(cars) // Print the results of one round
     }
 }
