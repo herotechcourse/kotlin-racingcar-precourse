c08f767b8660664433aa53d0db6768ede7d521ea
Irene Rivero <irene.rivero.casal@gmail.com>
2025-04-17 14:06:44 +0200

    feat(domain): implement car movement logic in Race class
    - Add executeRound method that moves each car based on random number
    - Use Randoms library to generate random numbers between 0 and 9
    - Move cars forward when random number is 4 or greater

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -1,5 +1,8 @@
 package racingcar.domain
 
+// Library Randoms from subject req
+import camp.nextstep.edu.missionutils.Randoms
+
 class Race(private val cars: List<Car>, private val rounds: Int) {
     fun run(): List<Car> {
         repeat(rounds) {
@@ -8,10 +11,16 @@ class Race(private val cars: List<Car>, private val rounds: Int) {
         return determineWinners()
     }
 
+    // method should move each car based on the random number generation rule
+    // cars move if a random number between 0-9 is 4 or greater
     private fun executeRound() {
-        // TODO
+        cars.forEach { car ->
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            car.move(randomNumber)
+        }
     }
 
+    // method should find the car(s) that have moved the furthest
     private fun determineWinners(): List<Car> {
         // TODO
         return emptyList()
