f34f8f0dddec7f6ce0bb781efd1969d6ed82d364
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-04-18 02:20:37 +0900

    feat(input): add messages for data input

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -6,11 +6,13 @@ class InputView {
     private val parser = InputParser()
 
     fun readCarNames(): List<String> {
+        println("Enter the names of the cars (comma-separated):")
         val input = Console.readLine()
         return parser.parseCarNames(input)
     }
 
     fun readNumberOfRounds(): Int {
+        println("How many rounds will be played?")
         val input = Console.readLine()
         return parser.parseNumberOfRounds(input)
     }
