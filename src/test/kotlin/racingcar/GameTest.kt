371d9b173f0ff302607aa78fca83bc47742a3799
VittorioDeMarzi <vittoriodm7@gmail.com>
2025-04-17 15:44:05 +0200

    test: add unit tests for Game.getWinners
    - Verifies winner selection with a single winner
    - Verifies correct behavior when there are multiple winners

---
+++ src/test/kotlin/racingcar/GameTest.kt
@@ -0,0 +1,36 @@
+package racingcar
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+ class GameTest {
+
+  //getWinners
+  //1. One winner
+  @Test
+  fun getWinners() {
+   val car1 =
+    Car("car1").apply { position = 1 }
+   val car2 =
+    Car("car2").apply { position = 2 }
+
+   val game = Game(listOf(car1, car2))
+   val winners = game.getWinners()
+   assertEquals(listOf(car2), winners)
+  }
+
+  //2. Multiple winners
+  @Test
+  fun getWinners_multipleWinners() {
+   val car1 =
+    Car("car1").apply { position = 1 }
+   val car2 =
+    Car("car2").apply { position = 2 }
+   val car3 =
+    Car("car3").apply { position = 2 }
+
+   val game = Game(listOf(car1, car2, car3))
+   val winners = game.getWinners()
+   assertEquals(listOf(car2, car3), winners)
+  }
+ }
