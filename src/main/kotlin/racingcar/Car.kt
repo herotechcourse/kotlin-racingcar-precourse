ae2542400a91e2ff6a4aadaedf979af9f95138c7
speculatingwook <bwook9908@gmail.com>
2025-04-17 20:53:10 +0900

    feat(car): implement basic movement logic with Engine interface

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+class Car(
+    private val name: String,
+    private var position: Int,
+    private val engine: Engine) {
+
+    fun goForward() {
+        position += engine.move()
+    }
+}
