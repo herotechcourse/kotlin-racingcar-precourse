079f5dcd172986b0509b402e1092823319c17e9d
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-17 12:48:33 +0200

    refactor(car): make move private and introduce tryMove with random check

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,10 +1,18 @@
 package racingcar
 
-data class Car(val name: String, var currLevel: Int=0) {
-  fun move() {
-    this.currLevel++;
+import racingcar.Utils.getRandomNumber
+
+data class Car(val name: String, var currLevel: Int = 0) {
+  private fun move() {
+    this.currLevel++
   }
+
   fun getProgress(): String {
     return "-".repeat(currLevel)
   }
+
+  fun tryMove() {
+    if (getRandomNumber() >= 4) move()
+  }
+
 }
