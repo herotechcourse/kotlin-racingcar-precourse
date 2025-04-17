de7bc00016513e0a4be9eb436b0feb1bfabf84e4
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-04-18 03:24:07 +0900

    test(racingcar): add test for Application

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -9,22 +9,40 @@ import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
     @Test
-    fun `feature test`() {
+    fun `should print race result and winner when one round is played`() {
         assertRandomNumberInRangeTest(
             {
                 run("pobi,woni", "1")
                 assertThat(output()).contains("pobi : -", "woni : ", "Winners : pobi")
             },
-            MOVING_FORWARD,
-            STOP,
+            MOVING_FORWARD, STOP
         )
     }
 
     @Test
-    fun `exception test`() {
-        assertSimpleTest {
-            assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
-        }
+    fun `should print final result and winner when multiple rounds are played`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni,jun", "3")
+                assertThat(output()).contains("pobi : --", "woni : -", "jun : ", "Winners : pobi")
+            },
+            MOVING_FORWARD, STOP, STOP,
+            MOVING_FORWARD, STOP, STOP,
+            STOP, MOVING_FORWARD, STOP
+        )
+    }
+
+    @Test
+    fun `should print final result and winners when multiple rounds are played`() {
+        assertRandomNumberInRangeTest(
+            {
+                run("pobi,woni,jun", "3")
+                assertThat(output()).contains("pobi : -", "woni : --", "jun : --", "Winners : woni, jun")
+            },
+            STOP, STOP, MOVING_FORWARD,
+            MOVING_FORWARD, MOVING_FORWARD, STOP,
+            STOP, MOVING_FORWARD, MOVING_FORWARD
+        )
     }
 
     override fun runMain() {
