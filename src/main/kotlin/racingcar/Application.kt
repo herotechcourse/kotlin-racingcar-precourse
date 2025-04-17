a5016d63e3fec547c2b1086d3fbd97c28dd459d4
GDMontsho <montshogoitseone@gmail.com>
2025-04-17 10:37:50 +0200

    feat: implement Car class with move and progress logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,13 +1,13 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 import java.lang.IllegalArgumentException
 
 fun main() {
     // TODO: Implement the program
 
     //Step One: We need to get user input for car names and rounds. We will use ReadLine to achieve this.
-
     println("Please enter the names of cars (separated by commas):")
     val namesOfCars = Console.readLine().split(",").map {it.trim()}
 
@@ -23,7 +23,7 @@ fun main() {
         throw IllegalArgumentException("Name of car must not exceed 5 characters")
     }
 
-    // 1.2 Convert user input rounds to a positive integer (greater that 1)
+    // 1.2 Convert user input rounds to a positive integer (greater than 1)
     println("How many rounds will be played?")
     val gameRounds = Console.readLine().toInt()
 
@@ -31,5 +31,32 @@ fun main() {
 
     if (gameRounds <= 0) {
         throw IllegalArgumentException("Number of Rounds Must Be Greater than 0")
+    }
+}
+
+//2.We need to create the cars:
+// 2.1 Cars need a name
+// 2.2 Cars need a current position they are in.
+// 2.3 Give cars ability to move forward.
+// 2.4 Track game progress.
+
+
+//2.1 Cars need a name
+class Car(val name: String) {
+//2.2 Cars need a current position they are in
+    var position: Int = 0
+        private set
+
+//2.3 Give cars ability to move forward.
+    fun moveCar() {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4) {
+            position++
+        }
+    }
+
+//2.4 Track game progress.
+    fun getGameProgress(): String {
+        return "-".repeat(position)
     }
 }
