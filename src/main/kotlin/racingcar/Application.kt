b28fc93170cb1fd9963d51550c95d7d76d4721ff
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-04-18 02:26:17 +0900

    feat(domain): implement race game progression Logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,25 @@
 package racingcar
 
+import racingcar.domain.*
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val carNames = inputView.readCarNames()
+    val cars = createCars(carNames)
+    val numberOfRounds = inputView.readNumberOfRounds()
+    val race = Race(Cars(cars), numberOfRounds)
+
+    val raceResults = race.start()
+
+    printResults(raceResults)
+}
+
+private fun createCars(carNames: List<String>) =
+    carNames.map { CarName(it) }.map { Car(it, RandomPowerGenerator()) }.toList()
+
+private fun printResults(raceResults: RaceResults) {
+    val outputView = OutputView()
+    outputView.printRaceResults(raceResults)
 }
