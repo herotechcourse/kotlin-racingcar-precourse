f4d068a1e93bbad86bdce9669cfc66eec9e53cb5
권장순 <jsoonworld@gmail.com>
2025-04-17 18:25:41 +0900

    refactor(RaceConfig) : replace InputView and OutputView with console implementations

--- src/main/kotlin/racingcar/config/RaceConfig.kt
+++ src/main/kotlin/racingcar/config/RaceConfig.kt
@@ -6,13 +6,13 @@ import racingcar.formatter.RaceProgressFormatter
 import racingcar.formatter.WinnerFormatter
 import racingcar.parser.CarNameParser
 import racingcar.parser.RoundParser
-import racingcar.view.InputView
-import racingcar.view.OutputView
+import racingcar.view.ConsoleInputView
+import racingcar.view.ConsoleOutputView
 
 class RaceConfig {
     fun raceRunner(): RacingGame {
-        val inputView = InputView()
-        val outputView = OutputView()
+        val inputView = ConsoleInputView()
+        val outputView = ConsoleOutputView()
         val carNameParser = CarNameParser()
         val roundParser = RoundParser()
         val numberGenerator = RandomNumberGenerator()
