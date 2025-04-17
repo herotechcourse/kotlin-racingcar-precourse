864042084accd5add67bbe0e4aa2ab707878f326
speculatingwook <bwook9908@gmail.com>
2025-04-18 08:46:00 +0900

    test(winner): add unit tests for winner determination

---
+++ src/test/kotlin/racingcar/WinnerTest.kt
@@ -0,0 +1,56 @@
+package racingcar
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class WinnerTest {
+
+    @Test
+    @DisplayName("When the result is single winner")
+    fun singleWinnerTest() {
+        val engine = FixedEngine(1)
+        val cars = listOf(
+            Car("pobi", 3, engine),
+            Car("woni", 1, engine),
+            Car("jun", 2, engine)
+        )
+
+        val winner = Winner()
+        val result = winner.findWinner(cars)
+
+        assertEquals(listOf("pobi"), result)
+    }
+
+    @Test
+    @DisplayName("When the result is multiple winner")
+    fun multipleWinnersTest() {
+        val engine = FixedEngine(1)
+        val cars = listOf(
+            Car("pobi", 3, engine),
+            Car("woni", 3, engine),
+            Car("jun", 2, engine)
+        )
+
+        val winner = Winner()
+        val result = winner.findWinner(cars)
+
+        assertEquals(listOf("pobi", "woni"), result)
+    }
+
+    @Test
+    @DisplayName("When the result is all")
+    fun allSameDistanceTest() {
+        val engine = FixedEngine(1)
+        val cars = listOf(
+            Car("pobi", 1, engine),
+            Car("woni", 1, engine),
+            Car("jun", 1, engine)
+        )
+
+        val winner = Winner()
+        val result = winner.findWinner(cars)
+
+        assertEquals(listOf("pobi", "woni", "jun"), result)
+    }
+}
