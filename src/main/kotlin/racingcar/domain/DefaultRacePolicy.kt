1dea03a9bb3c6bfa59f09ef632081650aaa0a131
권장순 <jsoonworld@gmail.com>
2025-04-17 18:23:39 +0900

    refactor(DefaultRacePolicy) : apply new RacePolicy interface with Car parameter

--- src/main/kotlin/racingcar/domain/DefaultRacePolicy.kt
+++ src/main/kotlin/racingcar/domain/DefaultRacePolicy.kt
@@ -1,7 +1,7 @@
 package racingcar.domain
 
 class DefaultRacePolicy : RacePolicy {
-    override fun canMove(number: Int): Boolean = number >= FORWARD_CONDITION
+    override fun canMove(car: Car, number: Int): Boolean = number >= FORWARD_CONDITION
 
     companion object {
         private const val FORWARD_CONDITION = 4
