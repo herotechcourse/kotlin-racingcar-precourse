5ecdaba2605f41fac18ad7c6bcc00d99dab98731
sunishae <sunishae44@gmail.com>
2025-04-17 13:54:23 +0900

    feat: added controller to connect user input and run the race

--- src/main/kotlin/racingcar/controller/RacingController.kt
+++ src/main/kotlin/racingcar/controller/RacingController.kt
@@ -1,2 +1,29 @@
 package racingcar.controller
 
+import racingcar.model.Car
+import racingcar.model.Cars
+import racingcar.model.RacingGame
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+class RacingController(
+    private val inputView: InputView,
+    private val outputView: OutputView
+){
+    fun run() {
+        val carNamesInput = inputView.readCarNames()
+        val roundCountInput = inputView.readNumberOfRounds()
+
+        val carNames = carNamesInput.split(",").map{ it.trim() }
+        val carList = carNames.map{ Car(it) }
+        val cars = Cars(carList)
+
+        val roundCount = roundCountInput.toInt()
+
+        OutputView.printStartMessage()
+        val game = RacingGame(cars, roundCount)
+        game.start()
+
+    }
+
+}
