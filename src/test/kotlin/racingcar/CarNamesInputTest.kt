4e27a3710acf482d041968dcb2fb43c178477838
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-04-18 00:09:45 +0900

    test(racingcar): add test for Input
    Use "\n" to simulate empty input. An empty string "" won’t trigger Console.readLine().

---
+++ src/test/kotlin/racingcar/CarNamesInputTest.kt
@@ -0,0 +1,72 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import support.IOTest
+
+class CarNamesInputTest : IOTest() {
+    @Test
+    fun `should return car names when input is valid`() {
+        // Given
+        val input = "pobi,woni,jun"
+
+        // When
+        val carNames = run(input) { Input.readNamesOfTheCars() }
+
+        // Then
+        val expected = listOf("pobi", "woni", "jun")
+        Assertions.assertThat(carNames).isEqualTo(expected)
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when car name is too long`() {
+        // Given
+        val input = "pobi,javaji"
+
+        // When & Then
+        assertSimpleTest {
+            assertInvalidCarNamesFor(input)
+        }
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when car names are duplicated`() {
+        // Given
+        val input = "pobi,pobi"
+
+        // When & Then
+        assertSimpleTest {
+            assertInvalidCarNamesFor(input)
+        }
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when input is blank`() {
+        // Given
+        val input = "   "
+
+        // When & Then
+        assertSimpleTest {
+            assertInvalidCarNamesFor(input)
+        }
+    }
+
+    @Test
+    fun `should throw IllegalArgumentException when input is empty`() {
+        // Given
+        val input = "\n"
+
+        // When & Then
+        assertSimpleTest {
+            assertInvalidCarNamesFor(input)
+        }
+    }
+
+    private fun assertInvalidCarNamesFor(input: String) {
+        assertThrows<IllegalArgumentException> {
+            runException(input) { Input.readNamesOfTheCars() }
+        }
+    }
+}
