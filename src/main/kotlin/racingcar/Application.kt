661bdc4b1223d92e57f734102335b0da0c0697b2
jongbinchoi <jongbin985@naver.com>
2025-04-17 22:13:53 +0900

    feat: Add Application.kt to start the race game

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+import racingcar.controller.RacingGameController
+import racingcar.view.InputView
+import racingcar.view.ResultView
+
 fun main() {
-    // TODO: Implement the program
+    val inputView = InputView()
+    val resultView = ResultView()
+    val controller = RacingGameController(inputView, resultView)
+
+    controller.race()
 }
