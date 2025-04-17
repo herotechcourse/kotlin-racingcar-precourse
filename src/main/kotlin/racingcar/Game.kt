ea6e9c1991ac40d71838f6a6e2950ac8d98896fe
glotar <Glotar@mail.ru>
2025-04-17 09:36:02 +0200

    feat(racingcar): accept and check user input
    Function initGame: requests user input for names of cars and number of rounds. Evaluates user input: if names of cars is empty or length of car name is bigger than 5 throws IllegalArgumentException.
    If number of rounds is less than one throws IllegalArgumentException. If couldn't parse number of rounds, throws NumberFormatException( : IllegalArgumentException)
    Input is used to fill the properties of Game class object.

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -1,18 +1,23 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
 class Game{
     private val cars: MutableList<Car> = mutableListOf()
-    private val numberOfRounds:Int
-    init {
-        //read input here
-        cars.add(Car("Dum1"))
-        cars.add(Car("Dum2"))
-        cars.add(Car("Dum3"))
-        cars.add(Car("Dum4"))
-        numberOfRounds = 0
+    private var numberOfRounds:Int = 0
+
+    // read input, check for errors
+    private fun initGame() {
+        println("Enter the names of the cars (comma-separated):")
+        val input = Console.readLine()
+        if (input.isNullOrEmpty()) throw IllegalArgumentException("No input with names provided")
+        val names = input.split(',')
+        for (name in names){
+            if (name.length > 5) throw IllegalArgumentException("Incorrect name: $name")
+            cars.add(Car(name))
+        }
+        println("How many rounds will be played?")
+        numberOfRounds = Console.readLine().toInt()
+        if (numberOfRounds < 1)
+            throw IllegalArgumentException("Less than one round")
     }
-//    fun printCars(){
-//        for (car in cars)
-//            car.printCar()
-//    }
 }
