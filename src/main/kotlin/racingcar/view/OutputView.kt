8a6d8875f730c409756c4d075ec903f1e849451b
sunishae <sunishae44@gmail.com>
2025-04-17 13:38:38 +0900

    feat: added OutputView to show car positions after each round

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,2 +1,18 @@
 package racingcar.view
 
+import racingcar.model.Car
+
+// using object = only one instance (singleton)
+// this object just handles output, no data is stored
+object OutputView {
+
+    // prints the result of one round
+    // for each car, show the name and how far it moved using dashes
+    fun printRoundResult(cars: List<Car>) {
+        for (car in cars) {
+            val bar = "-".repeat(car.position)
+            println("${car.name} : $bar")
+        }
+        println() // extra line between rounds
+    }
+}
