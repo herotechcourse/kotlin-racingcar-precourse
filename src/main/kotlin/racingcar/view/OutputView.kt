985f2e269f28b39a2e6c5fdb708bad6819f8618a
soeunnPark <carol0724@naver.com>
2025-04-17 16:32:20 +0900

    feat: add a function print questions

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -4,6 +4,17 @@ import racingcar.model.Referee
 import racingcar.model.Round
 
 class OutputView {
+    fun printCarNamesQuestion() {
+        println("Enter the names of the cars (comma-separated):")
+    }
+
+    fun printTryCountQuestion() {
+        println("How many rounds will be played?")
+    }
+
+    fun printRoundResultTitle() {
+        println("Race Results")
+    }
 
     fun printRoundResult(round: Round) {
         val roundResultForOutput = RoundResultDto.of(round).roundResultForOutput
