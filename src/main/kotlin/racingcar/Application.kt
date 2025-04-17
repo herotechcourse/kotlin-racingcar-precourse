317d73e381d39b183e32ae9622a29db6b6f8f5a2
GDMontsho <montshogoitseone@gmail.com>
2025-04-17 12:21:43 +0200

    chore: clean up comments and refactor final game structure

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,66 +2,54 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
-import java.lang.IllegalArgumentException
 
 fun main() {
-    // TODO: Implement the program
-
-    //Step One: We need to get user input for car names and rounds.
-    // We will use ReadLine to achieve this.(refactored for modularity and clarity)
-
-    fun readValidatedCarNames(): List<String> {
-        println("Please enter the names of cars (separated by commas):")
-        val namesOfCars = Console.readLine().split(",").map {it.trim()}
-
-        if(namesOfCars.any {it.length > 5}) {
-            throw IllegalArgumentException("Name of car must not exceed 5 characters")
-        }
-        return namesOfCars
-    }
-
-    // 1.2 Convert user input rounds to a positive integer (greater than 1)
-    fun readValidatedGameRounds(): Int {
-        println("How many rounds will be played?")
-        val gameRounds = Console.readLine().toIntOrNull()
-            ?: throw IllegalArgumentException("Rounds must be a number")
-
-        if (gameRounds <= 0) {
-            throw IllegalArgumentException("Number of Rounds Must Be Greater than 0")
-        }
-        return gameRounds
-    }
-
     val namesOfCars = readValidatedCarNames()
     val gameRounds = readValidatedGameRounds()
 
-    fun determineWinners(cars: List<Car>): List<String> {
-        val maxPosition = cars.maxOf { it.position }
-        return cars
-            .filter { it.position == maxPosition }
-            .map { it.name }
-    }
-
-
-    //2. Create Car obj.
-    val cars = namesOfCars.map {Car(it)}
+    val cars = namesOfCars.map { Car(it) }
 
     println("\nGame Results")
     repeat(gameRounds) {
-        cars.forEach {it.moveCar()}
+        cars.forEach { it.moveCar() }
         printGameRound(cars)
     }
 
     val winners = determineWinners(cars)
-    println("Winners: ${winners.joinToString(", ")}")
-
+    println("Winner : ${winners.joinToString(", ")}")
 }
 
-//3.Create fn that takes the Car obj as param.
-//Loops through each car.
-// Prints Car.
-// Prints the cars name/progress.
-// add spacing after each round
+// --- Input Fns ---
+
+fun readValidatedCarNames(): List<String> {
+    println("Enter the names of the cars (separated by commas':")
+    val namesOfCars = Console.readLine().split(",").map { it.trim() }
+
+    if (namesOfCars.any { it.length > 5 }) {
+        throw IllegalArgumentException("Name of car must not exceed 5 characters")
+    }
+
+    return namesOfCars
+}
+
+fun readValidatedGameRounds(): Int {
+    println("How many rounds will be played?")
+    val gameRounds = Console.readLine().toIntOrNull()
+        ?: throw IllegalArgumentException("Rounds must be a number")
+
+    if (gameRounds <= 0) {
+        throw IllegalArgumentException("Number of rounds must be greater than 0")
+    }
+
+    return gameRounds
+}
+
+// ---Logic that runs the game ---
+
+fun determineWinners(cars: List<Car>): List<String> {
+    val maxPosition = cars.maxOf { it.position }
+    return cars.filter { it.position == maxPosition }.map { it.name }
+}
 
 fun printGameRound(cars: List<Car>) {
     for (car in cars) {
@@ -70,21 +58,12 @@ fun printGameRound(cars: List<Car>) {
     println()
 }
 
+// -----------Class for our Car ---
 
-//2.We need to create the cars:
-// 2.1 Cars need a name
-// 2.2 Cars need a current position they are in.
-// 2.3 Give cars ability to move forward.
-// 2.4 Track game progress.
-
-
-//2.1 Cars need a name
 class Car(val name: String) {
-//2.2 Cars need a current position they are in
     var position: Int = 0
         private set
 
-//2.3 Give cars ability to move forward.
     fun moveCar() {
         val randomNumber = Randoms.pickNumberInRange(0, 9)
         if (randomNumber >= 4) {
@@ -92,7 +71,6 @@ class Car(val name: String) {
         }
     }
 
-//2.4 Track game progress.
     fun getGameProgress(): String {
         return "-".repeat(position)
     }
