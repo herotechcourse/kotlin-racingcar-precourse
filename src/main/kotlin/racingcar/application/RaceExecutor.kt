a513a9ad04d2b755c242c0190c2da84d9accf739
권장순 <jsoonworld@gmail.com>
2025-04-17 10:46:25 +0900

    feat(RaceExecutor): implement race execution to collect round results

---
+++ src/main/kotlin/racingcar/application/RaceExecutor.kt
@@ -0,0 +1,27 @@
+package racingcar.application
+
+import racingcar.domain.Car
+import racingcar.domain.Cars
+import racingcar.domain.NumberGenerator
+import racingcar.domain.RacePolicy
+
+class RaceExecutor(
+    private val numberGenerator: NumberGenerator,
+    private val cars: Cars,
+    private val round: Int,
+    private val racePolicy: RacePolicy
+) {
+    fun execute(): List<Cars> {
+        val roundResults = mutableListOf<Cars>()
+        repeat(round) {
+            cars.moveIf(this::canMoveCar)
+            roundResults.add(cars.snapshot())
+        }
+        return roundResults
+    }
+
+    private fun canMoveCar(car: Car): Boolean {
+        val number = numberGenerator.generate()
+        return racePolicy.canMove(number)
+    }
+}
