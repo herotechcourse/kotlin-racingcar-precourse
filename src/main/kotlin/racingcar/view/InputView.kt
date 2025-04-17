e4b950424dfb9934f40483f61877a05824ba480c
hongseokpyo <hongseokpyou@gmail.com>
2025-04-17 17:32:31 +0900

    refactor(inputview): change readline to camp library

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -5,7 +5,7 @@ import camp.nextstep.edu.missionutils.Console
 object InputView {
     fun readCarName(): List<String> {
         print("Enter the names of the cars (comma-separated):\n")
-        val players = readlnOrNull() ?: ""
+        val players = Console.readLine() ?: ""
         return players.split(",").map {
             val name = it.trim()
             if (name.length > 5) {
@@ -17,7 +17,7 @@ object InputView {
 
     fun readRoundCount(): Int {
         print("How many rounds will be played?\n")
-        val rounds = readlnOrNull() ?: ""
+        val rounds = Console.readLine() ?: ""
         return rounds.toInt()
     }
 }
