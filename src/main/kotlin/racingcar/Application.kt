948e8478e59e1ede2c78c0cfc0e0367f12185327
sominyun <somin455@naver.com>
2025-04-18 23:40:41 +0900

    feat: input car names

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,7 +1,9 @@
 package racingcar
 
+import racingcar.frontend.InputView
 import racingcar.frontend.OutputView
 
 fun main() {
     OutputView.printCarNamesRequest()
+    val carNames = InputView.getCarNames()
 }
---
+++ src/main/kotlin/racingcar/frontend/InputView.kt
@@ -0,0 +1,9 @@
+package racingcar.frontend
+
+import camp.nextstep.edu.missionutils.Console
+
+object InputView {
+    fun getCarNames(): String {
+        return Console.readLine()
+    }
+}
