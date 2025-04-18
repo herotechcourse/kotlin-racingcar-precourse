b91afc458c52ab9a142102e849cf4c38deb99699
David Gonzalez <davidegonzale@Davids-Air.fritz.box>
2025-04-18 12:04:06 +0200

    feat: implement input handling and validation for car names and round count

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,23 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    // TODO: Implement the program
+    println("Start your engines! Please enter the names of the cars that are about to race and remember to keep them short and separate them by a comma...Let's go!:")
+    val carNamesInput = Console.readLine()
+
+    val carNames = carNamesInput.split(",").map { it.trim() }
+
+    if (carNames.any { it.isEmpty() || it.length > 5 }) {
+        throw IllegalArgumentException("Hey! Every car must have a name no bigger than 5 characters.")
+    }
+
+    println("How many rounds will be played?")
+    val roundsInput = Console.readLine()
+    val numberOfRounds = roundsInput.toIntOrNull()
+        ?: throw IllegalArgumentException("Hey! Please enter a valid integer for number of rounds.")
+
+    if (numberOfRounds <= 0) {
+        throw IllegalArgumentException("Hey! The number of rounds must be a positive integer.")
+    }
+
 }
