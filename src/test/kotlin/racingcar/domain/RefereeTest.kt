c962f4f1402d809fc27c466afb701137cee22d7b
Jiihyun <cwlgusc@gmail.com>
2025-04-18 15:53:51 +0900

    test: add Referee test

---
+++ src/test/kotlin/racingcar/domain/RefereeTest.kt
@@ -0,0 +1,63 @@
+package racingcar.domain
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertAll
+import org.junit.jupiter.api.Test
+import racingcar.domain.car.Car
+import racingcar.domain.car.Name
+import racingcar.domain.numberGenerator.FixedNumberGenerator
+import java.util.*
+
+class RefereeTest {
+
+    @Test
+    fun `determine the winner when there is only one winner`() {
+        // given
+        val queueForCarA = LinkedList<Int>()
+        queueForCarA.add(1)
+
+        val queueForCarB = LinkedList<Int>()
+        queueForCarB.add(8)
+
+        val car1 = createCar("a", queueForCarA)
+        val car2 = createCar("b", queueForCarB)
+
+        val cars = Cars(listOf(car1, car2))
+        cars.moveAll()
+
+        val referee = Referee(cars)
+        // when
+        val winner = referee.determineWinner()
+        // then
+        assertThat(winner.first()).isEqualTo("b")
+    }
+
+    @Test
+    fun `determine the winners when there are multiple winners`() {
+        // given
+        val queueForCarA = LinkedList<Int>()
+        queueForCarA.add(5)
+
+        val queueForCarB = LinkedList<Int>()
+        queueForCarB.add(8)
+
+        val car1 = createCar("a", queueForCarA)
+        val car2 = createCar("b", queueForCarB)
+
+        val cars = Cars(listOf(car1, car2))
+        cars.moveAll()
+
+        val referee = Referee(cars)
+        // when
+        val winner = referee.determineWinner()
+        // then
+        assertAll(
+            { assertThat(winner).hasSize(2) },
+            { assertThat(winner.containsAll(listOf("a", "b"))) }
+        )
+    }
+
+    private fun createCar(name: String, queue: LinkedList<Int>): Car {
+        return Car(Name(name), 0, FixedNumberGenerator(queue))
+    }
+}
