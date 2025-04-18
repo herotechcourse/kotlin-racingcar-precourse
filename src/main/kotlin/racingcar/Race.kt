9334518ca098b0dab537d95b57df1d76293d172a
LisaW <LisaW@ya.ru>
2025-04-18 13:44:31 +0300

    feat: get new raceState and print information about round
    code: fun startRound(carsData: Map<String, Int>): Map<String, Int>

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -14,3 +14,12 @@ fun getCarMove(rand: Int): Int {
     println(rand)
     return if (rand < 4) 0 else 1
 }
+
+fun startRound(carsData: Map<String, Int>): Map<String, Int> {
+    val round = carsData.mapValues {
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        it.value + getCarMove(randomNumber)
+    }
+    printRound(round)
+    return round
+}
