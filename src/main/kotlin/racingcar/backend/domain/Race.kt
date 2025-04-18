2430527cfc1d1366de1b90055171c2faa53c4ec3
sominyun <somin455@naver.com>
2025-04-19 00:26:07 +0900

    feat: add car movement in Race object

--- src/main/kotlin/racingcar/backend/domain/Race.kt
+++ src/main/kotlin/racingcar/backend/domain/Race.kt
@@ -4,4 +4,14 @@ class Race(
     val car: Car,
     val position: MutableList<String> = mutableListOf(),
 ) {
+    private val lastPosition: String
+        get() = this.position.lastOrNull() ?: ""
+
+    fun move() {
+        this.position.add("$lastPosition-")
+    }
+
+    fun stay() {
+        this.position.add(lastPosition)
+    }
 }
