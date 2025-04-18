5ca2e6d582701966196dffc8a62ca7e3a3bda895
sominyun <somin455@naver.com>
2025-04-19 00:03:18 +0900

    feat: Connect controller and service

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,14 @@
 package racingcar
 
+import racingcar.backend.controller.RacingController
+import racingcar.backend.service.RacingService
 import racingcar.frontend.InputView
 import racingcar.frontend.OutputView
 
 fun main() {
+    val racingService = RacingService()
+    val racingController = RacingController(racingService)
+
     OutputView.printCarNamesRequest()
     val carNames = InputView.getCarNames()
 }
---
+++ src/main/kotlin/racingcar/backend/controller/RacingController.kt
@@ -0,0 +1,8 @@
+package racingcar.backend.controller
+
+import racingcar.backend.service.RacingService
+
+class RacingController(
+    private val racingService: RacingService
+) {
+}
