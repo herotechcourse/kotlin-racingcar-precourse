1cb08f58453f13a03e0ed94324f3e3248a9cd544
phan <hanfeelhoo@naver.com>
2025-04-18 10:54:05 +0900

    feat(Car) : add get car info method

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,11 +2,16 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 
-const val MOVE_THRESHOLD = 4
+const val MOVE_THRESHOLD: Int = 4
+const val PROGRESS_BAR: String = "-"
 
 class Car (val name: String, var pos: Int = 0) {
     private fun rollDice(): Int {
         return Randoms.pickNumberInRange(0, 9)
+    }
+
+    fun getCarInfo(): Pair<String, Int> {
+        return Pair(name, pos)
     }
 
     fun playRound() {
