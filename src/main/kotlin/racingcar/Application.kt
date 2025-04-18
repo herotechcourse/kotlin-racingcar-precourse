e98f5a4fa9142bab79591a98cef9dd06580fee60
Zaid <dagbanbia@gmail.com>
2025-04-18 09:40:39 +0200

    feat:input car names

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,38 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+
+
 fun main() {
-    // TODO: Implement the progra
+    // TODO: Implement the program
+
+    CarRacingGame.run()
 
 
+
+}
+
+
+class Car(val name: String){
+
+}
+
+object CarRacingGame{
+    private const val MAX_NAME_LENGTH = 5
+
+
+    fun run (){
+        val cars = inputCars()
+    }
+
+    private fun inputCars(): List<Car> {
+        println("Enter the names of the cars to race , seperated by commas : ")
+        val names = Console.readLine().split(",").map { it.trim() }
+        println(names) // list of cars name
+        return names.map{Car (it)}
+    }
+    
 
 }
