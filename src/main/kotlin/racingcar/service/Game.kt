a5f62746124a51b605f1f3f7416d471552aa52f1
Pavel Pukha <ppukha@gmail.com>
2025-04-18 23:55:34 +0200

    feat(game): implement Game controller with race logic and result output

--- src/main/kotlin/racingcar/service/Game.kt
+++ src/main/kotlin/racingcar/service/Game.kt
@@ -1,4 +1,29 @@
 package racingcar.service
 
-class Game {
+import racingcar.domain.Race
+
+class Game (
+    private val race: Race,
+    private val numberOfRounds: Int
+){
+
+    fun start(): Game {
+        repeat(numberOfRounds) {
+            race.runRound()
+            printProgress()
+        }
+        return this
+    }
+
+    fun printProgress() {
+        race.getCars().forEach{
+            println("${it.getName()} : ${"-".repeat(it.getPosition())}")
+        }
+        println()
+    }
+
+    fun printWinners() {
+        val winners = race.getWinners().map { it.getName() }
+        println("Winners : ${winners.joinToString {", "}}")
+    }
 }
