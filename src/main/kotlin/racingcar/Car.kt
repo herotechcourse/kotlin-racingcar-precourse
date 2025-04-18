136344e65504fcb3e7ee28cd5f807dbcf67a014a
Khush Bakht Naveed <khush.naveed@dci-student.org>
2025-04-19 00:42:28 +0200

    Refactor Car class with position tracking and name validation
    - Added validation to ensure car name is not blank and does not exceed 5 characters
    - Implemented move() function to update car position based on a random number
    - Added getProgress() function to return a visual representation of the car's position

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,4 +1,27 @@
 package racingcar
 
-class Car {
+class Car (private val name: String){
+
+    private var position: Int = 0
+
+    // Validation check in the init block to ensure the name is valid
+    init {
+        require(name.isNotBlank() && name.length <=5){
+            "Car name may not be blank!" +
+                    "Car name length should not be more than 5!"
+        }
+    }
+
+    // Increases the car's position by 1 if the random number is >= 4
+    fun move(randomNUmber: Int) {
+        if(randomNUmber >=4){
+            position++
+        }
+    }
+
+    // Returns a string representing the car's progress (visualizing the car's movement)
+    fun getProgress(): String{
+        return "$name : ${"-".repeat(position)}"
+    }
+
 }
