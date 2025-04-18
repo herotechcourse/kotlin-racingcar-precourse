965cdfbf078af30022cc162fa865fd32d0d499d0
Sanghyunn <shawnpark98@naver.com>
2025-04-18 22:53:15 +0900

    refactor: Separate MAX_NAME_LENGTH

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,16 +1,20 @@
 package racingcar.model
 
 class Car(val name: String) {
-  var position = 0
+    companion object {
+        const val MAX_NAME_LENGTH = 5
+    }
+
+    var position = 0
     private set
 
-  init {
-    require(name.isNotBlank() && name.length <= 5) {
-      "Car must have a name, and names cannot exceed 5 characters."
+    init {
+        require(name.isNotBlank() && name.length <= MAX_NAME_LENGTH) {
+            "Car must have a name, and names cannot exceed 5 characters."
+        }
     }
-  }
 
-  fun move() {
+    fun move() {
     position++
-  }
+    }
 }
