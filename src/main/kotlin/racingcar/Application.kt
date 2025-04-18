e074ee64b46ff3a09cafbc8145e03af33a5746b0
Seungyeon Kim <sungyeon1227@gmail.com>
2025-04-19 03:12:32 +0900

    refactor(main): update car racing logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,48 +1,6 @@
 package racingcar
+import racingcar.controller.GameController
 
-import camp.nextstep.edu.missionutils.Randoms
-import camp.nextstep.edu.missionutils.Console
-
-fun inputCarNames(): List<String> {
-    print("Enter the names of the cars (comma-separated): ")
-    val userInput = Console.readLine()
-    val carNames = userInput.split(",").map { it.trim() }
-
-    carNames.forEach {
-        require(it.isNotEmpty()) { "Each car must have a name." }
-        require(it.length <= 5) { "Car name must not exceed 5 characters." }
-    }
-
-    return carNames
-}
-
-fun inputRoundNum(): Int {
-    print("How many rounds will be played? ")
-    val userInput = Console.readLine()
-    val round = userInput.toIntOrNull() ?: throw IllegalArgumentException("Please enter a valid number.")
-
-    require(round > 0) { "Round must be greater than 0." }
-
-    return round
-}
-
-
-
-fun main() {
-    val carNames = inputCarNames()
-    val round = inputRoundNum()
-
-    println()
-    println("Race Results")
-    val cars= carNames.map{ Car(it)}
-    repeat(round){
-        cars.forEach {it.move()}
-        cars.forEach { it.printPosition() }
-        println()
-    }
-    val maxPosition = cars.maxOf {it.position}
-    val winners = cars.filter{ it.position == maxPosition}.map{it.name}
-
-
-    println("Winners : ${winners.joinToString(", " )}")
+fun main(){
+    GameController.runRacing()
 }
