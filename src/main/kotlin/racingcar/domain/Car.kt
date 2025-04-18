0244910be684e731546aa0a0b97fa76c121c4c28
Pavel Pukha <ppukha@gmail.com>
2025-04-18 03:09:06 +0200

    feat(car): implement Car class with name and movement logic

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,4 +1,18 @@
 package racingcar.domain
 
-class Car {
+import camp.nextstep.edu.missionutils.Randoms
+class Car (private val name: String) {
+    init {
+        require(name.length <=5) {"Car name cannot exceed 5 characters"}
+    }
+    private var position: Int = 0
+
+    fun move() {
+        if (Randoms.pickNumberInRange(0,9) >= 4) {
+            position++
+        }
+    }
+    fun getPosition(): Int = position
+
+    fun getName(): String = name
 }
