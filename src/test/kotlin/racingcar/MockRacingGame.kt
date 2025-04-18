38688af36b246ecbdee47037b302891590bb4b7c
wjunseok <wminsoo1@naver.com>
2025-04-18 10:02:12 +0900

    refactor(mock): move round logic out and rename roundCount to moveCallCount

--- src/test/kotlin/racingcar/MockRacingGame.kt
+++ src/test/kotlin/racingcar/MockRacingGame.kt
@@ -2,27 +2,21 @@ package racingcar
 
 import racingcar.generator.RandomNumberGenerator
 
-class MockRacingGame(val cars: List<Car>, val round: Int) {
+class MockRacingGame(val cars: List<Car>) {
 
-    var roundCount = 0
+    var moveCallCount = 0
         private set
 
     fun startGame(randomGenerator: RandomNumberGenerator) {
-        repeat(round) {
-            playOneRound(randomGenerator)
-            roundCount++
+        for (car in cars) {
+            car.move(randomGenerator)
+            moveCallCount++
         }
     }
 
     fun findWinner() : List<Car> {
         val max = cars.maxOf { car -> car.position }
         return cars.filter { car -> car.position == max }
-    }
-
-    private fun playOneRound(randomGenerator: RandomNumberGenerator) {
-        for (car in cars) {
-            car.move(randomGenerator)
-        }
     }
 
 }
