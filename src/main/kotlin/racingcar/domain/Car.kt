187936dcb71cf418673fdfdbaa372ca7a4cf2a38
Seungyeon Kim <sungyeon1227@gmail.com>
2025-04-19 03:06:29 +0900

    refactor(car): change Car logic by MVC pattern

---
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -0,0 +1,14 @@
+package racingcar.domain
+import camp.nextstep.edu.missionutils.Randoms
+
+class Car(val name:String){
+    var position = 0
+
+    fun move(){
+        val randomNumber = Randoms.pickNumberInRange(0,9)
+        if (randomNumber >= 4){
+            position++
+        }
+    }
+
+}
