42be514328533a9acd2f88f169cc955cea46438c
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2025-04-18 17:29:12 +0900

    refactor: simplify winner output formatting in OutputResult

--- src/main/kotlin/racingcar/view/OutputResult.kt
+++ src/main/kotlin/racingcar/view/OutputResult.kt
@@ -4,7 +4,7 @@ import racingcar.domain.Cars
 object OutputResult {
     fun showResult(cars: Cars) {
         val winners = cars.getWinners()
-        val winnerNames = winners.joinToString(", ") { it.name }
-        println("Winners : $winnerNames")
+            .joinToString(", ") { it.name }
+        println("Winners : $winners")
     }
 }
