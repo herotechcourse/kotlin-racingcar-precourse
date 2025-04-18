3cee1de619ed6050eb66e8580c2d803789dd1d2b
rinarina0429 <forrina0429@gmail.com>
2025-04-18 17:30:59 +0900

    feat(controller): add initial CarRacingController with race flow skeleton
    Created CarRacingController class and defined the main flow of the game.

---
+++ src/main/kotlin/racingcar/controller/CarRacingController.kt
@@ -0,0 +1,13 @@
+package racingcar.controller
+
+class CarRacingController {
+    fun run() {
+        // car names input
+
+        // number of rounds input
+
+        // run race
+
+        // announce winner
+    }
+}
