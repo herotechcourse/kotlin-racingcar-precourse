735e62fc3625a7db9089c79d2bc9b7af01cc4ea0
Irene Rivero <irene.rivero.casal@gmail.com>
2025-04-18 10:29:16 +0200

    test(ui): add manual testing for InputView in Application
    - Modify Application.run() to test InputView functionality
    - Add temporary logging to verify input parsing
    - Implement basic error handling for testing

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,15 +1,34 @@
 package racingcar
 
+import racingcar.domain.Car
+import racingcar.ui.InputView
+
 fun main() {
     Application().run()
 }
 
 class Application {
     fun run() {
-        // This will be implemented later
-        val name = "car"
-        val gameType = "racing"
-        println("This is a $name $gameType game")
-        println("----------")
+        try {
+            val inputView = InputView()
+
+            val carNames = inputView.readCarNames()
+            println("Entered:  $carNames")
+
+            val roundCount = inputView.readRounds()
+            println("Entered: " + roundCount)
+
+            val carsList = carNames.split(",").map { Car(it) }
+            println("Entered: " + carsList)
+
+            val roundsNumber = roundCount.toInt()
+            println("Entered: " + roundsNumber)
+
+            println("Input val success!")
+        }
+        catch (e: Exception) {
+            println("Error : ${e.message}")
+            e.printStackTrace()
+        }
     }
 }
