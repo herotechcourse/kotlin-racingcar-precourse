8a2930be35c81204f6e77e1130620cc7b2fcc210
Jisun Park <jisunpark.t@gmail.com>
2025-04-18 18:02:02 +0900

    feat(application): implement car movement based on random number per round
    - generate a random number between 0 and 9 for each car during each round
    - move the car forward if the number is 4 or greater

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,13 +4,21 @@ fun main() {
     println("Names of the cars (comma-separated):")
     val cars = readln().split(",").map { run {
         require(it.trim().length <= 5) { "Each name should be up to 5 characters"}
-        val car = Car(it)
+        val car = Car(it.trim())
         car
     } }
-    println(cars[0].name)
 
     println("How many rounds will be played?")
     val rounds = readln().toIntOrNull()
     require(rounds != null) {"The number of rounds should be a valid number"}
-    println(rounds)
+
+    println("\nRace Results")
+    for (i in 0..< rounds) {
+        cars.forEach{ car ->
+            car.move()
+            println(car.name + " : " + "-".repeat(car.distance))
+        }
+        println()
+    }
+
 }
--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,10 +1,16 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Car(
     val name: String,
     var distance: Int = 0
 ) {
     fun move() {
-        distance += 1
+        if (canMove()) distance += 1
+    }
+
+    private fun canMove(): Boolean {
+        return Randoms.pickNumberInRange(0, 9) >= 4
     }
 }
