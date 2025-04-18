44a65ac536834b8119322be3e8f9bc4b68a9fce5
haeun <jhe0903@naver.com>
2025-04-18 14:33:10 +0900

    feat(input): add validation for car names (max 5 characters)

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,6 +8,13 @@ fun inputCars(): List<String> {
     println("Enter the names of the cars (comma-seperated):");
     // Split the input string by commas
     val cars = Console.readLine().split(",");
+
+    for (car in cars) {
+        // Prohibit each car name to exceed 5 characters
+        if (car.length > 5) {
+            throw IllegalArgumentException("Each car name mush not exceed 5 characters.");
+        }
+    }
     return cars;
 }
 
