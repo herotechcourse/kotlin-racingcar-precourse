62407660cad57d09f7a0ebd7fee3b6bfc08f45bf
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-18 17:05:16 +0200

    test(race): add test for winner determining

--- src/test/kotlin/racingcar/RaceTest.kt
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -1,5 +1,6 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -11,6 +12,27 @@ class RaceTest : NsTest()   {
         val race = Race(cars)
         race.runRound { MOVING_FORWARD }
         assertThat(cars.all { it.position == 1 })
+    }
+
+    @Test
+    fun `winners finding test`() {
+        assertRandomNumberInRangeTest(
+            {
+                val cars = listOf(Car("pobi"), Car("woni"), Car("jido"))
+                val race = Race(cars)
+                repeat(2) {
+                    race.runRound { randomNumber() }
+                }
+                val winners = race.getWinners()
+                assertThat(winners.containsAll(listOf("pobi", "woni")))
+            },
+            MOVING_FORWARD,
+            MOVING_FORWARD,
+            STOP,
+            MOVING_FORWARD,
+            MOVING_FORWARD,
+            STOP
+        )
     }
 
     override fun runMain() {
