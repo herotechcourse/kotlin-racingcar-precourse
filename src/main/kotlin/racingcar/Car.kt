6d36be3f76eec3a954f486bcae31f7e0006880a6
minjung <minjung0768@gmail.com>
2025-04-19 03:23:25 +0900

    refactor(car): change to inject random number generator into Car for better testability
    - Modified Car class to accept a random number generator function as a constructor parameter
    - Improves testability by allowing deterministic behavior in tests

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,23 +1,25 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms
-
 /**
  * Represents a car in the racing game.
  *
  * @property name the name of the car
  * @property movement the number of moves the car has made
+ * @property randomNumberGenerator function that returns a random number (used for movement decision)
  */
-class Car(val name: String) {
+class Car(
+    val name: String,
+    private val randomNumberGenerator: () -> Int = { camp.nextstep.edu.missionutils.Randoms.pickNumberInRange(0, 9) }
+) {
     var movement = 0
 
     /**
-     * Moves the car forward if a randomly generated number between 0 and 9 is 4 or greater.
+     * Moves the car forward if a generated number is 4 or greater.
      * This simulates the car's ability to move based on chance.
      */
     fun moveIfPossible() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
-            movement++;
+        if (randomNumberGenerator() >= 4) {
+            movement++
         }
     }
 }
