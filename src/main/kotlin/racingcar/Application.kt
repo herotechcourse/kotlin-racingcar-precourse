c2ddbed580c75f46847fbdb468a3a78571427cf5
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-18 20:55:54 +0200

    feat(input): parse car names and number of rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars(comma-separated):")
+    val input = readLine() ?:""
+    val splitNames = input.split(",", ignoreCase = true, limit = 0) //delimiter can be "," or ", "
+    val names = ArrayList<String>()
+
+    println("How many rounds will be played?")
+    val numberOfRounds :Int = readLine()?.toInt() ?: 0
+    println(numberOfRounds)
+
+    for(i in splitNames.indices){
+        val carName = splitNames[i].removeWhitespaces()
+        names.add(carName)
+    }
+    println(names)
 }
+
+//Function to remove Whitespaces
+fun String.removeWhitespaces() = replace(" ", "")
