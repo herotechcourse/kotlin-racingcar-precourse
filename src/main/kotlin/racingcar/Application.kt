c5e1a0d3000bcb17ae8c223980c76b5472fe310a
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-04-18 09:45:25 +0900

    feat(racingcar): implement main function

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,20 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    Output.printCarNameInputMessage()
+    val carNames = Input.readNamesOfTheCars()
+
+    Output.printRoundInputMessage()
+    val round = Input.readNumberOfRounds()
+
+    var cars = carNames.map { Car(it) }
+
+    Output.printRaceStartMessage()
+    repeat(round) {
+        cars = Game.playOneRound(cars)
+        Output.printProgress(cars)
+    }
+
+    val winners = Game.getWinners(cars)
+    Output.printWinners(winners)
 }
