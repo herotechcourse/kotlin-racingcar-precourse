2f4e88842752585c2248af9616fec6f4d43dddcc
YongLeeCode <yonghwan311@gmail.com>
2025-04-18 10:34:32 +0900

    feat: argue the classes in Application

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,10 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    val nameInput: String = Input().input()
+    val names: List<String> = Input().getNames(nameInput)
+    val roundInput: String = Input().input()
+    val round: Int = Input().getRound(roundInput)
+
+    RacingService(names, round).play()
 }
