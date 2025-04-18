7c4dacbe399f0b4bc936a113bc820d550108de4f
ivosam1212 <isam-alv@student.42berlin.de>
2025-04-18 19:43:38 +0200

    feat: add car names validations
    - add validateCarNames function to check if the string is empty and to call validateEachCar
    - add validateEachCar to call other validations per car
    - add validateSpaces to check if there are no spaces in the car name
    - add validateEmpty to check if the name is empty
    - add validateNameLength to check is the car name is no longer than 5 chars

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -9,16 +9,51 @@ fun main() {
             append("\n- Use less than five characters per car name.")
         },)
     val carNames = Console.readLine()
-    // To do: Call a function that validates the input.
+    //Separates car names
+    val separatedCarNames = carSeparator(carNames)
+    // To test the carSeparator function
+    //   println(separatedCarNames)
+
+    //Run validations
+    validateCarNames(separatedCarNames)
     println("Enter the number of rounds to race:")
     val numberOfRounds = Console.readLine()
 
-    // To test the carSeparator function
-    //   val separatedCarNames = carSeparator(carNames)
-    //   println(separatedCarNames)
+
 }
 
-fun carSeparator(carNames: String): List<String> {
+private fun carSeparator(carNames: String): List<String> {
     val listOfCars = carNames.split(",").map { it.trim() }
     return listOfCars
+}
+
+private fun validateCarNames(separatedCarNames: List<String>) {
+    if (separatedCarNames.isEmpty()) {
+        throw IllegalArgumentException("Please provide the car names")
+    }
+    validateEachCar(separatedCarNames)
+}
+
+private fun validateEachCar(separatedCarNames: List<String>) {
+    separatedCarNames.forEach { name ->
+        validateSpaces(name)
+        validateEmpty(name)
+        validateNameLength(name)
+    }
+}
+
+private fun validateSpaces(str: String): Boolean {
+    return !str.any { it.isWhitespace() }
+}
+
+private fun validateEmpty(name: String) {
+    if (name.isEmpty()) {
+        throw IllegalArgumentException("You didn't provide a car name.")
+    }
+}
+
+private fun validateNameLength(name: String) {
+    if (name.length > 5) {
+        throw IllegalArgumentException("The car '$name' is longer than 5 characters.")
+    }
 }
