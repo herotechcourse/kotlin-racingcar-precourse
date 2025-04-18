637b3a018f49324a9e7d27f18f569dd329d55a2c
Giulia Galizoni <giuliagalizoni@gmail.com>
2025-04-18 10:38:58 +0200

    feat(game-logic): evaluate final results
    add getWinners function to check the highest position achieved and which cars are in the lead

--- src/main/kotlin/racingcar/GameLogic.kt
+++ src/main/kotlin/racingcar/GameLogic.kt
@@ -6,6 +6,12 @@ fun moveForward(): Boolean {
     return n >= 4
 }
 
+fun getWinners(cars: MutableMap<String, String>): Map<String, String> {
+    val highestPosition = cars.values.maxOfOrNull { it.length } ?: 0
+    val winners = cars.filter { it.value.length == highestPosition }
+    return winners
+}
+
 fun runRound(cars: MutableMap<String, String>) {
     for (car in cars) {
         if (moveForward()) {
