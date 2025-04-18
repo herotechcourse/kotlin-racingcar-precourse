6c9512d458f9f7e8b57e26978149863e29d70a8a
rinarina0429 <forrina0429@gmail.com>
2025-04-18 21:18:41 +0900

    feat(model): implement race execution logic in Race class
    - Add runRace function to execute the race for the given number of rounds
    - Add runEachRound to iterate over all cars and move them based on random value
    - Add moveCarsRandomly to update car position if random value >= 4
    - Add saveRoundResult to snapshot car positions after each round
    - Store all round results in a mutable list of CarPosition snapshots

--- src/main/kotlin/racingcar/model/Race.kt
+++ src/main/kotlin/racingcar/model/Race.kt
@@ -1,8 +1,33 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Race(
     var cars: List<Car>,
     var totalRound: Int,
-    var results: List<CarPosition> = emptyList(),
+    var results: MutableList<List<CarPosition>> = mutableListOf(),
 ) {
+    fun runRace() {
+        for (i in 1..totalRound) {
+            runEachRound()
+            saveRoundResult()
+        }
+    }
+
+    private fun runEachRound() {
+        for (i in cars.indices) {
+            moveCarsRandomly(cars[i])
+        }
+    }
+
+    private fun moveCarsRandomly(car: Car) {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            car.position++
+        }
+    }
+
+    private fun saveRoundResult() {
+        val roundSnapshot = cars.map { car -> CarPosition(car, car.position) }
+        results.add(roundSnapshot)
+    }
 }
