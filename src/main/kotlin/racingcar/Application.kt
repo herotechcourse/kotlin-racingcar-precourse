f61b1593e0c2f968248fe7188d868db0aab326e2
이민경(Minkyung Lee) <87940552+Minlee01@users.noreply.github.com>
2025-04-19 01:45:39 +0900

    [feat] Input Car Names

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,26 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     // TODO: Implement the program
+    val carNames = readNames
+    
+}
+
+fun readNames() : List<String> {
+    println("Enter the names of the cars (comma-seperated): ")
+    val input = Console.readLine()
+    val names = input.split(",").map {it.trim() }
+
+    if(names.isEmpty()) {
+        throw IllegalArgumentException("car names need to be entered")
+    }
+    names.forEach {
+        if(it.isEmpty() || it.length > 5){
+            throw IllegalArgumentException("each car names must be 1 to 5 characters long")
+        }
+    }
+    return names;
 }
