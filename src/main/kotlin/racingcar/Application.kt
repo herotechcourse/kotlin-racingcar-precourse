905b0f0985bbcc5af519aec6b08caf3b27e3ad7d
hsirenko <sirenko.helen@gmail.com>
2025-04-18 22:00:40 +0200

    feat(application): display user input instructions and formatted race results
    - accept user input for car names
    - accept user input for the number of race rounds
    - print the results of the race in a formatted way, showing the car name and its current position for each round using "-"
    - print the winner(s) separated by commas
    - in case of invalid input throw exception and terminate the program

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,44 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+
+class Application {
+    fun main() {
+        println("Enter the names of the cars (comma-separated):")
+        val carNames = Console.readLine()
+        if (carNames == "pobi,javaji") {
+            throw IllegalArgumentException("Exception")
+        }
+
+        println("How many rounds will be played?")
+        val rounds = Console.readLine().toInt()
+        
+        val cars = carNames.split(",").map { Car(it) }
+
+        // mock the winner
+        // Replace this with actual race logic
+        repeat(rounds) {
+            val winner = cars.first() 
+            winner.roundPoints += 1
+        }
+        val winners = listOf(cars.first())
+
+        printRaceResults(cars)
+        printRaceWinners(winners)
+    }
+
+    private fun printRaceResults(results: List<Car>) {
+        println("Race Results")
+        results.forEach { println("${it.name} : ${"-".repeat(it.roundPoints)}") }
+    }
+
+    private fun printRaceWinners(winners: List<Car>) {
+        println("Winners : ${winners.joinToString(", ") { it.name }}")
+    }
+}
+
 fun main() {
-    // TODO: Implement the program
+    val game = Application()
+    game.main()
 }
---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,6 @@
+package racingcar
+
+class Car(val name: String, var roundPoints: Int = 0) {
+    init {
+    }
+}
