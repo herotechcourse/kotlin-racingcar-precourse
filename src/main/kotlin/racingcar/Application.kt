61f9335084d9f1a60eb49fa064f48742fd833921
haeun <jhe0903@naver.com>
2025-04-18 14:24:07 +0900

    feat(input): accept names from user

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
 
-fun main() {
-    // TODO: Implement the program
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
+
+fun inputCars(): List<String> {
+    println("Enter the names of the cars (comma-seperated):");
+    // Split the input string by commas
+    val cars = Console.readLine().split(",");
+    return cars;
 }
+
+fun main() {
+    val cars = inputCars();
+}
+
+
