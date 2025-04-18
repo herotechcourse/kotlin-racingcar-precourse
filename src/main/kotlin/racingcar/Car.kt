a2b07d30f6e0e22d0916e7495b36b515213d6754
yeonupark <idepix@naver.com>
2025-04-18 14:21:47 +0900

    feat(racingcar): add conditional driving logic depending on random number

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,4 +2,10 @@ package racingcar
 
 data class Car(val name: String, var distance: Int = 0) {
 
+    fun moveIfPossible(num: Int) {
+        if (num >= 4) {
+            distance += 1
+        }
+    }
+
 }
