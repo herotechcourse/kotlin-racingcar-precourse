2a81785dfdb5207f24a36563946ae5d25f1807d0
LisaW <LisaW@ya.ru>
2025-04-18 13:19:14 +0300

    feat: validate car input
    test: empty car's name prompt test, result: Exception.
    test: car's names prompt separated by ' ' test, result: Exception.
    test: car's names length > 5 test, result: Exception

--- src/test/kotlin/racingcar/InputValidationTest.kt
+++ src/test/kotlin/racingcar/InputValidationTest.kt
@@ -1,4 +1,33 @@
 package racingcar
 
-class InputValidationTest {
+import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class InputValidationTest : NsTest() {
+    @Test
+    fun `empty car's name prompt test`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("", "1") }
+        }
+    }
+
+    @Test
+    fun `car's names prompt separated by ' ' test`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("car1 car2", "1") }
+        }
+    }
+
+    @Test
+    fun `car's name length longer then 5 test`() {
+        assertSimpleTest {
+            assertThrows<IllegalArgumentException> { runException("car123 car2", "1") }
+        }
+    }
+
+    override fun runMain() {
+        main()
+    }
 }
