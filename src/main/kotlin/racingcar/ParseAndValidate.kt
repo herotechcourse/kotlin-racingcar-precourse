731373ef8d1e5d0cd897a34c624578a81219d711
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-18 12:03:56 +0200

    style(input): make if statement in one line

--- src/main/kotlin/racingcar/ParseAndValidate.kt
+++ src/main/kotlin/racingcar/ParseAndValidate.kt
@@ -2,15 +2,11 @@ package racingcar
 
 object ParseAndValidate {
     private fun checkLength(input: String) {
-        if (input.length > 5) {
-            throw IllegalArgumentException("Length exceeds 5")
-        }
+        if (input.length > 5)  throw IllegalArgumentException("Length exceeds 5")
     }
 
     private fun checkEmpty(input: String) {
-        if (input.isEmpty()) {
-                throw IllegalArgumentException("String is empty")
-        }
+        if (input.isEmpty()) throw IllegalArgumentException("String is empty")
     }
 
     private fun requireValidName(name: String) {
