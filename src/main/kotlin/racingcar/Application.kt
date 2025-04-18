8aa5ba32a473dc0051b8ce9ed5ae005f94bcdd69
Zaid <dagbanbia@gmail.com>
2025-04-18 10:20:17 +0200

    feat: validate input cars names

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -30,9 +30,23 @@ object CarRacingGame{
     private fun inputCars(): List<Car> {
         println("Enter the names of the cars to race , seperated by commas : ")
         val names = Console.readLine().split(",").map { it.trim() }
-        println(names) // list of cars name
+        //println(names) // list of cars name
+        validateCarNames(names)
         return names.map{Car (it)}
+
     }
-    
+
+    private fun validateCarNames(names:List<String>){
+        names.forEach{
+            if(it.isEmpty() || it.length > MAX_NAME_LENGTH){
+                throw IllegalArgumentException("Car names must be between 1 and 5 characters long")
+            }
+        }
+    }
+
+
+
+
+
 
 }
