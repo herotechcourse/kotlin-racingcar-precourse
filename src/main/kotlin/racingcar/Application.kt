be5ea49c8672ad57031371f35e9218906edbb694
anshu2323 <abshahi2311@icloud.com>
2025-04-18 15:22:12 +0200

    1st commit

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,19 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars (comma-separated):")
+    val carNamesInput = Console.readLine()
+    val carNames = carNamesInput.split(",").map { it.trim() }
+
+    validateCarNames(carNames)
+
+    println("How many rounds will be played?")
+    val roundInput = Console.readLine()
+    val rounds = validateRoundCount(roundInput)
+
+    val cars = carNames.map { Car(it) }
+    val race = Race(cars, rounds)
+    race.start()
 }
