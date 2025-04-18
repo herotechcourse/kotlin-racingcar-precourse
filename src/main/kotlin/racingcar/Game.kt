23bc7b14d466106e503a00af8144b55487c1edf2
hyunjuntyler <hyunjun1994@hanyang.ac.kr>
2025-04-18 11:25:24 +0900

    feat(racingcar): implement isMovable function

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -3,16 +3,13 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 object Game {
-    fun playOneRound(cars: List<Car>): List<Car> {
-        val moveThreshold = 4
-        val minRandom = 0
-        val maxRandom = 9
+    private const val MOVE_THRESHOLD = 4
+    private const val MIN_RANDOM = 0
+    private const val MAX_RANDOM = 9
 
-        return cars.map {
-            val randomNumber = Randoms.pickNumberInRange(minRandom, maxRandom)
-            if (randomNumber >= moveThreshold) it.movedForward() else it
-        }
-    }
+    fun playOneRound(cars: List<Car>): List<Car> = cars.map { if (isMovable()) it.movedForward() else it }
 
     fun getWinners(cars: List<Car>): List<Car> = cars.filter { it.position == cars.maxOf { it.position } }
+
+    fun isMovable(): Boolean = Randoms.pickNumberInRange(MIN_RANDOM, MAX_RANDOM) >= MOVE_THRESHOLD
 }
