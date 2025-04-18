40634cfdb11b7d860d8c740f6866463afbfaa475
gokcesen <sengkce@gmail.com>
2025-04-18 15:37:13 +0200

    feat(car): add Car class with properties and methods

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,19 @@
+package racingcar
+
+class Car(val carName: String) {
+    private var position: Int = 0
+
+    fun getName(): String {
+        return this.carName
+    }
+
+    fun getPosition(): Int{
+        return this.position
+    }
+
+    fun moveForward(randomNumber: Int) {
+        if (randomNumber in 4..9){
+            position+=1
+        }
+    }
+}
