9dc04ba2f511f5ea7eed35cc7863482bdb7dc887
Emir Risyad <emirrsyd@gmail.com>
2025-04-18 19:38:14 +0200

    feat(RaceGame): implement RaceGame class to manage racing game logic

---
+++ src/main/kotlin/racingcar/model/RaceGame.kt
@@ -0,0 +1,33 @@
+package racingcar.model
+
+import racingcar.util.RandomNumberGenerator
+
+class RaceGame(private val cars: List<Car>, private var rounds: Int) {
+    private var currentRound: Int = 0
+    fun startRace() {
+        while (!isRaceEnd()) {
+            startRound()
+        }
+    }
+
+    private fun startRound() {
+        cars.forEach { car ->
+            if (canMoveCar())
+                car.move()
+        }
+        currentRound++
+    }
+
+    private fun canMoveCar(): Boolean = RandomNumberGenerator.generateRandomNumber() > MIN_NUMBER_TO_MOVE_CAR
+
+    private fun isRaceEnd(): Boolean = currentRound >= rounds
+
+    fun getWinners(): List<Car> {
+        val maxPosition = cars.maxOf { it.position }
+        return cars.filter { it.position == maxPosition }
+    }
+
+    companion object {
+        const val MIN_NUMBER_TO_MOVE_CAR = 4
+    }
+}
