07bc77e1de28d51db107bc8e5c044a6519939ad3
prithvy <prithvykrishna67@gmail.com>
2025-04-18 19:10:26 +0200

    feat: implement race game with final results

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -1,12 +1,16 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms;
+import camp.nextstep.edu.missionutils.Randoms
 
 data class Input(val cars: List<Car>, var totalRounds: Int) {
     init {
-        require(cars.size == 0) {"cars cannot be empty"}
-        require(totalRounds <= 0) {"total rounds should be at-least one"}
+        require(cars.size > 0) { "cars cannot be empty" }
+        require(totalRounds > 0) { "total rounds should be at-least one" }
     }
+}
+
+interface RandomNumberGenerator {
+    fun pickNumberInRange(startInclusive: Int, endInclusive: Int): Int
 }
 
 data class Car(val name: String, var position: Int = 0) {
@@ -14,9 +18,8 @@ data class Car(val name: String, var position: Int = 0) {
         require(name.length <= 5) { "name cannot exceed 5 characters: $name" }
     }
 
-    fun move(): Car {
-        val randomMove = Randoms.pickNumberInRange(0, 9)
-        if(randomMove >= 4) {
+    fun move(movement: Int): Car {
+        if (movement >= 4) {
             position++
         }
         return this
@@ -25,5 +28,33 @@ data class Car(val name: String, var position: Int = 0) {
     fun displayProgress(): Car {
         println("$name : ${"-".repeat(position)}")
         return this
+    }
+}
+
+data class Game(val input: Input) {
+    companion object {
+        const val RANDOM_MIN_RANGE = 0
+        const val RANDOM_MAX_RANGE = 9
+    }
+
+    fun run(customRandomGenerator: RandomNumberGenerator? = null) {
+        println()
+        println("Race Results")
+        repeat(input.totalRounds) {
+            val randomMove =
+                    customRandomGenerator?.pickNumberInRange(RANDOM_MIN_RANGE, RANDOM_MAX_RANGE)
+                            ?: Randoms.pickNumberInRange(RANDOM_MIN_RANGE, RANDOM_MAX_RANGE)
+
+            input.cars.forEach { car -> car.move(randomMove).displayProgress() }
+            println()
+        }
+        println()
+        displayWinners()
+    }
+
+    fun displayWinners() {
+        val maxPosition = input.cars.maxOf { it.position }
+        val winners = input.cars.filter { it.position == maxPosition }
+        println("Winners : ${winners.joinToString(", ") {it.name}}")
     }
 }
--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -4,20 +4,21 @@ import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeT
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Disabled
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
 class ApplicationTest : NsTest() {
-    @Test @Disabled
+    @Test
+    @Disabled
     fun `feature test`() {
         assertRandomNumberInRangeTest(
-            {
-                run("pobi,woni", "1")
-                assertThat(output()).contains("pobi : -", "woni : ", "Winners : pobi")
-            },
-            MOVING_FORWARD,
-            STOP,
+                {
+                    run("pobi,woni", "1")
+                    assertThat(output()).contains("pobi : -", "woni : ", "Winners : pobi")
+                },
+                MOVING_FORWARD,
+                STOP,
         )
     }
 
@@ -37,8 +38,41 @@ class ApplicationTest : NsTest() {
 
     @Test
     fun `test car movement`() {
-        Car("pobi", 0).move().displayProgress()
-        assertThat(output()).contains("pobi : -")
+        var car =
+                Car(
+                        "pobi",
+                        0,
+                )
+        car.move(1).move(2).move(3).move(4).move(5).move(6).displayProgress()
+        assertThat(output()).contains("pobi : --")
+    }
+
+    @Test
+    fun `test a simple race game (always moving)`() {
+        val alwaysMoveRandomGenerator: RandomNumberGenerator =
+                object : RandomNumberGenerator {
+                    override fun pickNumberInRange(startInclusive: Int, endInclusive: Int): Int {
+                        return MOVING_FORWARD
+                    }
+                }
+        val gameCars = listOf(Car("pobi", 0), Car("woni", 0))
+        val gameInput = Input(gameCars, 1)
+        Game(gameInput).run(alwaysMoveRandomGenerator)
+        assertThat(output()).contains("pobi : -", "woni : -", "Winners : pobi, woni")
+    }
+
+    @Test
+    fun `test a simple race game (always stop)`() {
+        val alwaysMoveRandomGenerator: RandomNumberGenerator =
+                object : RandomNumberGenerator {
+                    override fun pickNumberInRange(startInclusive: Int, endInclusive: Int): Int {
+                        return MOVING_FORWARD
+                    }
+                }
+        val gameCars = listOf(Car("pobi", 0), Car("woni", 0))
+        val gameInput = Input(gameCars, 1)
+        Game(gameInput).run(alwaysMoveRandomGenerator)
+        assertThat(output()).contains("pobi : ", "woni : ", "Winners : pobi, woni")
     }
 
     override fun runMain() {
