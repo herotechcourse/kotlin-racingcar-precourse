4f80ea98708b09284151063a91aaede15b1f4382
sominyun <somin455@naver.com>
2025-04-18 23:54:50 +0900

    feat: If a car name exceeds 5 characters, throw an IllegalArgumentException

--- src/main/kotlin/racingcar/backend/domain/validator/RaceNameValidator.kt
+++ src/main/kotlin/racingcar/backend/domain/validator/RaceNameValidator.kt
@@ -8,4 +8,10 @@ object RaceNameValidator {
             throw IllegalArgumentException(RacingError.CAR_NAME_BLANK.message)
         }
     }
+
+    fun validateCarNameLength(carName: String) {
+        if (carName.length > 5) {
+            throw IllegalArgumentException(RacingError.CAR_NAME_TOO_LONG.message)
+        }
+    }
 }
--- src/main/kotlin/racingcar/backend/exception/RacingError.kt
+++ src/main/kotlin/racingcar/backend/exception/RacingError.kt
@@ -2,5 +2,6 @@ package racingcar.backend.exception
 
 enum class RacingError(val message: String) {
     CAR_NAME_BLANK("Each car must have a name."),
+    CAR_NAME_TOO_LONG("Car name cannot exceed 5 characters."),
     ;
 }
