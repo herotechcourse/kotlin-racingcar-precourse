f19b69157e7fb7402465b09c53c33b3fe21fb534
wjunseok <wminsoo1@naver.com>
2025-04-18 09:58:48 +0900

    fix(racinggame): remove duplicate iteration causing extra round
    Removed redundant loop that caused the game to run one extra round.

--- src/main/kotlin/racingcar/RacingGame.kt
+++ src/main/kotlin/racingcar/RacingGame.kt
@@ -2,23 +2,17 @@ package racingcar
 
 import racingcar.generator.RandomNumberGenerator
 
-class RacingGame(val cars: List<Car>, val round: Int) {
+class RacingGame(val cars: List<Car>) {
 
     fun startGame(randomGenerator: RandomNumberGenerator) {
-        repeat(round) {
-            playOneRound(randomGenerator)
+        for (car in cars) {
+            car.move(randomGenerator)
         }
     }
 
     fun findWinner() : List<Car> {
         val max = cars.maxOf { car -> car.position }
         return cars.filter { car -> car.position == max }
-    }
-
-    private fun playOneRound(randomGenerator: RandomNumberGenerator) {
-        for (car in cars) {
-            car.move(randomGenerator)
-        }
     }
 
 }
