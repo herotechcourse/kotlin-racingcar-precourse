2ab4b65e087623971d8b2852f2a06f8a98102f3d
Sanghyunn <shawnpark98@naver.com>
2025-04-18 23:09:25 +0900

    feat: Implement InputView

---
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -0,0 +1,35 @@
+package view
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputView {
+    fun readCarNames(): List<String> {
+        println("Enter the names of the cars (comma-separated):")
+        val input = Console.readLine()
+
+        val carNames = input.split(",")
+
+        carNames.forEach { name ->
+            require(name.isNotBlank() && name.length <= 5) {
+                "Car must have a name, and names cannot exceed 5 characters."
+            }
+        }
+
+        return carNames
+    }
+
+    fun readNumberOfRounds(): Int {
+        println("How many rounds will be played?")
+        val input = Console.readLine()
+
+        val rounds = try {
+            input.toInt()
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("Please enter a valid number.")
+        }
+
+        require(rounds > 0) { "Rounds should be positive integer." }
+
+        return rounds
+    }
+}
