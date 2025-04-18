d27d495a0382783bc8e65cdb68788a5774f8434b
phan <hanfeelhoo@naver.com>
2025-04-18 11:08:48 +0900

    style(Car) : remove not used const val

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -3,9 +3,8 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
 const val MOVE_THRESHOLD: Int = 4
-const val PROGRESS_BAR: String = "-"
 
-class Car (val name: String, var pos: Int = 0) {
+class Car (private val name: String, private var pos: Int = 0) {
     private fun rollDice(): Int {
         return Randoms.pickNumberInRange(0, 9)
     }
