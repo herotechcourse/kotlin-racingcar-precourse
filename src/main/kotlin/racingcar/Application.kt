e4ef55ff9bda956797a0a67bc5c44441f95cda84
Pavel Pukha <ppukha@gmail.com>
2025-04-18 16:15:23 +0200

    feat(app): implement Application main logic for race input and execution

---
+++ src/main/kotlin/racingcar/Application.kt
@@ -0,0 +1,20 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+fun main() {
+
+    print(" Enter the names of the cars (comma-separated):")
+    val carNamesInput = Console.readLine()
+
+    print (" How many rounds will be played?")
+    val roundsInput = Console.readLine()
+
+    val race = RaceInputHandler.createRace(carsNamesInput, roundsInput)
+    val raceResult = race.start()
+
+    println("\nRace Results")
+    raceResult.printProgress()
+    raceResult.printWinners()
+
+}
