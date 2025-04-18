9c9d5417b54db560c5fe7c4701184d928a92ef42
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-18 11:49:21 +0200

    feat(input): add rounds input
    parse and validate rounds input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -6,4 +6,8 @@ fun main() {
     println("Enter the names of the cars (comma-separated):")
     val carNamesInput = Console.readLine()
     val carNames: List<String> = ParseAndValidate.parseAndValidateNames(carNamesInput)
+    println("How many rounds will be played?")
+    val roundsInput = Console.readLine()
+    val rounds = ParseAndValidate.parseAndValidateRounds(roundsInput)
+
 }
--- src/main/kotlin/racingcar/ParseAndValidate.kt
+++ src/main/kotlin/racingcar/ParseAndValidate.kt
@@ -18,11 +18,25 @@ object ParseAndValidate {
         checkLength(name)
     }
 
+    private fun checkInt(input: String) : Int {
+        return input.toIntOrNull(10) ?: throw IllegalArgumentException("Input must be number")
+    }
+
+    private fun checkIntValue(value: Int) {
+        if (value < 1) throw IllegalArgumentException("Number must be greater than 0")
+    }
+
     fun parseAndValidateNames(input: String): List<String> {
         checkEmpty(input)
         return input
             .split(",")
             .map { it.trim() }
             .onEach  { requireValidName(it) }
+    }
+
+    fun parseAndValidateRounds(input: String): Int {
+        val value = checkInt(input)
+        checkIntValue(value)
+        return value
     }
 }
