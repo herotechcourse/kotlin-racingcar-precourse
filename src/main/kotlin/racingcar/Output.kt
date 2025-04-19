4123a7c2c2f51378dfffb44cb74999a4b09f7067
YuGyeong98 <kuk3097@naver.com>
2025-04-20 00:59:19 +0900

    feat(Output): add get car movement characters, print car race result function

---
+++ src/main/kotlin/racingcar/Output.kt
@@ -0,0 +1,22 @@
+package racingcar
+
+class Output(private val round: Round) {
+
+    fun printCarRaceResult(carNames: MutableMap<String, Int>) {
+        carNames.forEach { (carName, movementCount) ->
+            round.moveCarForward(carNames, carName)
+            println("$carName : ${getCarMovementCharacters(carNames, carName, movementCount)}")
+        }
+        println()
+    }
+
+    private fun getCarMovementCharacters(
+        carNames: MutableMap<String, Int>,
+        carName: String,
+        movementCount: Int,
+    ) = MOVEMENT_CHARACTER.repeat(carNames.getOrDefault(carName, movementCount))
+
+    companion object {
+        private const val MOVEMENT_CHARACTER = "-"
+    }
+}
