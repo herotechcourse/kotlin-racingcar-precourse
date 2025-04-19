daf638c4fcd6325b4e24a2603b2f0520e80a0b1d
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-19 17:13:43 +0900

    feat: add number format validation to NumberValidator

--- src/main/kotlin/racingcar/validation/NumberValidator.kt
+++ src/main/kotlin/racingcar/validation/NumberValidator.kt
@@ -1,5 +1,14 @@
 package racingcar.validation
 
+internal const val INVALID_NUMBER_FORMAT_MSG = "Invalid Number Format."
+internal const val INVALID_NATURAL_NUMBER_MSG = "Number must be a natural number."
+
+
+fun validateNumberFormat(input: String) {
+    if (input.toIntOrNull() == null)
+        throw IllegalArgumentException(INVALID_NUMBER_FORMAT_MSG);
+}
 fun validateNatural(num: Int) {
-    if (num < 1) throw IllegalArgumentException("Number must be a natural number.");
+    if (num < 1)
+        throw IllegalArgumentException(INVALID_NATURAL_NUMBER_MSG);
 }
