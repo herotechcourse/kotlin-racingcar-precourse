9d8ab75413e3a595b971a2ed11ebc359da37982a
Emir Risyad <emirrsyd@gmail.com>
2025-04-19 16:39:49 +0200

    feat(OutputView): implement OutputView to show the race result

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,16 @@
+package racingcar.view
+
+import racingcar.model.Car
+
+object OutputView {
+    fun showRoundResult(cars: List<Car>) {
+        cars.forEach { car ->
+            println(car.displayProgress())
+        }
+        println("\n")
+    }
+
+    fun showWinners(carNames: List<String>) {
+        println("Winners: ${carNames.joinToString(", ")}")
+    }
+}
