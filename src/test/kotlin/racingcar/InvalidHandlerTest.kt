b0f59455810757debcd4c033cba74c731ee9b500
hiyoons <arin9825@naver.com>
2025-04-19 23:18:29 +0900

    test: add InvalidInputTest
    Ensured IllegalArgumentException is thrown
    in edge cases by using assertThrows

---
+++ src/test/kotlin/racingcar/InvalidHandlerTest.kt
@@ -0,0 +1,57 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.test.NsTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class InvalidHandlerTest {
+
+    val invalidHandler=InvalidHandler()
+
+    @Test
+    @DisplayName("Exception: Car names are not separated by commas")
+    fun `handleCommas test`(){
+        val input="pobiwonijun"
+        assertThrows<IllegalArgumentException> {
+            invalidHandler.handleCommas(input)
+        }
+    }
+
+    @Test
+    @DisplayName("Exception: Car name's length exceeds 5 Characters")
+    fun `handleNameLength test`(){
+        val data= listOf("akrlnkeri","rnkg","car")
+        assertThrows<IllegalArgumentException>{
+            invalidHandler.handleNameLength(data)
+        }
+    }
+
+    @Test
+    @DisplayName("Exception: Car name doesn't exceed 1 Character")
+    fun `handleNameEmpty Test`(){
+        val data= listOf("","pobi","woni","jun")
+        assertThrows<IllegalArgumentException>{
+            invalidHandler.handleNameEmpty(data)
+        }
+    }
+
+    @Test
+    @DisplayName("Exception: Duplicated Name Exist")
+    fun `duplicatedName Test` (){
+        val data= listOf("pobi","pobi","woni","woni","jun","jun")
+        assertThrows<IllegalArgumentException>{
+            invalidHandler.duplicatedName(data)
+        }
+    }
+
+    @Test
+    @DisplayName("Exception: Second input can't covert String into Number")
+    fun `convertInt Test` (){
+        val data="F1"
+        assertThrows<IllegalArgumentException>{
+            invalidHandler.convertInt(data)
+        }
+    }
+}
