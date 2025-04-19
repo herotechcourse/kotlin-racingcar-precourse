a40fdc789b45028b7a240560e702c46399819adb
sunishae <sunishae44@gmail.com>
2025-04-20 01:49:36 +0900

    test: add unit tests for Car name validation

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,35 @@
+package racingcar.model
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class CarTest {
+
+    @Test
+    fun `create car with valid name`() {
+        val car = Car("pobi")
+        assertThat(car.name).isEqualTo("pobi")
+    }
+
+    @Test
+    fun `empty name throws exception`() {
+        assertThrows<IllegalArgumentException> {
+            Car("")
+        }
+    }
+
+    @Test
+    fun `blank name throws exception`() {
+        assertThrows<IllegalArgumentException> {
+            Car("   ")
+        }
+    }
+
+    @Test
+    fun `name longer than 5 throws exception`() {
+        assertThrows<IllegalArgumentException> {
+            Car("abcdef")
+        }
+    }
+}
