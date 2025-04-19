76c9cea7ff58911b6d0d9e3480e8d5bd07774044
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-19 20:10:49 +0200

    feat(race): extract input logic from main
    change main function according new Race class logic
    make main shorter extracting logic receiving and creating car and race instances in getCarList and getRaceInstance functions

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,21 +1,27 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.ParseAndValidate.parseAndValidateNames
+import racingcar.ParseAndValidate.parseAndValidateRounds
 
 fun main() {
+    val cars = getCarsList()
+    val race = getRaceInstance(cars)
+    println("\nRace Results")
+    race.runRace()
+    race.printResult()
+}
+
+fun getCarsList(): List<Car> {
     println("Enter the names of the cars (comma-separated):")
     val carNamesInput = Console.readLine()
-    val carNames: List<String> = ParseAndValidate.parseAndValidateNames(carNamesInput)
+    val carNames: List<String> = parseAndValidateNames(carNamesInput)
+    return carNames.map { Car(it) }
+}
+
+fun getRaceInstance(cars: List<Car>): Race {
     println("How many rounds will be played?")
     val roundsInput = Console.readLine()
-    val rounds = ParseAndValidate.parseAndValidateRounds(roundsInput)
-    val cars = carNames.map { Car(it) }
-    val race = Race(cars)
-    println("\nRace Results")
-    repeat(rounds) {
-        race.runRound()
-        race.printProgress()
-    }
-    val winners = race.getWinners()
-    println("Winners : ${winners.joinToString(", ")}")
+    val rounds = parseAndValidateRounds(roundsInput)
+    return Race(cars, rounds)
 }
