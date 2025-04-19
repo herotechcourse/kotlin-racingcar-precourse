5a9029dfd9f1803e808719f3f3d179fce7cb6b4e
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-19 20:05:43 +0200

    feat(race): extract race logic from main
    extract logic repeating rounds and print race result from main
    make logic finding winners, print round progress and run round private

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -1,20 +1,34 @@
 package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 
-class Race(private val cars: List<Car>) {
-    fun runRound() {
-        cars.forEach { car ->
-            car.move(Randoms.pickNumberInRange(0, 9) >= 4)
+class Race(
+    private val cars: List<Car>,
+    private val rounds: Int
+) {
+    fun printResult() {
+        println("Winners : ${getWinnersNames().joinToString(", ")}")
+    }
+
+    fun runRace() {
+        repeat(rounds) {
+            runRound()
+            printProgress()
         }
     }
 
-    fun getWinners(): List<String> {
-        val maxPosition = cars.maxOfOrNull { it.position } ?: 0
-        return cars.filter { it.position == maxPosition }.map { it.name }
+    private fun runRound() {
+        cars.forEach { car ->
+            if (Randoms.pickNumberInRange(0, 9) >= 4) car.move()
+        }
     }
 
-    fun printProgress() {
+    private fun printProgress() {
         cars.forEach { println(it) }
         println()
+    }
+
+    private fun getWinnersNames(): List<String> {
+        val maxPosition = cars.maxOfOrNull { it.position } ?: 0
+        return cars.filter { it.position == maxPosition }.map { it.name }
     }
 }
