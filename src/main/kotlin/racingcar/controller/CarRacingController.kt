14ed1a2c28a52b1c2d44778e905690aeaf291e20
rinarina0429 <forrina0429@gmail.com>
2025-04-20 02:02:20 +0900

    feat(controller): add validation for car names and total round inputs

--- src/main/kotlin/racingcar/controller/CarRacingController.kt
+++ src/main/kotlin/racingcar/controller/CarRacingController.kt
@@ -2,15 +2,20 @@ package racingcar.controller
 
 import racingcar.model.Car
 import racingcar.model.Race
+import racingcar.validator.CarNamesValidator
+import racingcar.validator.TotalRoundValidator
 import racingcar.view.InputView
 import racingcar.view.OutputView
 
 class CarRacingController {
     fun run() {
         val carNames = InputView.getCarNames()
-        val totalRound = InputView.getTotalRound()
+        CarNamesValidator.validate(carNames)
 
-        val race = Race(Car.fromNames(carNames), totalRound)
+        val totalRound = InputView.getTotalRound()
+        TotalRoundValidator.validate(totalRound)
+
+        val race = Race(Car.fromNames(carNames), totalRound.toInt())
         race.runRace()
 
         OutputView.showAllRoundResult(race)
