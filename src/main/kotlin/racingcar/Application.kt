e9d3c0715a761ee50835b4f5dd5a9c17f55f07a3
Sofinam <omukowus@gmail.com>
2025-04-19 20:51:58 +0200

    implemented user input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
+data class Car(val name: String, var position: Int = 0)
+
 fun main() {
-    // TODO: Implement the program
+    // validating car names
+    try{
+        println("Enter car names (comma-seperated, max 5 characters each name):")
+        val input = readLine() ?: throw IllegalArgumentException("No input provided.")
+        val names = input.split(",").map { it.trim()}
+
+        if (names.isEmpty()) throw IllegalArgumentException("You didn't provide any car names.")
+
+        val cars = names.map { name ->
+            if (name.isEmpty() || name.length > 5)
+                throw IllegalArgumentException("Invalid car name: '$name'")
+            Car(name)
+        }
+        // Validate number of rounds
+        println("Enter number of rounds:")
+        val roundsInput = readLine() ?: throw IllegalArgumentException("No input for rounds.")
+        val rounds = roundsInput.toIntOrNull() ?: throw IllegalArgumentException("Rounds must be a number.")
+        println("\nRace Start!\n")
 }
