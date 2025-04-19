f8a4439515b74bcf8d69fd1f68aae8d60ffaa3e1
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-19 22:43:55 +0200

    feat: movement logic for Car class

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,21 +1,41 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("Enter the names of the cars(comma-separated):")
-    val input = readLine() ?:""
+    val input = Console.readLine()
     val splitNames = input.split(",", ignoreCase = true, limit = 0) //delimiter can be "," or ", "
     val names = ArrayList<String>()
 
     println("How many rounds will be played?")
-    val numberOfRounds :Int = readLine()?.toInt() ?: 0
+    val numberOfRounds :Int = Console.readLine().toInt()
     println(numberOfRounds)
 
     for(i in splitNames.indices){
         val carName = splitNames[i].removeWhitespaces()
         names.add(carName)
     }
-    println(names)
+    //println(names)
 }
 
 //Function to remove Whitespaces
 fun String.removeWhitespaces() = replace(" ", "")
+
+class Car(val carName:String, val rounds:Int) {
+    private var speed :Int = 0
+    private var movement = ArrayList<Char> (rounds)
+    var displacement = 0
+
+    private fun getSpeed() {
+        speed = Randoms.pickNumberInRange(0, 10)
+    }
+
+    fun moveCar() {
+        getSpeed()
+        if (speed >= 4) {
+            movement.add('-')
+        }
+        println(movement.joinToString(""))
+    }
+}
