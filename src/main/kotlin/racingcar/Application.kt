731166701f0031501980949cbd337038bf50d36d
Jisun Park <jisunpark.t@gmail.com>
2025-04-19 11:59:09 +0900

    refactor(prompt): move user prompts to UserPrompt class

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,16 +1,9 @@
 package racingcar
 
 fun main() {
-    println("Names of the cars (comma-separated):")
-    val cars = readln().split(",").map { run {
-        require(it.trim().length <= 5) { "Each name should be up to 5 characters"}
-        val car = Car(it.trim())
-        car
-    } }
-
-    println("How many rounds will be played?")
-    val rounds = readln().toIntOrNull()
-    require(rounds != null) {"The number of rounds should be a valid number"}
+    val prompt = UserPrompt()
+    val cars = prompt.promptCarNames()
+    val rounds = prompt.promptRounds()
 
     val race = Race(cars = cars, rounds = rounds)
 
---
+++ src/main/kotlin/racingcar/UserPrompt.kt
@@ -0,0 +1,22 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+class UserPrompt {
+    fun promptCarNames(): List<Car> {
+        println("Names of the cars (comma-separated):")
+        val cars = Console.readLine().split(",").map {
+            require(it.trim().length <= 5) { "Each name should be up to 5 characters"}
+            val car = Car(it.trim())
+            car
+        }
+        return cars
+    }
+
+    fun promptRounds(): Int {
+        println("How many rounds will be played?")
+        val rounds = Console.readLine().toIntOrNull()
+        require(rounds != null) {"The number of rounds should be a valid number"}
+        return rounds
+    }
+}
