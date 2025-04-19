69ce26cd458d7456efbef6f839c25fb5882fc8f3
Mans228 <zhenismansur@gmail.com>
2025-04-19 15:15:27 +0200

    fix(car): car can not be tested cause of the random number in move function

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -3,7 +3,11 @@ package racingcar.domain
 import camp.nextstep.edu.missionutils.Randoms
 import kotlin.random.Random
 
-data class Car(val name: String, var position: Int = 0) {
+data class Car(
+    val name: String,
+    var position: Int = 0,
+    private val randomPick: () -> Int = { Randoms.pickNumberInRange(0, 9) }
+) {
     init {
         require(name.isNotBlank() && name.length <= 5) {
             "Car name must be 1-5 characters long"
@@ -11,7 +15,7 @@ data class Car(val name: String, var position: Int = 0) {
     }
 
     fun move() {
-        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+        if (randomPick() >= 4) {
             position++
         }
     }
