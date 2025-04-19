848ae6fb8b7bb6f94d86765e14b34d854f4c890a
YongLeeCode <yonghwan311@gmail.com>
2025-04-19 21:24:51 +0900

    refactor: replace magic number with companion object

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -16,7 +16,7 @@ class CarTest {
     @DisplayName("Move foward when random value is equal or greater than 4")
     fun moveForwardTest() {
         val car = Car("pobi")
-        car.move(5)
+        car.move(MOVING_FORWARD)
         assertThat(car.getPositionBar()).isEqualTo("-")
     }
 
@@ -24,8 +24,13 @@ class CarTest {
     @DisplayName("Stay when random value is less than 4")
     fun stayTest() {
         val car = Car("pobi")
-        car.move(3)
+        car.move(STOP)
         assertThat(car.getPositionBar()).isEqualTo("")
+    }
+
+    companion object {
+        private const val MOVING_FORWARD: Int = 4
+        private const val STOP: Int = 3
     }
 }
 
