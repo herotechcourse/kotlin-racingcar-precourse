95bbfed485e98d5c272757ef8f6040bf39174d9b
Tebah(Sungkyum Kim) <sungkyum1@gmail.com>
2025-04-19 15:28:58 +0900

    refactor(domain): optimize round results collection in Race class
    - Changed the implementation of roundResults from ArrayList to mutableListOf for better performance and readability.
    - Replaced the for loop with repeat function to simplify the code structure.

--- src/main/kotlin/racingcar/domain/Race.kt
+++ src/main/kotlin/racingcar/domain/Race.kt
@@ -2,8 +2,8 @@ package racingcar.domain
 
 class Race(private val cars: Cars, private val round: Int) {
     fun start(): RaceResults {
-        val roundResults = ArrayList<RoundResult>()
-        for (i: Int in 1..round) {
+        val roundResults = mutableListOf<RoundResult>()
+        repeat(round) {
             cars.move()
             roundResults.add(RoundResult(cars.getCarStatuses()))
         }
