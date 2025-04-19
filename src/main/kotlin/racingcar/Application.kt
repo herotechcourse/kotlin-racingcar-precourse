ef16e7153ff76b0bb45d75cf65c3d057776d3dea
Arman Ebrahimi <armanebrhm@gmail.com>
2025-04-19 16:49:48 +0200

    feat:implement user input for cars name and rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,31 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: Implement the program
+    getUserName()
+    getRoundNumber()
+}
+fun getUserName():List<String>{
+    println("Enter the names of the cars (comma-separated):")
+    val userInput = Console.readLine()
+    var carsName = userInput.split(",")
+    carsName.forEach{
+        if (it.length>5)
+            throw IllegalArgumentException("names cannot exceed 5 characters!")
+        if (it.isBlank())
+            throw IllegalArgumentException("each car needs a name!")
+    }
+    return carsName
+}
+fun getRoundNumber():Int{
+    println("How many rounds will be played?")
+    try {
+        val userInput = Console.readLine()
+        val numberOfRounds = Integer.parseInt(userInput)
+        return numberOfRounds
+    }catch (e:Exception){
+        throw IllegalArgumentException("You entered invalid number!")
+    }
 }
