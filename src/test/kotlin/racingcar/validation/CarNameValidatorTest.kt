59374faa8177a51635ed6cfd5dd3ccb5c38d5799
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-19 15:42:01 +0900

    test(validation): add unit tests for car name validation logic

--- src/test/kotlin/racingcar/validation/CarNameValidatorTest.kt
+++ src/test/kotlin/racingcar/validation/CarNameValidatorTest.kt
@@ -1,12 +1,13 @@
-package racingcar.validation.CarNameValidatorTest;
+package racingcar.validation;
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
-import racingcar.validation.validateCarName
 
 private const val VALID_CAR_NAME = "Car";
 private const val EMPTY_CAR_NAME = "";
+private const val LONG_CAR_NAME = "Carrrrrr";
 
 
 class CarNameValidatorTest {
@@ -19,9 +20,17 @@ class CarNameValidatorTest {
 
     @Test
     fun `empty car name test`() {
-        val exception = assertThrows<Exception> {
+        val exception = assertThrows<IllegalArgumentException> {
             validateCarName(EMPTY_CAR_NAME);
         }
-        
+        assertEquals(INVALID_LENGTH_MSG, exception.message);
+    }
+
+    @Test
+    fun `long car name test`() {
+        val exception = assertThrows<IllegalArgumentException> {
+            validateCarName(LONG_CAR_NAME);
+        }
+        assertEquals(LONG_CAR_NAME, exception.message);
     }
 }
