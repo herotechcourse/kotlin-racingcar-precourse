e1166e4ee7687bcef94208038f5751f44e44b6f2
seohyun.kim <seohyun.kim@timf.co.kr>
2025-04-19 21:47:10 +0900

    feat(structure): create basic game structure

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,23 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    // TODO: Implement the program
+    val game = RacingGame()
+    game.play()
+}
+
+class RacingGame {
+    companion object {
+        private const val MAX_NAME_LENGTH = 5
+        private const val MOVE_FORWARD = 4
+    }
+
+    fun play() {
+        val cars = inputCars()
+        val rounds = inputRounds()
+        val positions = race(cars, rounds)
+        printWinners(cars, positions)
+    }
 }
