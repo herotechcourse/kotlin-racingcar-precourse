1eb160ffb16dda0d7d0cc4e7365d1406bb34cdc2
hiyoons <arin9825@naver.com>
2025-04-19 23:01:03 +0900

    feat: add methods to handler
    Added hadleEmptyFunc to handle empty data
    Added convertIntFunc to handle invalid data

--- src/main/kotlin/racingcar/InvalidHandler.kt
+++ src/main/kotlin/racingcar/InvalidHandler.kt
@@ -12,4 +12,34 @@ class InvalidHandler {
                 throw IllegalArgumentException("Car names should not exceed 5 Characters")
         }
     }
+
+    fun handleNameEmpty(data: List<String>){
+        data.forEach{
+            if(it.isEmpty())
+                throw IllegalArgumentException("Car name should exceed 1 Character")
+        }
+    }
+
+    fun duplicatedName(data:List<String>){
+        if(data.size!=data.distinct().count())
+            throw IllegalArgumentException("Duplicated Name exist!")
+    }
+
+    private fun isNumeric(data:String): Boolean{
+        return try{
+            data.toInt()
+            true
+        }
+        catch (e: NumberFormatException){
+            false
+        }
+    }
+    fun convertInt(data:String){
+        val converted=isNumeric(data)
+        if(!converted){
+            throw IllegalArgumentException("Invalid input : numbers only ")
+        }
+
+    }
+
 }
