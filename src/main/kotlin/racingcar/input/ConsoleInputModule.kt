ff417b9cbc3327ae16979e26d57c454c8e23819a
Taekeundo <pleasepose@gmail.com>
2025-04-19 17:28:47 +0200

    refactor: Move input functions into ConsoleInputModule
    Moved readCarNames() and readRounds() into the ConsoleInputModule to clarify input responsibility and improve structure.

---
+++ src/main/kotlin/racingcar/input/ConsoleInputModule.kt
@@ -0,0 +1,22 @@
+package racingcar.input
+
+class ConsoleInputModule : InputModule {
+    // 1. Read carNames
+    override fun readCarNames(): String {
+        println("Enter the names of the cars (comma-separated):")
+        val carNames = readLine()
+        if (carNames == null) {
+            return ""
+        }
+        return carNames
+    }
+    // 2. Read rounds
+    override fun readRounds(): String {
+        println("How many rounds will be played?")
+        val nRounds = readLine()
+        if (nRounds == null) {
+            return ""
+        }
+        return nRounds
+    }
+}
