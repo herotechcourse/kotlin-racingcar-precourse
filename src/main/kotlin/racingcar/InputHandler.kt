ec1828ade0827331db64772d83de9623f651f258
Petra Bencze <petra.bncz@gmail.com>
2025-04-19 14:03:33 +0200

    feat(InputHandler): InputHandler class
    new class reading and validating user input:
    carNames (List): 2 - 20, comma separated, 1-5 chars, digits or letters
    rounds (Int): 1 - 100, valid Int

---
+++ src/main/kotlin/racingcar/InputHandler.kt
@@ -0,0 +1,56 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+
+class InputHandler {
+    companion object {
+        private const val MIN_CAR_NAMES = 2
+        private const val MAX_CAR_NAMES = 20
+        private const val MIN_ROUNDS = 1
+        private const val MAX_ROUNDS = 100
+        private const val MIN_CHARS = 1
+        private const val MAX_CHARS = 5
+    }
+
+    fun readAndValidateCarNames() : List<String> {
+        println("Enter the names of the cars (comma-separated):")
+        val rawCarNames = Console.readLine()
+
+        val carNameList = rawCarNames.split(",")
+
+        require (carNameList.size >= MIN_CAR_NAMES) {
+            "At least $MIN_CAR_NAMES car names required."
+        }
+        require (carNameList.size <= MAX_CAR_NAMES ) {
+            "Maximum $MAX_CAR_NAMES car names allowed."
+        }
+        carNameList.forEach {
+            validateCarName(it)
+        }
+        return carNameList
+    }
+
+    fun readAndValidateRounds() : Int {
+        println("How many rounds will be played?")
+        val rawRounds = Console.readLine()
+
+        val rounds = rawRounds.toIntOrNull()
+        require (rounds != null) {
+            "Rounds must be an Integer number."
+        }
+        require (rounds in MIN_ROUNDS..MAX_ROUNDS) {
+            "Only rounds in range $MIN_ROUNDS - $MAX_ROUNDS accepted."
+        }
+        return rounds
+    }
+
+    private fun validateCarName(carName: String) {
+        require (carName.length in MIN_CHARS..MAX_CHARS) {
+            "Each car name must be between $MIN_CHARS and $MAX_CHARS characters."
+        }
+        // extract this to a function -> too much indentation
+        require (carName.all { it.isLetterOrDigit() }) {
+            "Car name can only contain letters and digits"
+        }
+    }
+}
