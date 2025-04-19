8c91331ffb29cb32d3645309886a822f8cb534bf
sc@t0a14 <gusdn6671@gmail.com>
2025-04-19 20:55:26 +0900

    feat: Add user input for car names and number of rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,7 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
     println("Enter the names of the cars (comma-separated):")
@@ -10,4 +11,13 @@ fun main() {
     println("Race Results")
 
 
+}
+
+data class Car(val name: String, var position: Int = 0) {
+    fun move() {
+        val randomValue = Randoms.pickNumberInRange(0, 9)
+        if (randomValue >= 4) {
+            position++
+        }
+    }
 }
