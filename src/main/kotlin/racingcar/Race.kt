54dc5d1bc6f46039ffe1af3d37e3209ddd168f22
Arman Ebrahimi <armanebrhm@gmail.com>
2025-04-19 19:12:36 +0200

    refactor: change move function

--- src/main/kotlin/racingcar/Race.kt
+++ src/main/kotlin/racingcar/Race.kt
@@ -5,10 +5,7 @@ import camp.nextstep.edu.missionutils.Randoms
 class Race(val cars: List<Car>,val rounds:Int) {
     fun start(){
         cars.forEach{
-            var randomNumber = Randoms.pickNumberInRange(0,9)
-            if (randomNumber > 4){
-                it.move()
-            }
+            move(it)
         }
         displayCarsPosition()
     }
@@ -25,6 +22,12 @@ class Race(val cars: List<Car>,val rounds:Int) {
             }
         }
         return counter >= 1
+    }
+    fun move(car:Car){
+        var randomNumber = Randoms.pickNumberInRange(0,9)
+        if (randomNumber > 4){
+            car.position++
+        }
     }
 
 }
