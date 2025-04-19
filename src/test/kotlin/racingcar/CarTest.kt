11fb152753706e329af1a4590ea339aeb07b292b
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-19 19:03:40 +0200

    test(race): fix car class tests
    add call of contains after assertThat

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,39 +1,37 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.test.NsTest
 import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 
 class CarTest : NsTest() {
     @Test
     fun `car move test true`() {
         val car = Car("car")
-        assertThat(car.position == 0)
+        assertTrue(car.position == 0)
         car.move(true)
-        assertThat(car.position == 1)
+        assertTrue(car.position == 1)
     }
 
     @Test
     fun `car move test false`() {
         val car = Car("car")
-        assertThat(car.position == 0)
+        assertTrue(car.position == 0)
         car.move(false)
-        assertThat(car.position == 0)
+        assertTrue(car.position == 0)
     }
 
     @Test
     fun `car print result test no round`() {
         val car = Car("car")
-        car.toString()
-        assertThat(output().contains("car : "))
+        assertThat(car.toString()).contains("car : ")
     }
 
     @Test
     fun `car print result test one round`() {
         val car = Car("car")
         car.move(true)
-        car.toString()
-        assertThat(output().contains("car : -"))
+        assertThat(car.toString()).contains("car : -")
     }
 
     @Test
@@ -42,8 +40,7 @@ class CarTest : NsTest() {
         repeat(2) {
             car.move(true)
         }
-        car.toString()
-        assertThat(output().contains("car : --"))
+        assertThat(car.toString()).contains("car : --")
     }
 
     override fun runMain() {
