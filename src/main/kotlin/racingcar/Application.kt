883efca1f99e062f7d12c28d5a8f41e5ab6bc0d6
Adijat Ojutomori <mac@Ojus-Macbook-Air.local>
2025-04-19 19:56:13 +0200

    feat: prompt for cars

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,15 @@
-package racingcar
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    // TODO: Implement the program
+    println("Car racing game coming soon!")
+    promptForCars()
+}
+
+data class Car(val name: String)
+
+fun promptForCars(): String {
+    println("Enter the names of the cars (comma-separated):")
+    val input = Console.readLine()
+    return input
 }
