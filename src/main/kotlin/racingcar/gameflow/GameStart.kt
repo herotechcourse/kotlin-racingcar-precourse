bcce5c6ec2462628df412e96db4e74990ad89610
Taekeundo <pleasepose@gmail.com>
2025-04-19 19:27:31 +0200

    refactor: Remove unused OutputModule
    Before OutputModule was used for input prompts but not anymore after moving print logic into ConsoleInputModule. GameManager now only depends on InputModule

--- src/main/kotlin/racingcar/gameflow/GameStart.kt
+++ src/main/kotlin/racingcar/gameflow/GameStart.kt
@@ -1,29 +1,13 @@
-/**
- *  [ Logic flow ]
- *  1. Test GameManager.start()
- *  2. Make fake Modules (ex) InputModule, OutputModule
- *  3. Check calling status with 'called' variable.
- *  4. Return 'GameStartResult' after test.
- *
- *  [ Why 'private' type? ]
- *  Encapsulation: Only 'GameManager' can control it for safety.
- */
 package racingcar.gameflow
+
 import racingcar.input.InputModule
-import racingcar.output.OutputModule
 
 data class GameStartResult(val carNames: String, val rounds: String)
 
-class GameManager (private val input: InputModule, private val output: OutputModule)
-{
-    fun start(): GameStartResult
-    {
-        output.requestCarNames()
+class GameManager (private val input: InputModule) {
+    fun start(): GameStartResult {
         val carNames = input.readCarNames()
-
-        output.requestRounds()
         val rounds = input.readRounds()
-
         return GameStartResult(carNames, rounds)
     }
 }
