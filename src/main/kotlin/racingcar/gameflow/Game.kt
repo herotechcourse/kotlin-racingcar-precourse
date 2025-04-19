64285976fb31c0b012c765f27a661b966abd059a
Taekeundo <pleasepose@gmail.com>
2025-04-19 22:06:38 +0200

    feat: Implement Game class to control game flow
    This class include (input, validataion, initialization, and game loop). Works as the main controller that connects input, logic, and output to run the game base on the MVC structure.

---
+++ src/main/kotlin/racingcar/gameflow/Game.kt
@@ -0,0 +1,36 @@
+package racingcar.gameflow
+
+// View
+import racingcar.input.InputModule
+import racingcar.output.OutputModule
+
+// Logic
+import racingcar.raceexecution.generateRandomNumber
+import racingcar.raceexecution.isMoveConditionMet
+
+// Validation
+import racingcar.validation.validateCarNames
+import racingcar.validation.validateRounds
+
+class Game(private val input: InputModule,
+            private val output: OutputModule) {
+    fun start() {
+        // 1. Getting input from User and save it.
+        val inputHandler = GameManager(input)   // Just holding the remote control.
+        val gameConfig = inputHandler.start()   // Press the remote control.
+
+        // 2. Validation
+        validateCarNames(gameConfig.carNames)
+        validateRounds(gameConfig.rounds)
+
+        // 3. Initialize game
+        val initResult = gameInit(gameConfig)
+        val listCars = initResult.cars
+        val nRounds = initResult.rounds
+
+        // 4. Start game
+        val generate = { generateRandomNumber() }
+        val isMovable = { number: Int -> isMoveConditionMet(number) }
+        gameLoop(listCars, nRounds, output, generate, isMovable)
+    }
+}
