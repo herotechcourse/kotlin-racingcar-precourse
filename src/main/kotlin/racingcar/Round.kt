cb973c7f33bd3da6dc946af786ed9c4170e81f06
YuGyeong98 <kuk3097@naver.com>
2025-04-20 00:49:25 +0900

    feat(Round): add get random number, move car forward function

---
+++ src/main/kotlin/racingcar/Round.kt
@@ -0,0 +1,23 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Round {
+
+    fun moveCarForward(carNames: MutableMap<String, Int>, carName: String) {
+        if (getRandomNumber() >= MOVING_FORWARD_CRITERIA) {
+            carNames[carName] = carNames.getOrDefault(carName, 0) + MOVING_FORWARD_POINT
+        }
+    }
+
+    private fun getRandomNumber() =
+        Randoms.pickNumberInRange(RANDOM_START_NUMBER, RANDOM_END_NUMBER)
+
+    companion object {
+        private const val RANDOM_START_NUMBER = 0
+        private const val RANDOM_END_NUMBER = 9
+
+        private const val MOVING_FORWARD_CRITERIA = 4
+        private const val MOVING_FORWARD_POINT = 1
+    }
+}
