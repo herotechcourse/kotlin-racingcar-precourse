0eb118149b55db8c9581f68b387f9425f4a2fd79
Arman Ebrahimi <armanebrhm@gmail.com>
2025-04-19 17:23:48 +0200

    add car entity

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,14 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+class Car(val name: String) {
+    var position:Int = 0
+
+    fun move(){
+        position++
+    }
+    fun printPosition(){
+        print("$name: ${"-".repeat(position)}")
+    }
+}
