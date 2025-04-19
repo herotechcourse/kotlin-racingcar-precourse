50aca756aeddb233919cfc263e66562638b92ebc
Mans228 <zhenismansur@gmail.com>
2025-04-19 16:07:13 +0200

    test(race): add tests to get the winner of the race

--- src/test/kotlin/racingcar/RaceTest.kt
+++ src/test/kotlin/racingcar/RaceTest.kt
@@ -14,5 +14,29 @@ class RaceTest {
         assertThat(names).containsExactly("a", "b", "c")
     }
 
+    @Test
+    fun `race returns only one winner`() {
+        val race = Race(listOf("car1", "car2", "car3"))
 
+        val cars = race.getCars()
+        cars[0].position = 2
+        cars[1].position = 3
+        cars[2].position = 4
+
+        val winners = race.getWinners()
+        assertThat(winners).containsExactlyInAnyOrder("car3")
+    }
+
+    @Test
+    fun `race returns multiple winners`() {
+        val race = Race(listOf("car1", "car2", "car3"))
+
+        val cars = race.getCars()
+        cars[0].position = 2
+        cars[1].position = 3
+        cars[2].position = 3
+
+        val winners = race.getWinners()
+        assertThat(winners).containsExactlyInAnyOrder("car2", "car3")
+    }
 }
