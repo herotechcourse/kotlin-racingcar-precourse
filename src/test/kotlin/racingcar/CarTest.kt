edb7d3f90c1868a0b114c72506e3e8e48f4b2359
Mariia Kulikova <111587633+mariyakulikova@users.noreply.github.com>
2025-04-19 15:47:49 +0200

    test(race): add new test and refactor old for car class
    add new tests to test car move, refactor old ones make them covering all car class behavior

--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -6,18 +6,44 @@ import org.junit.jupiter.api.Test
 
 class CarTest : NsTest() {
     @Test
-    fun `car move test`() {
+    fun `car move test true`() {
         val car = Car("car")
+        assertThat(car.position == 0)
         car.move(true)
         assertThat(car.position == 1)
     }
 
     @Test
-    fun `car print result test`() {
+    fun `car move test false`() {
+        val car = Car("car")
+        assertThat(car.position == 0)
+        car.move(false)
+        assertThat(car.position == 0)
+    }
+
+    @Test
+    fun `car print result test no round`() {
+        val car = Car("car")
+        car.toString()
+        assertThat(output().contains("car : "))
+    }
+
+    @Test
+    fun `car print result test one round`() {
         val car = Car("car")
         car.move(true)
         car.toString()
         assertThat(output().contains("car : -"))
+    }
+
+    @Test
+    fun `car print result test two round`() {
+        val car = Car("car")
+        repeat(2) {
+            car.move(true)
+        }
+        car.toString()
+        assertThat(output().contains("car : --"))
     }
 
     override fun runMain() {
