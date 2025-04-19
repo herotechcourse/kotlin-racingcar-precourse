b55c983842c630b4b0925d2f5f383a25df69a087
Emir Risyad <emirrsyd@gmail.com>
2025-04-19 16:40:47 +0200

    feat(Car): add displayProgress method to display the car progress

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -4,6 +4,11 @@ class Car(val name: String) {
     var position: Int = 0
         private set
 
+    fun displayProgress(): String {
+        val progress = "-".repeat(position)
+        return "$name : $progress"
+    }
+
     fun move() {
         position++
     }
