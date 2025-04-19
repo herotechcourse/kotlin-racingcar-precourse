48e77de6a77d3e144faabca1f80a54fdd340eb46
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-19 14:59:03 +0900

    refactor(validation): simplify natural number validation

--- src/main/kotlin/racingcar/validation/NumberValidator.kt
+++ src/main/kotlin/racingcar/validation/NumberValidator.kt
@@ -1,8 +1,5 @@
 package racingcar.validation
 
-private const val MIN_NAME_LENGTH = 1;
-private const val MAX_NAME_LENGTH = 5;
-
-fun isNaturalNumber(num: Int): Boolean {
-    return num >= 1;
+fun validateNatural(num: Int) {
+    if (num < 1) throw IllegalArgumentException("Number must be a natural number.");
 }
