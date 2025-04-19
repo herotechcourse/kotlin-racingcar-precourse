44c64250c0b19172de311900ec9f9db4b7c0df7f
sc@t0a14 <gusdn6671@gmail.com>
2025-04-19 20:32:42 +0900

    feat: Read user input for the number of rounds
    Implemented reading user input for the number of rounds using Console.readLine().

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,4 +5,5 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     println("Enter the names of the cars (comma-separated):")
     println("How many rounds will be played?")
+    val gameCount = Console.readLine()
 }
