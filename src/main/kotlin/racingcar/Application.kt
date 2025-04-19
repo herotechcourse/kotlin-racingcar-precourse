688a82bb77b2d6c3f7004482e817292959a9a2b4
Arman Ebrahimi <armanebrhm@gmail.com>
2025-04-19 09:33:48 +0200

    implement user entry for car name feature.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,13 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
     // TODO: Implement the program
+}
+fun getNames():List<String>{
+    println("Enter the names of the cars (comma-separated):")
+    val names = Console.readLine()
+    val parts = names.split(",")
+    return parts
 }
