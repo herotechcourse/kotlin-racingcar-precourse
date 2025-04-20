0695f2f6096bdfc0fb2475758e95b3def7591416
Jihun Kim <zh1229@naver.com>
2025-04-21 01:29:10 +0900

    refactor(model): switch Car to use RandomsWrapper for random movement

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,14 +1,13 @@
 package racingcar.model
 
-import camp.nextstep.edu.missionutils.Randoms
+import racingcar.util.RandomsWrapper
 
 data class Car(
     val name: String,
     var position: Int = 0
 ) {
     fun tryMove() {
-        val randomNumber = Randoms.pickNumberInRange(0, 9)
-        if (randomNumber >= 4) {
+        if (RandomsWrapper.next() >= 4) {
             position++
         }
     }
