ddc7b00e14366da6f6dac617a1270fe622285967
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-20 17:15:01 +0900

    feat(input): add inputManager.kt to handle reading and validating input

--- src/main/kotlin/racingcar/input/inputManager.kt
+++ src/main/kotlin/racingcar/input/inputManager.kt
@@ -1,2 +1,36 @@
 package racingcar.input
 
+import camp.nextstep.edu.missionutils.Console
+import racingcar.input.validation.validateCarName
+import racingcar.input.validation.validateNatural
+import racingcar.input.validation.validateNumberFormat
+import racingcar.input.util.getCarNamesFromInput
+
+
+fun readLineAndProcess(): String {
+    val line: String? = Console.readLine();
+    if(line.isNullOrBlank())
+        throw IllegalArgumentException();
+    return line;
+}
+
+fun readLineAndGetCars(): Array<String>{
+    val nameInput: String = readLineAndProcess();
+    val cars = getCarNamesFromInput(nameInput);
+
+    cars.forEach {
+            car -> validateCarName(car);
+    }
+
+    return cars;
+}
+
+fun readLineAndGetRounds(): Int {
+    val roundsInput: String = readLineAndProcess();
+    validateNumberFormat(roundsInput);
+
+    val rounds: Int = Integer.parseInt(roundsInput);
+    validateNatural(rounds);
+
+    return rounds;
+}
