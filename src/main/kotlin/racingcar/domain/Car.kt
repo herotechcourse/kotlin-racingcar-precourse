f376c61be632e8bcb254e9678ff789dc3cb992fe
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2025-04-21 00:35:38 +0900

    refactor: Move Car name validation to init block

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -5,19 +5,21 @@ data class Car(
     var position: Int = 0,
     private val moveStrategy: MoveStrategy = RandomMoveStrategy()
 ) {
+    init {
+        validateName()
+    }
+    private fun validateName() {
+        if (name.isBlank()) {
+            throw IllegalArgumentException("Car name cannot be blank.")
+        }
+        if (name.length > 5) {
+            throw IllegalArgumentException("Car name must be 5 characters or fewer.")
+        }
+    }
+
     fun moveForward() {
         if (moveStrategy.isMovable()) {
             position++
-        }
-    }
-    companion object {
-        fun validateCar(name: String) {
-            if (name.isBlank()) {
-                throw IllegalArgumentException("Car name cannot be blank.")
-            }
-            if (name.length > 5) {
-                throw IllegalArgumentException("Car name must be 5 characters or fewer.")
-            }
         }
     }
 }
