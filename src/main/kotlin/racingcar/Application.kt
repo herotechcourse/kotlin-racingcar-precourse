f1e4ee76716b4ca3add130eacc2770c6f1c3f315
hhes0225 <hhes0225@gmail.com>
2025-04-21 05:19:02 +0900

    feat: implement main entry to read input and start race

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    // TODO: Implement the program
+    println("Enter the name of the cars (comma-separated):")
+    val carInput = Console.readLine() ?: ""
+    println("How many rounds will be played?")
+    val roundInput = Console.readLine() ?: ""
+
+    val race = Race(carInput, roundInput.toInt())
+    println()
+
+    race.playRound()
 }
