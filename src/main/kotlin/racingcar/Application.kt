a8d0f1f3613a7f72566c24581a2aa37cfc78b2c4
Namsophia <namsophie9@gmail.com>
2025-04-20 12:56:42 +0200

    validated number of rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,4 +1,6 @@
-package racingcar
+
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
 
 data class Car(val name: String, var position: Int = 0)
 
@@ -6,7 +8,7 @@ fun main() {
     // validating car names
     try{
         println("Enter car names (comma-seperated, max 5 characters each name):")
-        val input = readLine() ?: throw IllegalArgumentException("No input provided.")
+        val input = Console.readLine()
         val names = input.split(",").map { it.trim()}
 
         if (names.isEmpty()) throw IllegalArgumentException("You didn't provide any car names.")
@@ -18,7 +20,17 @@ fun main() {
         }
         // Validate number of rounds
         println("Enter number of rounds:")
-        val roundsInput = readLine() ?: throw IllegalArgumentException("No input for rounds.")
+        val roundsInput = Console.readLine()
         val rounds = roundsInput.toIntOrNull() ?: throw IllegalArgumentException("Rounds must be a number.")
+        if (rounds <= 0) throw IllegalArgumentException("Rounds must be greater than 0.")
         println("\nRace Start!\n")
-}
+
+        // Run game for number of rounds
+        repeat(rounds) {
+            for (car in cars) {
+                val move = Randoms.pickNumberInRange(0, 9)
+                if (move >= 4) {
+                    car.position += 1
+                }
+            }
+        }
