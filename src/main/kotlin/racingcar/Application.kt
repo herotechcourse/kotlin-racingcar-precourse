01e9cb1aa9296dc080643d2a66d0b4101bbf74bf
Fanny Yudris <fanny.yudris@gmail.com>
2025-04-20 19:54:09 +0200

    feat: implement round input and validation

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -14,9 +14,18 @@ fun main() {
         throw e
     }
 
-    // TEMP: Just to verify input works
-    println("Cars entered:")
-    cars.forEach { println(it.name) }
+    println("How many rounds will be played?")
+    val roundInput = Console.readLine()
 
-    // TODO: Ask for number of rounds
+    val rounds = try {
+        val number = roundInput.toInt()
+        require(number > 0) { "Number of rounds must be greater than 0." }
+        number
+    } catch (e: Exception) {
+        println("Invalid input: ${e.message}")
+        throw IllegalArgumentException("Invalid number of rounds.")
+    }
+
+    // TEMP output to test
+    println("Rounds to play: $rounds")
 }
