292813cfb7ba29739e49f3b374ebf6b35387943c
Daria Eliseeva <elisey422das@gmail.com>
2025-04-20 17:51:18 +0200

    feat: allow users to enter car names

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,10 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars (comma-separated):")
+    val carsInput = Console.readLine()
+    val cars = carsInput.split(",")
+    println(cars)
 }
