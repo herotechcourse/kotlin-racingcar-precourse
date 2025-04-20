3993de26d2b383f7996e7a295310ae383c9daeb2
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-20 17:36:32 +0900

    feat(storage): add random movement logic to Car class

--- src/main/kotlin/racingcar/storage/Car.kt
+++ src/main/kotlin/racingcar/storage/Car.kt
@@ -1,10 +1,26 @@
 package racingcar.storage
 
+import camp.nextstep.edu.missionutils.Randoms
+
+private const val RANDOM_THRESHOLD = 4;
+
 class Car(val name: String) {
-    var distance: Int = 0
-        private set
+    private var distance: Int = 0
 
     fun moveForward() {
         distance++;
+    }
+
+    fun moveForwardRandomly(){
+        if(shouldMoveForward())
+            moveForward();
+    }
+
+    private fun shouldMoveForward(): Boolean {
+        return generateNumber() >= RANDOM_THRESHOLD;
+    }
+
+    private fun generateNumber(): Int{
+        return Randoms.pickNumberInRange(0, 9);
     }
 }
