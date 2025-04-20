03716bd94aa1c2256b263e13f85179f6d2712689
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-20 20:35:15 +0200

    fix: add a line before printing result

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,13 +4,14 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
 
-    println("Enter the names of the cars(comma-separated):")
+    println("Enter the names of the cars (comma-separated):")
     val input = Console.readLine()
     val names = validateCarNames(input.split(","))
 
     println("How many rounds will be played?")
     val rounds = Console.readLine()
     val numberOfRounds: Int = validateRounds(rounds)
+    println()
 
     val cars = names.map { Car(it) }
     val race = Race(cars, numberOfRounds)
