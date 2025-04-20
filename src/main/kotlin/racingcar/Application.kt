eb0933fb7ce83ce4fbd3c2abd62f3694b3475a9d
cat <myakoven@student.42berlin.de>
2025-04-20 20:07:27 +0200

    feat(race): provide racing logic
    Add raceThem function
    	- geneate a random number (>=4 means a car moves forward this round)
    		- use car.move()
    	- race all cars and print their moves for each round
    		- use car.printRoundStatus()
    	- values are upgraded within each car object

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,20 +1,16 @@
 package racingcar
 
-import java.lang.IllegalArgumentException
 import camp.nextstep.edu.missionutils.Randoms
+import java.lang.IllegalArgumentException
 
-val maxRounds: UInt = 200u
+val maxRounds: Int = 200
 
 fun main() {
-    for (i in 0 until 5 step 2) println(i)
-    // for (i in 10 until 5) println(i)
-
-
-    // TODO: Implement the program
     try {
         val names: List<String> = getUserInputNames()
         val cars: List<Car> = makeCars(names)
-        val numRounds: UInt = getUserInputRounds()
+        val numRounds: Int = getUserInputRounds()
+        raceThem(cars, numRounds)
     } catch (error: IllegalArgumentException) {
         println("Error: ${error.message}")
     }
@@ -24,8 +20,12 @@ fun makeCars(names: List<String>): List<Car> {
     return names.map { name -> Car(name) }
 }
 
-// fun raceThem(cars: List<Car>, nRounds: UInt)
-// {
-    
-// }
-
+fun raceThem(cars: List<Car>, nRounds: Int) {
+    for (i in 0 until nRounds) {
+        cars.forEach { car ->
+            if (Randoms.pickNumberInRange(0, 9) >= 4) car.move()
+            car.printRoundStatus()
+        }
+        println("")
+    }
+}
