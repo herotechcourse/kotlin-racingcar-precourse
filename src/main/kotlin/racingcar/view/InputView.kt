a2f6a8ce8e2c74db780bc31e357c34769a8d2ddc
Jiihyun <cwlgusc@gmail.com>
2025-04-20 15:00:53 +0900

    refactor: create StringParser for parsing string - parse input by delimiter - parse input to Int type

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -1,26 +1,25 @@
 package racingcar.view
 
 import camp.nextstep.edu.missionutils.Console
+import racingcar.util.StringParser
 
 class InputView {
 
     fun readCarNames(): List<String> {
         println(READ_CAR_NAMES_MESSAGE)
         val carNames = readInput()
-        return carNames.split(CAR_NAME_DELIMITER)
+        return StringParser.parseByDelimiter(carNames, CAR_NAME_DELIMITER)
     }
 
     fun readTotalRounds(): Int {
         println(READ_TOTAL_ROUNDS_MESSAGE)
         val input = readInput()
-        val round = input.toIntOrNull()
-        require(round != null) { INVALID_NUMBER_ERROR }
-        return round
+        return StringParser.parseToInt(input)
     }
 
     private fun readInput(): String {
-        val input = Console.readLine()
-        require(input.trim().isNotBlank()) { INVALID_INPUT_ERROR }
+        val input = Console.readLine().trim()
+        require(input.isNotBlank()) { INVALID_INPUT_ERROR }
         return input
     }
 
@@ -31,6 +30,5 @@ class InputView {
         private const val CAR_NAME_DELIMITER = ","
 
         private const val INVALID_INPUT_ERROR = "[ERROR] Input value does not exist."
-        private const val INVALID_NUMBER_ERROR = "[ERROR] Input value must be a number."
     }
 }
