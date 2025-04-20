8c6cca206d4269443ebfa7636e7e07c4f0448c22
siyeonSon <siyeon0209@naver.com>
2025-04-20 19:30:05 +0900

    refactor: use missionutils.Console

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -4,15 +4,16 @@ import racingcar.util.Constant.CAR_NAME_DELIMITER
 import racingcar.util.Constant.CAR_NAME_MESSAGE
 import racingcar.util.Constant.ROUND_COUNT_MESSAGE
 import racingcar.util.Exceptions.validateInteger
+import camp.nextstep.edu.missionutils.Console
 
 class InputView {
     fun inputCarNames(): List<String> {
         println(CAR_NAME_MESSAGE)
-        return readln().split(CAR_NAME_DELIMITER).map { it.trim() }
+        return Console.readLine().split(CAR_NAME_DELIMITER).map { it.trim() }
     }
 
     fun inputRoundCount(): Int {
         println(ROUND_COUNT_MESSAGE)
-        return readln().also { it.validateInteger() }.toInt()
+        return Console.readLine().also { it.validateInteger() }.toInt()
     }
 }
