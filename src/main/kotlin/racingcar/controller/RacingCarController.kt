bf1a6ca8765bec72b2d0d94985fd4f02c41c748b
songpink <jejeje04@naver.com>
2025-04-20 23:57:39 +0900

    refactor: separate the method

--- src/main/kotlin/racingcar/controller/RacingCarController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarController.kt
@@ -14,12 +14,23 @@ class RacingCarController(
         val cars = inputView.readCarNames().map { Car(it) }
         val numberOfRounds = inputView.readNumberOfRounds()
         val racingGame = RacingGame(cars, DefaultRandomNumberGenerator())
+        playRace(numberOfRounds, racingGame, cars)
+        decideWinner(racingGame)
+    }
 
+    private fun playRace(
+        numberOfRounds: Int,
+        racingGame: RacingGame,
+        cars: List<Car>,
+    ) {
         outputView.printRaceResultsMessage()
         repeat(numberOfRounds) {
             racingGame.moveCars()
             outputView.printRaceResults(cars)
         }
+    }
+
+    private fun decideWinner(racingGame: RacingGame) {
         val winners = racingGame.decideWinner()
         outputView.printWinners(winners)
     }
