4a76b31c9855dd9a76902c812bbc02d4dbb6d7c2
siyeonSon <siyeon0209@naver.com>
2025-04-20 18:17:18 +0900

    feat: a car can move forward if a randomly generated number between 0 and 9 is 4 or greater

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,5 +1,7 @@
 package racingcar.model
 
+import camp.nextstep.edu.missionutils.Randoms
+import racingcar.util.Constant.CAR_MOVEMENT_CONDITION
 import racingcar.util.Constant.CAR_NAME_CANNOT_BLANK_ERROR_MESSAGE
 import racingcar.util.Constant.CAR_NAME_EXCEED_ERROR_MESSAGE
 import racingcar.util.Constant.MAX_CAR_NAME_SIZE
@@ -11,5 +13,11 @@ class Car(val name: String) {
     init {
         require(name.length <= MAX_CAR_NAME_SIZE) { CAR_NAME_EXCEED_ERROR_MESSAGE }
         require(name.isNotBlank()) { CAR_NAME_CANNOT_BLANK_ERROR_MESSAGE }
+    }
+
+    fun move() {
+        if (Randoms.pickNumberInRange(0, 9) >= CAR_MOVEMENT_CONDITION) {
+            position++
+        }
     }
 }
