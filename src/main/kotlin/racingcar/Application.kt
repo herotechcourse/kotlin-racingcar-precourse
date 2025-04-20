cfa050de56577e0186930ebf5bfa72168dd8ff3e
cat <myakoven@student.42berlin.de>
2025-04-20 19:39:39 +0200

    chore(all): backup push

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,15 @@
 package racingcar
 
 import java.lang.IllegalArgumentException
+import camp.nextstep.edu.missionutils.Randoms
 
 val maxRounds: UInt = 200u
 
 fun main() {
+    for (i in 0 until 5 step 2) println(i)
+    // for (i in 10 until 5) println(i)
+
+
     // TODO: Implement the program
     try {
         val names: List<String> = getUserInputNames()
@@ -18,4 +23,9 @@ fun main() {
 fun makeCars(names: List<String>): List<Car> {
     return names.map { name -> Car(name) }
 }
+
+// fun raceThem(cars: List<Car>, nRounds: UInt)
+// {
+    
+// }
 
