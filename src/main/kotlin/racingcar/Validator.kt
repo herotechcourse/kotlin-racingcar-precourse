60ad26f887b184dbab058555ffa9bf780c1c5eb9
YuGyeong98 <kuk3097@naver.com>
2025-04-20 15:42:44 +0900

    refactor(Validator): add condition to validate name function if name is blank

--- src/main/kotlin/racingcar/Validator.kt
+++ src/main/kotlin/racingcar/Validator.kt
@@ -3,8 +3,8 @@ package racingcar
 class Validator {
 
     fun validateName(name: String, limit: Int) {
-        if (name.length > limit) {
-            throw IllegalArgumentException("Car name can be entered up to $limit characters.")
+        if (name.length > limit || name.isBlank()) {
+            throw IllegalArgumentException("Car name can be entered up to $limit characters, and empty or blank is not possible.")
         }
     }
 
