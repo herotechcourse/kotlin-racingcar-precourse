97a726aea57ea80e99690f88a8274a81e9275ddf
sominyun <somin455@naver.com>
2025-04-21 03:52:59 +0900

    test: add tests for returning integer when round input is valid

--- src/test/kotlin/racingcar/service/RacingServiceTest.kt
+++ src/test/kotlin/racingcar/service/RacingServiceTest.kt
@@ -3,6 +3,7 @@ package racingcar.service
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import racingcar.backend.dto.CarNameRequest
+import racingcar.backend.dto.RoundRequest
 import racingcar.backend.service.RacingService
 
 class RacingServiceTest {
@@ -17,5 +18,13 @@ class RacingServiceTest {
         assertEquals(2, result.size)
         assertEquals("pobi", result[0].carName)
         assertEquals("woni", result[1].carName)
+    }
+
+    @Test
+    fun `should return integer when round input is valid`() {
+        val request = RoundRequest("5")
+        val result = racingService.convertRoundToNum(request)
+
+        assertEquals(5, result)
     }
 }
