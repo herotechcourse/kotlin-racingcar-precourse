b37e09aae2b985d7675f2c9f66beff0a4cc7fde4
madhumitha murali <mithaeie@gmail.com>
2025-04-20 16:03:51 +0200

    test(getinput): unit tests for user input

--- src/test/kotlin/racingcar/ApplicationTest.kt
+++ src/test/kotlin/racingcar/ApplicationTest.kt
@@ -3,6 +3,7 @@ package racingcar
 import camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest
 import camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest
 import camp.nextstep.edu.missionutils.test.NsTest
+import io.io
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
@@ -24,6 +25,46 @@ class ApplicationTest : NsTest() {
     fun `exception test`() {
         assertSimpleTest {
             assertThrows<IllegalArgumentException> { runException("pobi,javaji", "1") }
+        }
+    }
+
+    @Test
+    fun `check total round is integer`() {
+        assertSimpleTest {
+            // no of is integer and not string
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "a") }
+        }
+    }
+
+    @Test
+    fun `check total round is within integer range`() {
+        assertSimpleTest {
+            // no of is integer < Int.Max_size
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "10000000000000000000") }
+        }
+    }
+
+    @Test
+    fun `dont allow total rounds less than 1`() {
+        assertSimpleTest {
+            // no of rounds >= 1
+            assertThrows<IllegalArgumentException> { runException("pobi,woni", "0") }
+        }
+    }
+
+    @Test
+    fun `dont allow duplicate car names`() {
+        assertSimpleTest {
+            // no of rounds >= 1
+            assertThrows<IllegalArgumentException> { runException("pobi,Pobi") }
+        }
+    }
+// todo move to Race class
+    @Test
+    fun `dont allow single car names`() {
+        assertSimpleTest {
+            // single car name
+            assertThrows<IllegalArgumentException> { runException("pobi", "1") }
         }
     }
 
