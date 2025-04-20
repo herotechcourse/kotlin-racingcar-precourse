614f195ff7ae89bdf3c34122cee5615ad9200a69
GSIII <kalseo901@gmail.com>
2025-04-20 17:29:22 +0900

    feat(output) : implement game logic and print output

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,20 @@
 package racingcar
 
 import racingcar.view.InputView
+import racingcar.view.OutputView
+import racingcar.service.Car
+import racingcar.service.RacingGame
+
 
 fun main() {
     val carNames = InputView.readCarNames()
     val roundCount = InputView.readRoundCount()
+
+    val cars = carNames.map { Car(it) }
+    val game = RacingGame(cars, roundCount)
+
+    game.startRace()
+
+    val winners = game.getWinners()
+    OutputView.printWinners(winners)
 }
