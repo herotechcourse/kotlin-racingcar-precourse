04e1cbfaa55f88bffb8aaf48d36ac3f5bcd7dce8
rz-p <pramadhan9@gmail.com>
2025-04-20 13:44:47 +0200

    feat: implement car name input functionality with validation

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,19 @@
 package racingcar
 
 fun main() {
-    // TODO: Implement the program
+    val carNames = inputCarNames()
 }
+
+fun inputCarNames(): List<String> {
+    println("Input car names (separated by commas)")
+    val input = readLine() ?: throw IllegalArgumentException("Input invalid!")
+    if (!input.contains(",")) {
+        throw IllegalArgumentException("Names must be separated by commas")
+    }
+    val carNames = input.split(",").map { it.trim() }
+    if (carNames.any { it.length > 5}) {
+        throw IllegalArgumentException("Names cannot be longer than 5 characters")
+    }
+    return carNames
+}
+    
