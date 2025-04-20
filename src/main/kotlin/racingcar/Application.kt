90cfb0517e48bb184e2a7aabb12be245addf8c3b
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-20 19:09:21 +0900

    feat: add interactions and gameplay to main method

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,25 @@
 package racingcar
 
+import racingcar.input.readLineAndGetRounds
+import racingcar.input.readLineAndGetCars
+import racingcar.output.printFromOutputManager
+import racingcar.race.race
+
 fun main() {
-    // TODO: Implement the program
+    val cars: Array<String> = getCarsFromDialog()
+    val rounds: Int = getRoundNumbersFromDialog();
+
+    race(cars, rounds);
+    printFromOutputManager();
+}
+
+fun getCarsFromDialog(): Array<String> {
+    println("Enter the names of the cars (comma-separated):");
+    val cars: Array<String> = readLineAndGetCars();
+    return cars;
+}
+fun getRoundNumbersFromDialog(): Int{
+    println("How many rounds will be played?");
+    val rounds: Int = readLineAndGetRounds();
+    return rounds;
 }
