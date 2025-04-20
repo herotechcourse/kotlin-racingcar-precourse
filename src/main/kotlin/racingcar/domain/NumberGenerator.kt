dd24b5888d1d19d614a613acb77e1ccb6345d5c2
ahyeon-github <ahyeon31@sookmyung.ac.kr>
2025-04-21 00:45:15 +0900

    refactor: Delete NumberGenerator.kt

--- src/main/kotlin/racingcar/domain/NumberGenerator.kt
+++
@@ -1,8 +0,0 @@
-package racingcar.domain
-import camp.nextstep.edu.missionutils.Randoms
-
-object NumberGenerator {
-    fun createRandomNumber(): Int {
-        return Randoms.pickNumberInRange(0, 9)
-    }
-}
