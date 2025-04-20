8555465ecee7f5f1c7e1c2bfa27be116c42096ed
Taekeundo <pleasepose@gmail.com>
2025-04-20 21:26:57 +0200

    refactor: Delegate validation to GameConfigurator
    Moved input handling and validation logic out of Game and into GameConfigurator to clarify responsibility of Game class as a flow controller.

--- src/main/kotlin/racingcar/gameflow/Game.kt
+++ src/main/kotlin/racingcar/gameflow/Game.kt
@@ -1,37 +1,35 @@
 package racingcar.gameflow
 
-// View
 import racingcar.input.InputModule
 import racingcar.output.OutputModule
-
-// Logic
 import racingcar.raceexecution.generateRandomNumber
 import racingcar.raceexecution.isMoveConditionMet
 
-// Validation
-import racingcar.validation.validateCarNames
-import racingcar.validation.validateRounds
-
+/**
+ *  [ Lambdas ]
+ *  These delay the execution of generateRandomNumber() and isMoveConditionMet() logic until each round.
+ *
+ *  Difference between normal value and lambda
+ *  val num = generateRandomNumber()            // executed immediately, once.
+ *  val generate = { generateRandomNumber() }   // executed every time it's called, generate().
+ */
 class Game(private val input: InputModule,
             private val output: OutputModule) {
     fun start() {
         // 1. Getting input from User and save it.
-        val inputHandler = GameManager(input)   // Just holding the remote control.
-        val gameConfig = inputHandler.start()   // Press the remote control.
+        val inputHandler = GameConfigurator(input)   // Just holding the remote control.
+        val gameConfig = inputHandler.configure()   // Press the remote control.
 
-        // 2. Validation
-        validateCarNames(gameConfig.carNames)
-        validateRounds(gameConfig.rounds)
-
-        // 3. Initialize game
+        // 2. Initialize game
         val initResult = gameInit(gameConfig)
         val listCars = initResult.cars
         val nRounds = initResult.rounds
 
-        // 4. Start game
+        // 3. Set movement condition and randomness with Lambdas.
         val generate = { generateRandomNumber() }
         val isMovable = { number: Int -> isMoveConditionMet(number) }
 
+        // 4. Start game
         println("\nRace Results")
         gameLoop(listCars, nRounds, output, generate, isMovable)
     }
