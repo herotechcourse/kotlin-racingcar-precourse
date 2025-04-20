9269bddd45c4c0d17dd37d5a6e6ee182af2ebedf
cat <myakoven@student.42berlin.de>
2025-04-20 20:54:26 +0200

    feat(winners): implement logic to find and print winners
    Call to printWinners() function will
    	- Determine the winner
    	- Print winner names, comma separated

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,8 +2,10 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
 import java.lang.IllegalArgumentException
+import racingcar.moveIndicator
 
 val maxRounds: Int = 200
+val moveIndicator: Int = 4
 
 fun main() {
     try {
@@ -11,6 +13,7 @@ fun main() {
         val cars: List<Car> = makeCars(names)
         val numRounds: Int = getUserInputRounds()
         raceThem(cars, numRounds)
+        printWinners(cars, numRounds)
     } catch (error: IllegalArgumentException) {
         println("Error: ${error.message}")
     }
@@ -23,9 +26,33 @@ fun makeCars(names: List<String>): List<Car> {
 fun raceThem(cars: List<Car>, nRounds: Int) {
     for (i in 0 until nRounds) {
         cars.forEach { car ->
-            if (Randoms.pickNumberInRange(0, 9) >= 4) car.move()
+            if (Randoms.pickNumberInRange(0, 9) >= moveIndicator) car.move()
             car.printRoundStatus()
         }
         println("")
     }
+}
+
+fun printWinners(cars: List<Car>, nRounds: Int) {
+    var roundsWon = nRounds
+    var foundFirstWin = false
+    print("Winners : ")
+    do {
+        cars.forEach { car ->
+            foundFirstWin = checkWinner(car, roundsWon, foundFirstWin) || foundFirstWin
+        }
+    } while (!foundFirstWin && roundsWon-- > 0)
+}
+
+fun checkWinner(car: Car, roundsWon: Int, foundFirstWin: Boolean): Boolean {
+    if (car.getMovesMade() == roundsWon) {
+        printWinnerName(car, foundFirstWin)
+        return true
+    }
+    return false
+}
+
+fun printWinnerName(car: Car, foundFirstWin: Boolean) {
+    if (foundFirstWin) print(", ")
+    print("${car.getName()}")
 }
