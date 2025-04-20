ed103b0400e25dcb956c8f27053daba4db124101
yojin <roadlroal@naver.com>
2025-04-20 20:44:31 +0100

    feat(carracing): add CarRacing Class in Application.kt
    - private Car List cars, Int totalRound
    - implement function init - display message and get input from user, if input is invalid, Throw IllegalArgumentException
    - implement funciton racing - process round based on totalRound
    - implement function printResult - print Winner of the race

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -25,13 +25,89 @@ class Car(name: String) {
     }
 }
 
+class CarRacing {
+    private val cars = mutableListOf<Car>()
+    private var totalRound: Int = 0
+
+    fun run() {
+        init()
+        racing()
+        printResult()
+    }
+
+    private fun init() {
+        println("Enter the names of the cars (comma-separated):")
+        getCarInput()
+
+        println("How many rounds will be played?")
+        getRoundInput()
+
+        println()
+    }
+
+    private fun getCarInput() {
+        val input = Console.readLine()?.trim()
+            ?: throw IllegalArgumentException("Invalid Input: names are NULL.")
+
+        if (input.isEmpty())
+            throw IllegalArgumentException("Invalid Input: names are empty")
+
+        val names = input.split(",").map { it.trim() }
+
+        if (names.any { !it.matches(Regex("^[A-Za-z1-9 ]+$")) })
+            throw IllegalArgumentException("Invalid input: only alphabets, numbers, or spaces are allowed..")
+
+        for (name in names) {
+            cars.add(Car(name))
+        }
+    }
+
+    private fun getRoundInput() {
+        val input = Console.readLine()?.trim()
+            ?: throw IllegalArgumentException("Invalid Input: round is NULL.")
+
+        try {
+            val round = input.toInt()
+            if (round < 1) throw IllegalArgumentException("Invalid Input: only positive numbers are allowed.")
+            totalRound = round
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("Invalid Input: only Integer values are allowed")
+        }
+    }
+
+    private fun racing() {
+        println("Race Results")
+
+        for (round in 1..totalRound) {
+            for (car in cars) {
+                car.runRound()
+                car.printStatus()
+            }
+            println()
+        }
+    }
+
+    private fun printResult() {
+        val maxMoved = cars.maxOfOrNull { it.movedRounds } ?: 0
+        val winners = cars.filter { it.movedRounds == maxMoved }.map { it.name }
+
+        println("Winner(s): ${winners.joinToString(", ")}")
+    }
+}
+
+
 fun main() {
     // TODO: Implement the program
 
+    val carRacing = CarRacing()
+
+    carRacing.run()
+}
+
+fun testCarClass() {
     val testCars = mutableListOf<Car>().apply {
         add(Car("test"))
         add(Car("test2"))
-        // 필요하다면 더 추가…
     }
 
     for (i in 1..10) {
