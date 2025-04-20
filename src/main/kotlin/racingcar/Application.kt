1f32bd84aa4171b78f27d9900a39895374e22f7b
김소운 <sozzykim@naver.com>
2025-04-21 01:05:26 +0900

    feat(racing): implement racing game logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,9 +1,11 @@
 package racingcar
 
 fun main() {
-    // test InputView
+    // test
     val names = InputView.readCarNames()
     val rounds = InputView.readRoundCount()
+    val cars = names.map {Car(it)}
+    Racing(cars, rounds).play()
 
     println("car names: $names")
     println("rounds: $rounds")
