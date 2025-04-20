ccfa4650ac9a621a37c3d841d2e4303bc623140f
YuGyeong98 <kuk3097@naver.com>
2025-04-20 19:28:36 +0900

    refactor(Round): extract get max movement count function

--- src/main/kotlin/racingcar/Round.kt
+++ src/main/kotlin/racingcar/Round.kt
@@ -3,7 +3,9 @@ package racingcar
 class Round {
 
     fun getWinners(cars: Set<Car>): List<Car> {
-        val maxMovementCount = cars.maxOf { it.movementCount }
+        val maxMovementCount = getMaxMovementCount(cars)
         return cars.filter { it.movementCount == maxMovementCount }
     }
+
+    private fun getMaxMovementCount(cars: Set<Car>) = cars.maxOf { it.movementCount }
 }
