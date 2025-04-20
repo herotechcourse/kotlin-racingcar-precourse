dc31196bae69aa1b5a92d37c97546406c345c277
KuMMii <4.prgmng@gmail.com>
2025-04-21 00:17:49 +0900

    feat(board): add features
    Added printing each round and the result.
    Checking winner feature isn't completed

--- src/main/kotlin/racingcar/Board.kt
+++ src/main/kotlin/racingcar/Board.kt
@@ -7,4 +7,24 @@ class Board {
     fun setCars(newCars: List<Car>) {
         cars = newCars
     }
+
+    // Print a round
+    fun printRound() {
+        for (car in cars) {
+            println("${car.name} : ${car.result}")
+        }
+        println()
+    }
+    
+    // Check the winners
+    fun checkWinner(): List<String> {
+        var winners = mutableListOf<String>()
+
+        return winners
+    }
+    
+    // Print the result
+    fun printResult(winners: List<String>) {
+        println("Winners : ${winners.joinToString()}")
+    }
 }
