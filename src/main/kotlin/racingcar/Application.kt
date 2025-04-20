42901f640f70f3e0f1c721754998b86befcf937c
Daria Eliseeva <elisey422das@gmail.com>
2025-04-20 17:59:12 +0200

    feat: allow users to enter a number of rounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -4,7 +4,9 @@ import camp.nextstep.edu.missionutils.Console
 
 fun main() {
     println("Enter the names of the cars (comma-separated):")
-    val carsInput = Console.readLine()
-    val cars = carsInput.split(",")
-    println(cars)
+    val carNames = Console.readLine().split(",")
+    println(carNames)
+    println("How many rounds will be played?")
+    val rounds = Console.readLine().toIntOrNull()
+    println(rounds)
 }
