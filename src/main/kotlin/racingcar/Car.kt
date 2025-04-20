1e76cc09021eaa2e8c7eb540ef106781425c0afb
Petra Bencze <petra.bncz@gmail.com>
2025-04-20 14:00:35 +0200

    feat(Car): new Car class
    Car class with carName and _moveCount properties, with move and printProgress methods
    and with documentation
    
    BREAKING CHANGE: new Car class

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,31 @@
+package racingcar
+
+import camp.nextstep.edu.missionutils.Randoms
+
+/**
+ * Car class
+ * @constructor primary constructor to initialize the carName
+ * @param carName: read-only property (val)
+ * @property _moveCount: mutable private property (var)
+ * @property moveCount: exposes _moveCount as read-only public property
+ * via a getter
+ * @see move: increments _moveCount based on random numbers
+ * @see printProgress: prints carName and _moveCount in a specific syntax
+ */
+class Car(val carName: String) {
+    private var _moveCount: Int = 0
+    val moveCount: Int
+        get() = _moveCount
+
+    fun move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4)
+            _moveCount++
+    }
+
+    fun printProgress() {
+        print("$carName : ")
+        for (i in 1.._moveCount)
+            print("-")
+        println()
+    }
+}
