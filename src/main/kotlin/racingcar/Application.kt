0a043a58af230b67518aef85095fc6a35a498a08
KuMMii <4.prgmng@gmail.com>
2025-04-21 01:13:32 +0900

    feat(game): add input features

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
-fun main() {
-    // TODO: Implement the program
+class Game() {
+    var round: Int = 0
+    // Get car input
+    fun nameInput(): List<String>{
+        println("Enter the names of the cars (comma-separated):")
+        val input = readLine()!!
+        return input.trim().split(",")
+    }
+
+    // Get number of rounds input
+    fun roundInput() {
+        println("How many rounds will be played?")
+        val input = readLine()!!
+        round=input.toInt()
+    }
 }
+
+fun main() {
+
+}
+
+
