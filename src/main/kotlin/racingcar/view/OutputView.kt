aef0add6cc216413e31651ad7268e1e236b8dc70
DanyilHuliaiev <danyilhuliaiev@gmail.com>
2025-04-20 14:08:40 +0200

    feat($OutputView): implement OutputView for race display
    - Create race introduction display
    - Implement round results visualization
    - Add winner announcement functionality
    - Create error message display method

---
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -0,0 +1,27 @@
+package racingcar.view
+
+import racingcar.model.Car
+import racingcar.model.Constants
+
+object OutputView {
+    fun printRaceIntro() {
+        println("\nRace Results")
+    }
+
+    fun printRoundResults(cars: List<Car>) {
+        cars.forEach { car ->
+            val positionDisplay = Constants.POSITION_MARKER.repeat(n = car.getPosition())
+            println("${car.name} : $positionDisplay")
+        }
+        println()
+    }
+
+    fun printWinners(winners: List<Car>) {
+        val winnersNames = winners.joinToString(", ") {it.name}
+        println("Winners: $winnersNames")
+    }
+
+    fun printError(message: String) {
+        println(message)
+    }
+}
