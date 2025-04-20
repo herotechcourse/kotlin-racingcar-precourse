37d544aae60dfdb2e38f8af701fccdef69cc8a19
Taekeundo <pleasepose@gmail.com>
2025-04-20 23:21:02 +0200

    chore: restore build.gradle.kts to init template
    Removed application plugin and mainClass settings. Reverted file to match the original setup. Later confirmed that build script modification is not allowed.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.24"
-    application // Enables the 'run' task for executing the main() function
 }
 
 group = "camp.nextstep.edu"
@@ -17,20 +16,9 @@ repositories {
 
 dependencies {
     implementation("com.github.woowacourse-projects:mission-utils:1.2.0")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
-    testImplementation("org.assertj:assertj-core:3.25.3")
-}
-
-// Specifies the entry point of the application.
-application {
-    mainClass.set("racingcar.ApplicationKt")
 }
 
 tasks {
-    // Setting for getting input in terminal.
-    named<JavaExec>("run") {
-        standardInput = System.`in`
-    }
     test {
         useJUnitPlatform()
     }
