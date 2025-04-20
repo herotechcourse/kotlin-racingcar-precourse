7b38f8185ebaaf560739a4e884d693266d23a579
Regan Immanuel <reganimmanuelshawn@gmail.com>
2025-04-20 21:41:00 +0200

    feat: implement car name input and validation

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,18 @@
 package racingcar
-
+import camp.nextstep.edu.missionutils.Console
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars (comma-separated):")
+    val input = Console.readLine()
+    val carNames = input.split(",").map { it.trim() }
+
+    validateCarNames(carNames)
+
+}
+
+fun validateCarNames(carNames: List<String>) {
+    for (name in carNames){
+        if(name.isEmpty() || name.length > 5) {
+            throw IllegalArgumentException("Each car name must be 1 to 5 characters long.")
+        }
+    }
 }
