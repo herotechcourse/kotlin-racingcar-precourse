985db4a83d93edb28203a9d910e26a94f7df75df
songpink <jejeje04@naver.com>
2025-04-20 17:56:12 +0900

    refactor: extract message to constant

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,11 +1,10 @@
 package racingcar.view
 
 import racingcar.model.Car
-import racingcar.model.RacingGame
 
 class OutputView {
     fun printRaceResultsMessage() {
-        println("Race Results")
+        println(MESSAGE_RACE_RESULTS)
     }
 
     fun printRaceResults(cars: List<Car>) {
@@ -19,12 +18,19 @@ class OutputView {
 
     private fun printProgress(car: Car) {
         repeat(car.position) {
-            print("-")
+            print(MESSAGE_PROGRESS_BAR)
         }
     }
 
     fun printWinners(winners: List<Car>) {
-        val winnersOutput = winners.joinToString(", ") { it.name }
-        println("Winners : ${winnersOutput}")
+        val winnersOutput = winners.joinToString(MESSAGE_SEPARATOR) { it.name }
+        println(MESSAGE_WINNERS + winnersOutput)
+    }
+
+    companion object {
+        private const val MESSAGE_RACE_RESULTS = "Race Results"
+        private const val MESSAGE_PROGRESS_BAR = "-"
+        private const val MESSAGE_SEPARATOR = ", "
+        private const val MESSAGE_WINNERS = "Winners : "
     }
 }
