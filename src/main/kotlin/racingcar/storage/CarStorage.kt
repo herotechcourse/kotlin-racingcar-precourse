1d3ce9735b617e43bf53ed043c2bece87e1c0974
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-20 14:47:45 +0900

    feat(storage): implement CarStorage with addCar and moveCar method

--- src/main/kotlin/racingcar/storage/CarStorage.kt
+++ src/main/kotlin/racingcar/storage/CarStorage.kt
@@ -1,4 +1,23 @@
 package racingcar.storage
 
+import racingcar.storage.exception.CarNotFoundException
+import racingcar.storage.exception.DuplicateCarException
+
+private val storage = mutableMapOf<String, Int>();
+private const val DEFAULT_POSITION = 0;
+
 class CarStorage {
+
+    fun addCar(name: String){
+        if(storage[name] != null)
+            throw DuplicateCarException(name);
+        storage[name] = DEFAULT_POSITION;
+    }
+
+    fun moveCar(name: String){
+        var current = storage[name];
+        if(current == null)
+            throw CarNotFoundException(name);
+        storage[name] = current + 1;
+    }
 }
