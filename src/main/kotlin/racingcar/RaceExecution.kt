cddee9a30096ec5219e73d239715d6dd23980f09
Farhana Ahmed Fima <farhana.ahmed.fima@gmail.com>
2025-04-20 23:17:57 +0200

    feat: add executeSingleRound to implement each round of race
    For each car, generate a random number between 0 and 9.
    If the number is 4 or greater, increment the car's position by 1.
    Print each car's progress after the round.

--- src/main/kotlin/racingcar/RaceExecution.kt
+++ src/main/kotlin/racingcar/RaceExecution.kt
@@ -1,12 +1,25 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
+
 fun executeCarRace(listOfCarNames: List<String>, numberOfRounds: Int): Map<String, Int> {
     val carPositions = mutableMapOf<String, Int>()
     listOfCarNames.forEach { carName ->
         carPositions[carName] = 0
     }
     repeat(numberOfRounds) {
-        //Todo: execute single-round race
+        executeSingleRound(listOfCarNames, carPositions)
+        println()
     }
     return carPositions
+}
+
+fun executeSingleRound(listOfCarNames: List<String>, carPositions: MutableMap<String, Int>) {
+    listOfCarNames.forEach { carName ->
+        val randomNumber = Randoms.pickNumberInRange(0, 9)
+        if (randomNumber >= 4) {
+            carPositions[carName] = carPositions[carName]!! + 1
+        }
+        //Todo print each car's progress
+    }
 }
