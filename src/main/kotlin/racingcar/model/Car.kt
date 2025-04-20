82c3b4f06c4084d8ffc6c5a9fd278ba00f986437
Sheyda <sheyda.sheydayi95@gmail.com>
2025-04-21 01:04:16 +0200

    feat: Edit car class

--- src/main/kotlin/racingcar/model/Car.kt
+++ src/main/kotlin/racingcar/model/Car.kt
@@ -1,12 +1,24 @@
 package racingcar.model
 
 import camp.nextstep.edu.missionutils.Randoms
-import kotlin.random.Random
 
+/**
+* Represents a car in the race
+* @param name The car's name
+* @throws IllegalArgumentException if name is empty or exceeds 5 charachters
+ */
 data class Car(val name:String) {
+
+    init {
+        validateName(name)
+    }
+
     private var position = 0
 
-    fun validateName(name: String) {
+    /**
+    * Validates that the car name meets requirements
+     */
+    private fun validateName(name: String) {
         if (name.isEmpty()){
             throw IllegalArgumentException("Car name cannot be empty")
         }
@@ -14,20 +26,30 @@ data class Car(val name:String) {
             throw IllegalArgumentException("Car name cannot exceed 5 characters: $name")
         }
     }
+
+    /**
+    * Moves the car forward if the random number is 4 or greater
+     */
     fun moveForward() {
         if (shouldMove()) {
             position++
+        }
     }
 
-    fun displayProgress(): String {
-        return "$name: ${"-".repeat(position)}"
-    }
-}
+    /**
+    * Determines if the car should move based on random number
+    * @return true if car should move, false otherwise
+     */
 
     private fun shouldMove(): Boolean {
         return Randoms.pickNumberInRange(0,9) >= 4
     }
 
+    /**
+    * Gets the current position of the car
+    * @return current position
+     */
     fun getPosition(): Int{
         return position
-    }}
+    }
+}
