4a521f4bb00c45a9de0b86f7b46a17f8745b9d62
rz-p <pramadhan9@gmail.com>
2025-04-21 00:44:03 +0200

    feat: add Car class with movement logic

---
+++ src/main/kotlin/racingcar/Car.kt
@@ -0,0 +1,11 @@
+package racingcar
+
+class Car(val name: String) {
+    var position: Int = 0
+
+    fun move(randomNumber: Int) {
+        if (randomNumber >= 4) {
+            position++
+        }
+    }
+} 
