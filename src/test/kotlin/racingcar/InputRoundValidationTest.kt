5240873bc9abec6a7a306151e85ebbdfaa7c80d1
Apoorva-S-Natesh <apoorva1326@gmail.com>
2025-04-20 13:39:09 +0200

    test: add unit test for float,out of in range input

--- src/test/kotlin/racingcar/InputRoundValidationTest.kt
+++ src/test/kotlin/racingcar/InputRoundValidationTest.kt
@@ -32,8 +32,24 @@ class InputRoundValidationTest {
 
     @Test
     fun validInput() {
-        val input = "3"
+        val input = "+3"
         val result = validateRounds(input)
         assertEquals(result ,3)
+    }
+
+    @Test
+    fun `input float number error`(){
+        val input = "1.5"
+        assertThrows<IllegalArgumentException> {
+            validateRounds(input)
+        }
+    }
+
+    @Test
+    fun `input out of integer range error`(){
+        val input = "2147483648"
+        assertThrows<IllegalArgumentException> {
+            validateRounds(input)
+        }
     }
  }
