a1e3ea176db08482ac88dfffcf27f98246bbc760
siyeonSon <siyeon0209@naver.com>
2025-04-20 18:38:31 +0900

    feat: print the winners

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -3,6 +3,7 @@ package racingcar.view
 import racingcar.model.Car
 import racingcar.util.Constant.CAR_POSITION_SYMBOL
 import racingcar.util.Constant.RACE_RESULT_MESSAGE
+import racingcar.util.Constant.WINNER_MESSAGE
 
 class OutputView {
     fun printProcess(cars: List<Car>) {
@@ -10,5 +11,9 @@ class OutputView {
         cars.forEach { car ->
             println("${car.name} : ${CAR_POSITION_SYMBOL.repeat(car.position)}")
         }
+    }
+
+    fun printWinner(winner: List<Car>) {
+        println("$WINNER_MESSAGE ${winner.joinToString(", ") { it.name }}")
     }
 }
