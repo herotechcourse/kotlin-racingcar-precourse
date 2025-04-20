a10aa66c0555b423916991f3bcc373ef2deba080
AtomicLiquors <95601940+AtomicLiquors@users.noreply.github.com>
2025-04-20 19:07:51 +0900

    feat(race): create main game logic within raceManager.kt

--- src/main/kotlin/racingcar/race/raceManager.kt
+++ src/main/kotlin/racingcar/race/raceManager.kt
@@ -1,4 +1,39 @@
 package racingcar.race
 
-class raceManager {
+import racingcar.output.addLine
+import racingcar.repository.Car
+import racingcar.repository.CarRepository
+import racingcar.repository.ListCarRepository
+
+private val carRepository: CarRepository = ListCarRepository();
+private var topScore = 0;
+fun race(cars: Array<String>, rounds: Int){
+    addLine();
+    addLine("Race Results");
+
+    carRepository.addMultipleCars(cars);
+
+    for (i in 1..rounds) {
+        val carList: List<Car> = carRepository.getAllCars();
+        carList.forEach { car ->
+            run {
+                val currentCarDistance = car.moveForwardRandomly();
+                topScore = topScore.coerceAtLeast(currentCarDistance);
+                addLine(car.toString());
+            }
+        }
+        addLine();
+    }
+
+    val winners: MutableList<String> = mutableListOf();
+    val carList: List<Car> = carRepository.getAllCars();
+    carList.forEach {
+        car -> run {
+            if(car.getDistance() == topScore){
+                winners.add(car.getName());
+            }
+        }
+    }
+    val result = winners.joinToString(", ");
+    addLine("Winners : $result");
 }
