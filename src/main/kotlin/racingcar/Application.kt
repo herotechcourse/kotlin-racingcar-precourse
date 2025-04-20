c6d10f4bf71ea4888abb1c57f5f49ee3c9ee1192
yojin <roadlroal@naver.com>
2025-04-20 19:28:32 +0100

    feat(car): add Car class in Application.kt
    - private variables String name, int movedRounds with getter
    - implement function runRound that process round
    - implement function printStatus that print car name and current movedRounds

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,6 +1,29 @@
 package racingcar
 
-// initial!!
+import camp.nextstep.edu.missionutils.Randoms
+import camp.nextstep.edu.missionutils.Console
+
+const val MOVE_THRESHOLD = 4
+
+class Car(name: String) {
+    private val _name: String = name
+    val name: String
+        get() = _name
+
+    private var _movedRounds: Int = 0
+    val movedRounds: Int
+        get() = _movedRounds
+
+    fun runRound() {
+        if (Randoms.pickNumberInRange(0, 9) >= MOVE_THRESHOLD) {
+            _movedRounds++
+        }
+    }
+
+    fun printStatus() {
+        println("$name: ${"-".repeat(_movedRounds)}")
+    }
+}
 
 fun main() {
     // TODO: Implement the program
