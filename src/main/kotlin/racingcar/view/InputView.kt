b33601fe8fd15ed9418af3b8e3fbc94b08e901e3
siyeonSon <siyeon0209@naver.com>
2025-04-20 18:21:04 +0900

    feat: receive the number of rounds from the user

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -2,10 +2,16 @@ package racingcar.view
 
 import racingcar.util.Constant.CAR_NAME_DELIMITER
 import racingcar.util.Constant.CAR_NAME_MESSAGE
+import racingcar.util.Constant.ROUND_COUNT_MESSAGE
 
 class InputView {
     fun inputCarNames(): List<String> {
         println(CAR_NAME_MESSAGE)
         return readln().split(CAR_NAME_DELIMITER).map { it.trim() }
+    }
+
+    fun inputRoundCount(): Int {
+        println(ROUND_COUNT_MESSAGE)
+        return readln().toInt()
     }
 }
