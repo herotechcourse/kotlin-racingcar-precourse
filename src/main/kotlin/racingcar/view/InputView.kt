da2477c90695e9f13653b8a6b39fa81e50578ebb
songpink <jejeje04@naver.com>
2025-04-20 17:53:01 +0900

    refactor: extract message to constant

--- src/main/kotlin/racingcar/view/InputView.kt
+++ src/main/kotlin/racingcar/view/InputView.kt
@@ -5,12 +5,17 @@ import racingcar.model.Car
 
 class InputView {
     fun receiveCarNamesInput(): List<Car> {
-        println("Enter the names of the cars (comma-separated):")
+        println(MESSAGE_INPUT_CAR_NAMES)
         return Console.readLine().split(",").map { Car(it) }
     }
 
     fun receiveNumberOfRounds(): Int {
-        println("How many rounds will be played?")
+        println(MESSAGE_INPUT_NUMBER_OF_ROUNDS)
         return Console.readLine().toInt()
+    }
+
+    companion object {
+        private const val MESSAGE_INPUT_CAR_NAMES = "Enter the names of the cars (comma-separated):"
+        private const val MESSAGE_INPUT_NUMBER_OF_ROUNDS = "How many rounds will be played?"
     }
 }
