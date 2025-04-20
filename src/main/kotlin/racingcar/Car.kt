4ff9a224fe5da4e2038b2a3ad90ac5e631fdc14f
KuMMii <4.prgmng@gmail.com>
2025-04-21 01:13:49 +0900

    feat(car): add name setter

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -2,17 +2,29 @@ package racingcar
 
 import kotlin.random.Random
 
-class Car(val name: String) {
+class Car() {
+    var name: String = ""
     var result: String =""
-    var move: Int = 0
+
+
+    // Set the name
+    fun setName(name: String) {
+        if (name.isNotBlank() && name.length < 5) {
+            this.name =name
+        } else {
+            throw IllegalArgumentException("Name must be between 1 and 5")
+        }
+    }
 
     // Generate a random number
-    fun generateNumber() {
-        move = Random.nextInt(0,10)
+    fun generateNumber(): Int {
+        return Random.nextInt(0,10)
     }
 
     // Move(update the result)
-    fun moveCar() {
-        result += "-".repeat(move)
+    fun moveCar(move: Int) {
+        if (move>=4){
+            result += "-".repeat(move)
+        }
     }
 }
