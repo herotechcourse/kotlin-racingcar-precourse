d49ca376cc69cfb8f124fd2bc4fa2e44aed58133
jvnlee <lhjun1028@naver.com>
2025-04-21 21:35:26 +0900

    feat: add main application logic

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import racingcar.domain.RacingGame
+import racingcar.view.InputView
+import racingcar.view.ResultView
+
 fun main() {
-    // TODO: Implement the program
+    val carNames = InputView.readCarNames()
+    val roundNumber = InputView.readRoundNumber()
+
+    val game = RacingGame(carNames)
+
+    println("\nRace Results")
+
+    repeat(roundNumber) {
+        game.playRound()
+        ResultView.printRound(game.cars)
+    }
+
+    ResultView.printWinners(game.findWinners())
 }
