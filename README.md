3141ba99b57ebf2b956345a2155b4812edc62c9f
Yu Jin <qldls0307@naver.com>
2025-04-21 23:15:09 +0900

    docs: add an annotation

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # kotlin-racingcar-precourse
+
+## implementation
+
+1
+
+## Commit tag
+
+- feat (feature)
+- fix (bug fix)
+- docs (documentation)
+- style (formatting, missing semi colons, …)
+- refactor
+- test (when adding missing tests)
+- chore (maintain)
--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -8,14 +8,19 @@ fun main() {
 
     /**
      * 1) The user should be able to specify car names and the number of rounds.
-     * 2) Car names should be comma-separated when entered by the user.
-     * 3) Each car must have a name, and names cannot exceed 5 characters.
+     * - Car names should be comma-separated when entered by the user.
+     * - Each car must have a name, and names cannot exceed 5 characters.
      */
 
     println("Enter the names of the cars (comma-separated):")
     val list = Console.readLine().split(",") //todo 공백제거
-    for(item in list){
-        if (item.count()>5){
+    for (item in list) {
+
+        /**
+         * 6) If the user inputs invalid data, the program should throw an IllegalArgumentException and terminate.
+         *
+         */
+        if (item.count() > 5) {
             throw IllegalArgumentException()
         }
     }
@@ -38,24 +43,36 @@ fun playGame(gameSet: List<Game>, roundNumber: Int): List<String> {
 
     println("Race Results")
 
-    for (i in 1..roundNumber){
+    for (i in 1..roundNumber) {
 
-        for (play in gameSet){
-            val num = Randoms.pickNumberInRange(0,9)
+        for (play in gameSet) {
+            val num = Randoms.pickNumberInRange(0, 9)
 
-            if(num>4){
-                play.runCount+=1
-
+            /**
+             * 2) Each of the n cars can either move forward or stay still during a given number of rounds.
+             * 3) A car moves forward if a randomly generated number between 0 and 9 is 4 or greater.
+             */
+            if (num > 4) {
+                play.runCount += 1
             }
+
+            /**
+             * 4) When printing the progress of each car, display the car's name alongside its movement.
+             *
+             */
+
             print("${play.playerName}: ")
-                printRunCount(play.runCount)
+            printRunCount(play.runCount)
             println()
         }
         println()
     }
 
 
-
+    /**
+     * 5) After the race is complete, display the winners. There can be multiple winners if they reach the same final position.
+     * - If there are multiple winners, their names should be displayed and separated by commas.
+     */
     val maxCount = gameSet.maxOfOrNull { it.runCount }
     val winnerList = gameSet.filter { it.runCount == maxCount }
         .map { it.playerName }
@@ -63,8 +80,8 @@ fun playGame(gameSet: List<Game>, roundNumber: Int): List<String> {
     return winnerList
 }
 
-fun printRunCount(count: Int){
-    for (i in 1..count){
+fun printRunCount(count: Int) {
+    for (i in 1..count) {
         print("-")
     }
 }
