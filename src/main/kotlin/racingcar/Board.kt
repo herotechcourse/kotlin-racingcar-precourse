5ef534451802628e703330d59600b6fe98f01761
KuMMii <4.prgmng@gmail.com>
2025-04-21 19:26:41 +0900

    feat(Board): add checking winner

--- src/main/kotlin/racingcar/Board.kt
+++ src/main/kotlin/racingcar/Board.kt
@@ -1,11 +1,12 @@
 package racingcar
 
 class Board {
-    var cars: List<Car> = listOf()
+    var cars: MutableList<Car> = mutableListOf()
+        private set
 
     // Set cars
-    fun setCars(newCars: List<Car>) {
-        cars = newCars
+    fun setCars(newCar: Car) {
+        cars.add(newCar)
     }
 
     // Print a round
@@ -19,6 +20,17 @@ class Board {
     // Check the winners
     fun checkWinner(): List<String> {
         var winners = mutableListOf<String>()
+        var max = 0
+
+        for (car in cars) {
+            if (car.result.length > max) {
+                winners = mutableListOf<String>()
+                winners.add(car.name)
+                max = car.result.length
+            } else if (car.result.length == max) {
+                winners.add(car.name)
+            }
+        }
 
         return winners
     }
