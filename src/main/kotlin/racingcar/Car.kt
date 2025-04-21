caa7529781c0d2d81e3a60b1c514170bc99381c8
jslusark <slusark.jessica@gmail.com>
2025-04-21 15:48:27 +0200

    refactor(Car): clean up Car class by removing unused debug code
    - Removed data used as initial logic building and debugging
    - Removed useless comments

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -4,21 +4,12 @@ import camp.nextstep.edu.missionutils.Randoms
 
 class Car(val name: String) {
 
-  val id: Int = nextId++ // thought could be useful to set an id for every car
-
-  companion object {
-    private var nextId: Int = 1
-  }
-
   var position: Int = 0
-    private set // decided to keep the position private, to prevent unepected behaviour
+    private set
 
-  fun attemptMovement(): Int {
+  fun attemptMovement() {
     val random = Randoms.pickNumberInRange(0, 9)
-    if (random >= 4) {
-      position += 1
-    }
-    return random // added for simple debugging purposes
+    if (random >= 4) { position++ }
   }
 
   fun progressBar(): String = "-".repeat(position)
