828a8453f80d943621078b1bf7c617955825ce47
maryleebu <marylee.buerckner@aol.com>
2025-04-21 23:52:59 +0200

    fix double Application mention

--- src/test/kotlin/ApplicationTest.kt
+++ src/test/kotlin/ApplicationTest.kt
@@ -1,12 +1,17 @@
-package racingcar
-import org.junit.jupiter.api.Test
+  package racingcar
 
+import org.junit.jupiter.api.Test
+import org.assertj.core.api.Assertions.assertThat
 
 class ApplicationTest {
+
   @Test
-  fun `runs the game without crashing`() {
-    Application.main(arrayOf())
+  fun `car moves forward when random is 4 or more`() {
+    val car = Car("test")
+    
+    repeat(750) {
+      car.moveIfPossible()
+    }
+    assertThat(car.position).isGreaterThan(0)
   }
 }
-
-  
