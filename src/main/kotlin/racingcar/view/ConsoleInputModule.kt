aea8598d3c204a8337beed9bb7a909f8b2ffc4d3
Taekeundo <pleasepose@gmail.com>
2025-04-21 23:58:19 +0200

    final test

--- src/main/kotlin/racingcar/view/ConsoleInputModule.kt
+++ src/main/kotlin/racingcar/view/ConsoleInputModule.kt
@@ -1,22 +1,18 @@
 package racingcar.view
+import camp.nextstep.edu.missionutils.Console
 
 class ConsoleInputModule : InputModule {
     // 1. Read carNames
     override fun readCarNames(): String {
         println("Enter the names of the cars (comma-separated):")
-        val carNames = readLine()
-        if (carNames == null) {
-            return ""
-        }
+        val carNames = Console.readLine()
         return carNames
     }
+
     // 2. Read rounds
     override fun readRounds(): String {
         println("How many rounds will be played?")
-        val nRounds = readLine()
-        if (nRounds == null) {
-            return ""
-        }
+        val nRounds = Console.readLine()
         return nRounds
     }
 }
