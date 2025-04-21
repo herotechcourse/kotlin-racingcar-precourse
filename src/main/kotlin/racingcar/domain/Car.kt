1b577389dae25a714bc60f0f96aa79eed51b0702
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-04-22 01:41:23 +0900

    refactor(domain): inject random number generator into Car for more reliable test code
    - replace internal call to Randoms.pickNumberInRange with injectable lambda
    - allows fixed number in random number generation while testing, gaining more reliable test code

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,21 +1,21 @@
 package racingcar.domain
 import camp.nextstep.edu.missionutils.Randoms
 
-class Car(val name: String, private var position: Int = 0) {
+class Car(
+    val name: String,
+    private var position: Int = 0,
+    private val randomGenerator: () -> Int = { Randoms.pickNumberInRange(0, 9) }) {
     init {
         require(name.isNotEmpty() && name.length <= 5) { "Car name must be between 1 and 5 characters" }
     }
 
     fun moveIfRandomNumberIsOverFour(){
-        var randNum = getRandNumber()
-        if(randNum >= 4){
+        if(randomGenerator() >= 4){
             moveForward()
         }
     }
 
     private fun moveForward() = position++
-
-    private fun getRandNumber():Int= Randoms.pickNumberInRange(0,9)
 
     fun getPosition(): Int = position
 }
