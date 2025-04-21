c822531e64385f790df6461a0ce5de5bfa782fe2
downback <daeun.down.back@gmail.com>
2025-04-21 16:03:14 +0200

    feat(input): implement user input for car names and racing rounds
    - Prompted user for comma-separated car names using Console.readLine()
    - Prompted user for number of racing rounds
    - Temporarily printed parsed input for testing purposes
    - Input validation will be handled separately in the next feature

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,14 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
 fun main() {
-    // TODO: Implement the program
+    println("Enter the names of the cars (comma-separated):")
+    val carNamesInput = Console.readLine().split(",")
+
+    println("How many rounds will be played?")
+    val roundInput = Console.readLine()
+
+    println("Car Names: ${carNamesInput[0]}, ${carNamesInput[1]}, ${carNamesInput[2]}")
+    println("Racing Round: $roundInput Round")
 }
