c9bb73b22ed51f072694725cf76fee3c4850fae3
Ahmed Salah <ahmedelshaikh51@gmail.com>
2025-04-21 15:22:54 +0200

    feat(parser): enforce unique car names

--- src/main/kotlin/racingcar/parser/Parser.kt
+++ src/main/kotlin/racingcar/parser/Parser.kt
@@ -5,8 +5,16 @@ import racingcar.model.Car
 
 object Parser {
   fun parseCars(input: String): MutableList<Car> {
-    return input.split(",")
+
+    val names = input
+      .split(",")
       .map { it.trim() }
+
+    require(names.size == names.distinct().size) {
+      ErrorMessages.ERROR_DUPLICATE_CAR_NAME
+    }
+
+    return names
       .onEach {
         require(it.isNotEmpty()) { ErrorMessages.ERROR_EMPTY_CAR_NAME }
         require(it.length <= 5) { ErrorMessages.ERROR_LONG_CAR_NAME }
