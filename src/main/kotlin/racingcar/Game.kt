ae39453621530a7b8af0cc8b8792767b84e0986c
cat <myakoven@student.42berlin.de>
2025-04-21 03:01:19 +0200

    fix(game): restore required formatting with space after "Winners"
    "Winners : " instead of "Winners: "

--- src/main/kotlin/racingcar/Game.kt
+++ src/main/kotlin/racingcar/Game.kt
@@ -5,8 +5,8 @@ import camp.nextstep.edu.missionutils.Randoms
 class Game(val cars: List<Car>) {
     companion object {
         const val moveIndicator: Int = 4
-		const val randMin: Int = 0
-		const val randMax: Int = 9
+        const val randMin: Int = 0
+        const val randMax: Int = 9
     }
 
     private var totalRounds = 0
@@ -19,17 +19,21 @@ class Game(val cars: List<Car>) {
 
     private fun raceThem() {
         for (i in 0 until totalRounds) {
-            cars.forEach { car ->
-                if (Randoms.pickNumberInRange(randMin, randMax) >= moveIndicator) car.move()
-                car.printRoundStatus()
-            }
+            playRound()
             println("")
+        }
+    }
+
+    private fun playRound() {
+        for (car in cars) {
+            if (Randoms.pickNumberInRange(randMin, randMax) >= moveIndicator) car.move()
+            car.printRoundStatus()
         }
     }
 
     private fun printWinners() {
         var maxWon = cars.maxOf { car -> car.getMovesMade() }
         val winners = cars.filter { car -> car.getMovesMade() == maxWon }
-		print("Winners : " + winners.joinToString(", ") { it.getName() })
-	}
+        print("Winners : " + winners.joinToString(", ") { it.getName() })
+    }
 }
