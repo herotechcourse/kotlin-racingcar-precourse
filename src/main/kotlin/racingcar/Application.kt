fdf0a9a4716f5d9c7e4e96f23a788940c3edc511
minsang <minsang007@gmail.com>
2025-04-21 21:14:31 +0900

    feat(main): add user input for number of rounds
    Add a prompt to receive the number of race rounds from the user.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -5,12 +5,15 @@ import racingcar.validation.CarNameValidator
 
 fun main() {
     // TODO: Implement the program
-    print("Car names : ")
+    print("Names of the cars: ")
     val input = readln()
 
     val carNames = input.split(",")
     CarNameValidator.validateCarNames(carNames)
     val cars = CarFactory.createCars(carNames)
 
-    println("등록된 자동차: ${cars.map { it.name }}")
+    print("Number of rounds: ")
+    val n = readln().toInt()
+    print("round input: $n")
 }
+
