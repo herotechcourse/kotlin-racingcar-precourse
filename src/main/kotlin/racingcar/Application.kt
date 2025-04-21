fccb516cd495b88b21554f352acf5a382ab9b18c
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-04-21 17:59:00 +0200

    feat(main): Implement main function to run car racing simulation
    Implement main function:
    - get user input for car names
    - get user input for number of rounds
    - initialize Race instance
    - start race simulation

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,12 @@
 package racingcar
 
+import racingcar.models.Car
+import racingcar.models.InputHandler
+import racingcar.models.Race
+
 fun main() {
     // TODO: Implement the program
+    val cars : List<Car> = InputHandler.getCars()
+    val numOfRounds = InputHandler.getNumberOfRounds()
+    Race(cars, numOfRounds).start()
 }
