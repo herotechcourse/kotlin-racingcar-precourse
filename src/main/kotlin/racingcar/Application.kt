15cdb6829a408433c02482fb816697f3426d9ad0
Alina Gabriela Alina <alinaghetler4@gmail.com>
2025-04-21 14:53:03 +0200

    feat(carMovement): implement car movement feature

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,28 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
-    val names = readln().split(",").map { it.trim() }
+    val names = Console.readLine().split(",").map { it.trim() }
     if (names.any {!it.matches(Regex("^[A-Za-z]+$")) || it.length > 5 }) {
         throw IllegalArgumentException("Invalid name")
+    }
+
+    val rounds = Console.readLine().toInt()
+
+    val results = mutableMapOf<String, Int>()
+
+    for (name in names) {
+        results[name] = 0
+    }
+
+    repeat(rounds) {
+        for (name in names) {
+            val randomNumber = Randoms.pickNumberInRange(0, 9)
+            if (randomNumber >= 4) {
+                results[name] = results[name]!! + 1
+            }
+        }
     }
 }
