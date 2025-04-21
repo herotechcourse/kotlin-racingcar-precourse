3bc8573a5518cd58e7efb81ff172101892134ed2
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2025-04-21 19:36:22 +0200

    feat: add empty name check in Car constructor

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -6,11 +6,17 @@ class Car (val name: String) {
     var steps: Int = 0
 
     init {
+        checkName()
+    }
+
+    fun checkName () {
+        if (name.length == 0) {
+            throw IllegalArgumentException("Name is empty")
+        }
         if (name.length > 5) {
             throw IllegalArgumentException("Name cannot exceed 5 characters")
         }
     }
-
     fun move() {
         val randomNumber = Randoms.pickNumberInRange(0, 9)
         if (randomNumber >= 4) {
