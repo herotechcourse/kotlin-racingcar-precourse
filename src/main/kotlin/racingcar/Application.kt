b7355e6f579e166d400def4cfee31d8313e2f4b4
Anushka Raman <anushkaraman93@gmail.com>
2025-04-21 12:44:06 +0200

    feat(car-racing): Implementation for car-racing along with validations

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,24 +1,29 @@
 package racingcar
 
 import camp.nextstep.edu.missionutils.Randoms
-import java.io.Console
-import java.util.*
+import camp.nextstep.edu.missionutils.Console
 
 
 fun main() {
     val cars = getCarNames()
     val rounds = getRounds()
 
-    println(cars)
-    println(rounds)
+    repeat(rounds) {
+        moveCar(cars)
+    }
+
+    getWinner(cars)
 }
 
 
-fun getCarNames(): Map<String, String> {
-    println("Enter the names of the cars (comma-separated):")
+fun getCarNames(): MutableMap<String, String> {
     val cars = mutableMapOf<String, String>()
-
-    if (cars.isEmpty() || cars.any { it.value.isBlank() || it.value.length > 5 }) {
+    println("Enter the names of the cars (comma-separated):")
+    Console.readLine().split(',').map{ it.trim()
+     }.map{
+        cars[it] = ""
+    }
+    if (cars.any { it.key.isBlank() || it.key.length > 5 }) {
         throw IllegalArgumentException("Car names should be 1–5 characters.")
     }
     return cars
@@ -26,6 +31,28 @@ fun getCarNames(): Map<String, String> {
 
 fun getRounds(): Int {
     println("How many rounds will be played?")
-    return readln().toInt()
+    val rounds =  Console.readLine().toInt()
+    if (rounds <= 0) throw IllegalArgumentException("Rounds should be at least 1 or more.")
+    return rounds
 }
 
+fun moveCar(cars: MutableMap<String, String>) {
+    for(car in cars) {
+        if(Randoms.pickNumberInRange(0, 9) >= 4) {
+            cars[car.key] += "-"
+        }
+        println("${car.key} : ${car.value}")
+    }
+}
+
+fun getWinner(cars: MutableMap<String, String>) {
+    val winner = mutableListOf<String>()
+    val winnerScore: Int = cars.maxOf { it.value.length }
+    for(car in cars) {
+        if(car.value.length == winnerScore) {
+            winner.add(car.key)
+
+        }
+    }
+    println("Winners : ${winner.joinToString { it }}")
+}
