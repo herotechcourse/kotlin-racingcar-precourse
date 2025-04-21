a76674d0d9c295c8f2d4f2b824049c352be44d28
sam971114 <simonana@naver.com>
2025-04-22 04:20:49 +0900

    feat(core) : define Car class with name and position
    Set initial position 0 and car name is set by constructor
    
    Refs: #1

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,20 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+import camp.nextstep.edu.missionutils.Randoms
+
 fun main() {
     // TODO: Implement the program
+}
+
+class Car(val name: String) {
+    var position = 0
+        private set
+
+    fun move() {
+        if (Randoms.pickNumberInRange(0, 9) >= 4) {
+            position++
+        }
+    }
+    fun getPositionSymbol(): String = "-".repeat(position)
 }
