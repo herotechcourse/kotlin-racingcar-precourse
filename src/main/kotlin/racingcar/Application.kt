51c8fec0ec615b81446fcf97647febb1db235170
rwaeng <rwaeng0421@gmail.com>
2025-04-22 00:56:29 +0900

    feat(main): implement main application flow to run the game

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,24 @@
 package racingcar
 
+import racingcar.domain.Car
+import racingcar.domain.Race
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
 fun main() {
-    // TODO: Implement the program
+    val names: List<String> = InputView.getCarNames()
+    val rounds: Int = InputView.getRounds()
+
+    val cars = names.map { Car(it) }.toList()
+    val race = Race(cars, rounds)
+
+    val roundResults = race.start()
+
+    OutputView.printResultIntro()
+    roundResults.forEach { roundSnapshot ->
+        OutputView.printResults(roundSnapshot)
+    }
+
+    val winners = race.getWinners()
+    OutputView.printWinners(winners)
 }
