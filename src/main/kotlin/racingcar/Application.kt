0a896dd4a8dae8517c1ba3c02147cfd2b23e32b8
minsang <minsang007@gmail.com>
2025-04-21 21:30:09 +0900

    feat(main): 메ãapply InputNumberValidator to round input
    Add validation step in main() to prevent invalid round inputs.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,6 +2,7 @@ package racingcar
 
 import racingcar.factory.CarFactory
 import racingcar.validation.CarNameValidator
+import racingcar.validation.InputNumberValidator
 
 fun main() {
     // TODO: Implement the program
@@ -14,6 +15,9 @@ fun main() {
 
     print("Number of rounds: ")
     val n = readln().toInt()
+
+    InputNumberValidator.validateInputNumber(n)
+
     print("round input: $n")
 }
 
