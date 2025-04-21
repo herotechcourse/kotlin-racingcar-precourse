5f1eafa9a5d6817e49048dd2975291aa86a08a7a
YongLeeCode <yonghwan311@gmail.com>
2025-04-21 22:44:20 +0900

    refactor: update main for refactoring

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,18 +2,18 @@ package racingcar
 
 import racingcar.io.Input
 import racingcar.io.Output
+import racingcar.randomNumber.RandomNumberImpl
 
 fun main() {
     println("Enter the names of the cars (comma-separated):")
-    val nameInput: String = Input().input()
-    val names: List<String> = Input().getNames(nameInput)
+    val names: List<String> = Input().getNames(Input().input())
 
     println("How many rounds will be played?")
-    val roundInput: String = Input().input()
-    val round: Int = Input().getRound(roundInput)
+    val round: Int = Input().getRound(Input().input())
 
-    val (raceResultMap, winners) = RacingService(names, round).play()
-    val output = Output()
-    output.displayRaceResults(raceResultMap)
-    output.displayWinners(winners)
+    val cars: List<Car> = names.map { Car(it) }
+    val randomNumbers = List(round){RandomNumberImpl().generate(names.size)}
+    val (raceResultMap, winners) = RacingService(cars, randomNumbers).play()
+
+    Output(raceResultMap, winners)
 }
