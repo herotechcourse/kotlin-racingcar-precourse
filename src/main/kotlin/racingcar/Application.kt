48e6ec89f0a0ee270949a08a3503c87dfa17bcd0
downback <daeun.down.back@gmail.com>
2025-04-21 16:51:53 +0200

    feat(input): implement validation for car names
    - Validates that input is not blank
    - Ensures no empty names exist between commas
    - Rejects any name longer than 5 characters
    - Displays all valid car names using joinToString()

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,13 +2,39 @@ package racingcar
 
 import camp.nextstep.edu.missionutils.Console
 
+
+fun getValidName(): List<String> {
+    while (true) {
+        println("Enter the names of the cars (comma-separated):")
+        val carNamesInput = Console.readLine().trim()
+
+        if(carNamesInput.isBlank()) {
+            throw IllegalArgumentException("You didn't type anything!")
+        }
+
+        val carNames = carNamesInput.split(",").map { it.trim() }
+
+        if (carNames.any { it.isBlank() }) {
+            throw IllegalArgumentException("Each car must have a name — no blanks allowed between commas.")
+        }
+
+        val invalidNames = carNames.filter { it.length > 5 }
+        if (carNames.any { it.length > 5 }) {
+            throw IllegalArgumentException("Car names must be 1–5 characters long. Problem with: ${invalidNames.joinToString()}")
+        }
+
+        return carNames
+
+    }
+
+}
+
 fun main() {
-    println("Enter the names of the cars (comma-separated):")
-    val carNamesInput = Console.readLine().split(",")
+    val carNames = getValidName()
 
     println("How many rounds will be played?")
     val roundInput = Console.readLine()
 
-    println("Car Names: ${carNamesInput[0]}, ${carNamesInput[1]}, ${carNamesInput[2]}")
+    println("Car Names: ${carNames.joinToString(", ")}")
     println("Racing Round: $roundInput Round")
 }
