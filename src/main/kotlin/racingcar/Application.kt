6a4e38bbe56be4dd20ac75a483fa858a85e6eaa4
minsang <minsang007@gmail.com>
2025-04-22 01:10:27 +0900

    refactor(main): change winner logic in main by using Winner class
    Now the main function simply calls winner.findWinners() and winner.displayWinners().

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,19 +3,20 @@ package racingcar
 import camp.nextstep.edu.missionutils.Randoms
 import racingcar.factory.CarFactory
 import racingcar.model.Car
+import racingcar.model.Winner
 import racingcar.validation.CarNameValidator
 import racingcar.validation.InputNumberValidator
 
 fun main() {
     // TODO: Implement the program
-    print("Names of the cars: ")
+    println("Enter the names of the cars (comma-separated): ")
     val carInput = readln()
 
     val carNames = carInput.split(",")
     CarNameValidator.validateCarNames(carNames)
     val cars = CarFactory.createCars(carNames)
 
-    print("Number of rounds: ")
+    println("How many rounds will be played?")
     val roundInput = readln()
 
     val n = InputNumberValidator.validateInputNumber(roundInput)
@@ -31,23 +32,8 @@ fun main() {
         println()
     }
 
-    val winners = mutableListOf<Car>()
-    for (car in cars) {
-        if (winners.isEmpty()) {
-            winners.add(car)
-            continue
-        }
-        if (car.position > winners.last().position) {
-            winners.clear()
-            winners.add(car)
-        } else if (car.position == winners.last().position) {
-            winners.add(car)
-        } else if (car.position < winners.last().position && winners.size > 1) {
-            winners.removeLast()
-        } else {
-            continue
-        }
-    }
-    print("Winners : " + winners.joinToString(", ") { it.name })
+    val winner = Winner(cars)
+    winner.findWinners()
+    winner.displayWinners()
 }
 
