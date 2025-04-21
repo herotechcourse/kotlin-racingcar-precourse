c4a1585265ee8d840d9eb220f79a7cd72a67b62d
Burcu <burcu@MacBook-Air-von-Burcu.local>
2025-04-21 17:14:23 +0200

    test: add unit test for Car.move() position logic

---
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -0,0 +1,15 @@
+package racingcar
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.RepeatedTest
+
+class CarTest {
+    // move() position logic test
+    @RepeatedTest(10)
+    fun `move function should either increase position by 1 or keep it same`() {
+        val car = Car("pobi")
+        val initialPosition = car.position
+        car.move()
+        assertThat(car.position).isBetween(initialPosition, initialPosition + 1)
+    }
+}
