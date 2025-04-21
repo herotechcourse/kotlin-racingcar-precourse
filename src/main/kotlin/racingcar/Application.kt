aa3e1326e8c30277899aa343f044711235ef8a68
rlamsp <rlamsp234@naver.com>
2025-04-21 22:43:00 +0900

    feat: create a Car Racing class

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -14,6 +14,48 @@ class Car(val name: String, private val canMove: () -> Boolean) {
     }
 }
 
+class CarRacing(private val canMove: () -> Boolean) {
+    private var cars: List<Car> = listOf()
+    private var roundCount: Int = 0
+
+    fun setCars(input: String) {
+        val names = input.split(",").map { it.trim() }
+        cars = names.map { name -> Car(name, canMove)}
+    }
+
+    fun setRoundCount(input: Int) {
+        roundCount = input
+    }
+
+    fun race() {
+        println()
+        println("Race Results")
+        repeat(roundCount) {
+            for (car in cars) {
+                car.moveForward()
+                car.printStatus()
+            }
+            println()
+        }
+        getWinners()
+    }
+
+    private fun isValidNameLength(names: List<String>): Boolean {
+        return !names.any { it.length > 5 }
+    }
+
+    private fun getWinners() {
+        val max = cars.maxOfOrNull { it.position } ?: 0
+        val winners = cars.filter { it.position == max }
+        printResult(winners)
+    }
+
+    private fun printResult(winners: List<Car>) {
+        val names = winners.joinToString(", ") { it.name }
+        println("Winners : $names")
+    }
+}
+
 fun main() {
     // TODO: Implement the program
 }
