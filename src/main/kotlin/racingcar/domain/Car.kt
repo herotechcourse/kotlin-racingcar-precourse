ed3a3ba33f9706db1dc6847aa3cf57d377c37474
서창덕 <44630705+Garodden@users.noreply.github.com>
2025-04-21 21:33:50 +0900

    refactor(domain): chanve position field of Car class to private

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -1,8 +1,11 @@
 package racingcar.domain
 
 
-class Car(val name: String, var position: Int = 0) {
+class Car(val name: String, private var position: Int = 0) {
     init {
         require(name.isNotEmpty() && name.length <= 5) { "Car name must be between 1 and 5 characters" }
     }
+
+
+
 }
