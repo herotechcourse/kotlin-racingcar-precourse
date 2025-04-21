9138f700d007332aac15ef2fb0048eb39742d65b
haebyun <bhk4392@gmail.com>
2025-04-21 20:18:19 +0900

    feat: implement move logic when random number >= 4

--- src/main/kotlin/racingcar/model/RacingCar.kt
+++ src/main/kotlin/racingcar/model/RacingCar.kt
@@ -1,8 +1,16 @@
 package racingcar.model
 
+private const val MOVE_TRIGGER_VALUE = 4
+
 class RacingCar(
     private val name: String,
-    private val position: Int
+    private var position: Int
 ) {
     constructor(name: String) : this(name, 0)
+
+    fun moveForwardIfPossible(randomNumber: Int) {
+        if(randomNumber >= MOVE_TRIGGER_VALUE) {
+            position++
+        }
+    }
 }
