509bd11b29efc14b9f22373a97115ef5adf01250
rlamsp <rlamsp234@naver.com>
2025-04-21 22:41:52 +0900

    feat: create a Car class

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,19 @@
 package racingcar
 
+class Car(val name: String, private val canMove: () -> Boolean) {
+    var position: Int = 0
+        private set
+    fun moveForward() {
+        if (canMove()) {
+            position++
+        }
+    }
+
+    fun printStatus() {
+        println("$name : ${"-".repeat(position)}")
+    }
+}
+
 fun main() {
     // TODO: Implement the program
 }
