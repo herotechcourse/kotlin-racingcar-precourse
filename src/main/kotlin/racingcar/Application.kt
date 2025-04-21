82a5acb9c377e94582e9a47c63ffbe96ea543537
netabirami <net.abirami@gmail.com>
2025-04-21 08:28:33 +0200

    feat(validations): add the validations

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,19 +3,38 @@ import camp.nextstep.edu.missionutils.Console
 import camp.nextstep.edu.missionutils.Randoms
 
 fun main() {
-    println("Names of the cars (comma-separated):")
-    val carNamesInput = Console.readLine()
-    val carNames = carNamesInput.split(",").map { it.trim() }
+    val carNames = getCarNames()
 
-    if (carNames.any { it.length > 5 }) {
-        throw IllegalArgumentException("Car names should be 5 characters or less.")
-    }
-
-    println("Number of rounds ?")
-    val roundCount = Console.readLine().toInt()
-
+    val roundCount = getRounds()
     val cars = carNames.map { Car(it) }
 
+    runTheRace(roundCount, cars)
+
+    announceTheWinners(cars)
+}
+
+private fun getRounds(): Int {
+    println("Number of rounds ?")
+    val input = Console.readLine()
+
+    return try {
+        val rounds = input.toInt()
+        if (rounds <= 0) {
+            throw IllegalArgumentException("Round cannot 0 or lower")
+        }
+        rounds
+    } catch (e: NumberFormatException) {
+        throw IllegalArgumentException("Round must be a valid number")
+    }
+}
+
+private fun announceTheWinners(cars: List<Car>) {
+    val maxPosition = cars.maxOf { it.position }
+    val winners = cars.filter { it.position == maxPosition }.joinToString(", ") { it.name }
+    println("Winners : $winners")
+}
+
+private fun runTheRace(roundCount: Int, cars: List<Car>) {
     println("\nRace results by round:")
     repeat(roundCount) {
         cars.forEach { car ->
@@ -27,10 +46,21 @@ fun main() {
         cars.forEach { println("${it.name} : ${"-".repeat(it.position)}") }
         println()
     }
+}
 
-    val maxPosition = cars.maxOf { it.position }
-    val winners = cars.filter { it.position == maxPosition }.joinToString(", ") { it.name }
-    println("Winners : $winners")
+private fun getCarNames(): List<String> {
+    println("Names of the cars (comma-separated):")
+    val carNamesInput = Console.readLine()
+    val carNames = carNamesInput.split(",").map { it.trim() }
+
+    if (carNames.any { it.isEmpty() }) {
+        throw IllegalArgumentException("Car names cannot be empty.")
+    }
+
+    if (carNames.any { it.length > 5 }) {
+        throw IllegalArgumentException("Car names should be 5 characters or less.")
+    }
+    return carNames
 }
 
 class Car(val name: String, var position: Int = 0) {
