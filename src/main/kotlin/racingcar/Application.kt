99d4ccf4f04b097dac98f6d06082447952403aa6
Ajibade Victoria <ajibadevictoria@Ajibades-MacBook-Pro.local>
2025-04-21 22:33:33 +0200

    feat(input): get user input

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,21 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Console
+
+
+fun getCars(): List<String> {
+    println("Please enter the names of the cars (comma separated):")
+    val input = Console.readLine()
+    return input.split(",").map { it.trim() }
+}
+
+fun getNumberOfRounds(): Int {
+    println("Please enter the number of rounds:")
+    val input = Console.readLine()
+    return input.toInt()
+}
+
 fun main() {
-    // TODO: Implement the program
+    val cars = getCars()
+    val rounds = getNumberOfRounds()
 }
