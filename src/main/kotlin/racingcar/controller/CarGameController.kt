86e392de0ce20942c8ed7e032b7257bfb11d78d9
rootPark <rmsdnjs518@gmail.com>
2025-04-22 00:09:00 +0900

    feat : Car model feature

--- src/main/kotlin/racingcar/controller/CarGameController.kt
+++ src/main/kotlin/racingcar/controller/CarGameController.kt
@@ -2,9 +2,7 @@ package racingcar
 
 import racingcar.view.InputView
 
-class CarGameController(
-  private val inputView : InputView
-) {
+class CarGameController(private val inputView : InputView) {
   fun run() {
     val carNames = inputView.readCarNames()
     val rounds = inputView.readRounds()
