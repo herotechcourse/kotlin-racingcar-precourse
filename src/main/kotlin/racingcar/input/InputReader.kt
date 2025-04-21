fda32e09e3bc300ee2bd02152f31045078ca116c
Hkunnath <honeyk.mec@gmail.com>
2025-04-21 14:43:24 +0200

    refactor(input): separating input reading and validation logics to different methods

--- src/main/kotlin/racingcar/input/InputReader.kt
+++ src/main/kotlin/racingcar/input/InputReader.kt
@@ -4,23 +4,43 @@ import camp.nextstep.edu.missionutils.Console
 
 class InputReader {
 
-    fun readAndGetCarNames(): List<String>{
+    fun readAndGetCarNames(): List<String> {
         println("Enter the names of the cars (comma-separated):")
-        val carNamesInput = Console.readLine()
-        require (carNamesInput.isNotEmpty()) { "Car names must not be empty" }
-        val carNamesList = carNamesInput.split(",").map { it.trim()}
-
-        require(carNamesList.size == carNamesList.distinct().size) { "Car names must be unique" }
-        carNamesList.forEach {
-            require(it.length <= 5) { "CarName exceeds 5 characters" }
-        }
-        return carNamesList
+        val input = readInput()
+        val carNames = parseCarNames(input)
+        validateCarNames(carNames)
+        return carNames
     }
 
-    fun readAndGetRounds(): Int{
+    fun readAndGetRounds(): Int {
         println("How many rounds will be played?")
-        val numberOfRounds = Console.readLine().toIntOrNull() ?: throw IllegalArgumentException("Rounds must not be empty")
-        require(numberOfRounds > 0) {"Rounds should be greater than 0"}
-        return numberOfRounds
+        val input = readInput()
+        val rounds = parseRounds(input)
+        validateRounds(rounds)
+        return rounds
+    }
+
+    private fun readInput(): String {
+        return Console.readLine()
+    }
+
+    private fun parseCarNames(input: String): List<String> {
+        require(input.isNotBlank()) { "Car names must not be empty" }
+        return input.split(",").map { it.trim() }
+    }
+
+    private fun validateCarNames(carNames: List<String>) {
+        require(carNames.size == carNames.distinct().size) { "Car names must be unique" }
+        carNames.forEach {
+            require(it.length <= 5) { "CarName exceeds 5 characters" }
+        }
+    }
+
+    private fun parseRounds(input: String): Int {
+        return input.toIntOrNull() ?: throw IllegalArgumentException("Rounds must not be empty")
+    }
+
+    private fun validateRounds(rounds: Int) {
+        require(rounds > 0) { "Rounds should be greater than 0" }
     }
 }
