8d462d6a8cf02e0755c0e95c26fecb9ce7302a49
minsang <minsang007@gmail.com>
2025-04-22 02:44:36 +0900

    refactor(ã main): update main to use RacingController
    Now main only creates a RacingController with ConsoleInput and ConsoleOutput and runs it.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,31 +1,12 @@
 package racingcar
 
-import racingcar.factory.CarFactory
+import racingcar.controller.RacingController
 import racingcar.input.ConsoleInput
-import racingcar.input.InputView
-import racingcar.model.Winner
-import racingcar.validation.CarNameValidator
-import racingcar.validation.InputNumberValidator
 import racingcar.view.ConsoleOutput
-import racingcar.view.OutputView
 
 fun main() {
     // TODO: Implement the program
-    val inputView: InputView = ConsoleInput
-    val outputView: OutputView = ConsoleOutput
-
-    val carNames = inputView.readCarNames()
-    CarNameValidator.validateCarNames(carNames)
-    val cars = CarFactory.createCars(carNames)
-
-    val roundInput = inputView.readRound()
-    val n = InputNumberValidator.validateInputNumber(roundInput)
-
-    outputView.printResultPerRound(n, cars)
-
-    val winner = Winner(cars)
-    winner.findWinners()
-
-    outputView.displayWinners(winner.winners)
+    val racingController = RacingController(ConsoleInput, ConsoleOutput)
+    racingController.run()
 }
 
