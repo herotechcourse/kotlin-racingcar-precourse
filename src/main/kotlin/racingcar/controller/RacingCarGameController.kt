1d5c8ec1d2c38e965cf60c39d1a3b506fd708f88
haebyun <bhk4392@gmail.com>
2025-04-21 22:01:20 +0900

    feat: implement race result printing with round history and winners

--- src/main/kotlin/racingcar/controller/RacingCarGameController.kt
+++ src/main/kotlin/racingcar/controller/RacingCarGameController.kt
@@ -3,9 +3,11 @@ package racingcar.controller
 import racingcar.model.RacingCar
 import racingcar.model.RacingCars
 import racingcar.view.InputView
+import racingcar.view.OutputView
 
 class RacingCarGameController(
-    private val inputView: InputView
+    private val inputView: InputView,
+    private val outputView: OutputView
 ) {
     fun run() {
         val carNames = inputView.readCarNames()
@@ -14,6 +16,8 @@ class RacingCarGameController(
 
         val histories = buildHistories(racingCars, rounds)
         val winners = racingCars.findWinners()
+
+        outputView.printRaceResult(histories, winners)
     }
 
     private fun buildHistories(racingCars: RacingCars, rounds: Int): List<List<RacingCar>> {
