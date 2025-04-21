9a70d7d07fc1c7a39ee9a21aaa3c6351d28d8a5e
차정혁 <ckwjdgur1234@gmail.com>
2025-04-22 00:41:13 +0900

    test(car): test position change based on number

--- src/test/kotlin/racingcar/domain/CarTest.kt
+++ src/test/kotlin/racingcar/domain/CarTest.kt
@@ -1,5 +1,6 @@
 package racingcar.domain
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
 
@@ -16,5 +17,28 @@ class CarTest {
         assertThrows(IllegalArgumentException::class.java) {
             Car("abcdef")
         }
+    }
+
+    @Test
+    fun `car moves when number is greater than or equal to threahold`() {
+        val car = Car("abc")
+
+        car.moveIfOverThreshold(MOVING_FORWARD)
+
+        assertThat(car.position).isEqualTo(1)
+    }
+
+    @Test
+    fun `car do not move when number is less than threahold`() {
+        val car = Car("abc")
+
+        car.moveIfOverThreshold(STOP)
+
+        assertThat(car.position).isEqualTo(0)
+    }
+
+    companion object {
+        const val MOVING_FORWARD = 5
+        const val STOP = 3
     }
 }
