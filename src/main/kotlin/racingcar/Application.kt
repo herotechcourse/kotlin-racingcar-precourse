97665c4ab9b7e9b89c2ae1f31b48dd59831567ed
hongseokpyo <hongseokpyou@gmail.com>
2025-04-21 21:15:09 +0900

    refactor: clean the code

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,25 +3,6 @@ package racingcar
 import racingcar.view.InputView
 import racingcar.view.readRoundCount
 
-//object Application {
-//    @JvmStatic
-//    fun main() {
-//        val car = InputView.readCarName()
-//        val rounds = InputView.readRoundCount()
-//
-//        val game = RacingGame(car)
-//        game.play(rounds)
-//    }
-//}
-
-//fun main() {
-//    val car = InputView.readCarName()
-//    val rounds = InputView.readRoundCount()
-//
-//    val game = RacingGame(car)
-//    game.play(rounds)
-//}
-
 class Application {
     companion object {
         @JvmStatic
