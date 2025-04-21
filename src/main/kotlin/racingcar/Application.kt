9e2b72d3ef2eaf60f8f91ac2350c1c85b38f212f
Alina Gabriela Alina <alinaghetler4@gmail.com>
2025-04-21 16:22:46 +0200

    refactor: extract race execution in a separate method

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,8 +1,8 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms
 import racingcar.services.ConsoleReaderService
 import racingcar.services.ReaderService
+import racingcar.services.executeRace
 
 fun main() {
     val readerService = ReaderService(ConsoleReaderService())
@@ -10,17 +10,7 @@ fun main() {
 
     val rounds = readerService.readRounds()
 
-    val results = names.associateWith { 0 }.toMutableMap()
-
-    repeat(rounds) {
-        for (name in names) {
-            val randomNumber = Randoms.pickNumberInRange(0, 9)
-            results[name] = results[name]!! + 1
-            if (randomNumber >= 4) {
-                results[name] = results[name]!! + 1
-            }
-        }
-    }
+    val results = executeRace(names, rounds)
 
     for ((name, score) in results) {
         println("$name : ${"-".repeat(score)}")
---
+++ src/main/kotlin/racingcar/model/Race.kt
@@ -0,0 +1,4 @@
+package racingcar.model
+
+class Race(val results: MutableMap<String, Int>, val rounds: Int) {
+}
