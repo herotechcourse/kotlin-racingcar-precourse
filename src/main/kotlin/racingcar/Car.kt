737ce7bd27d42b4c4c949ebc9672d9aeb450e6e1
KuMMii <4.prgmng@gmail.com>
2025-04-21 19:27:39 +0900

    feat(car): change Random library

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,24 +1,27 @@
 package racingcar
 
+import camp.nextstep.edu.missionutils.Randoms
 import kotlin.random.Random
 
 class Car() {
     var name: String = ""
+        private set
     var result: String =""
+        private set
 
 
     // Set the name
     fun setName(name: String) {
-        if (name.isNotBlank() && name.length < 5) {
+        if (name.isNotBlank() && name.length <= 5) {
             this.name =name
         } else {
-            throw IllegalArgumentException("Name must be between 1 and 5")
+            throw IllegalArgumentException("Name must be between 1 and 5. Current name is ${name}")
         }
     }
 
     // Generate a random number
     fun generateNumber(): Int {
-        return Random.nextInt(0,10)
+        return Randoms.pickNumberInRange(0, 9)
     }
 
     // Move(update the result)
