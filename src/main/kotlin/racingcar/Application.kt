9c0249f7809c810c0851fd0b6d09a4166d6043fc
won <won@wonui-MacBookAir.local>
2025-04-22 00:13:27 +0900

    feat(input): take number of race rounds from user

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -7,7 +7,6 @@ fun main() {
 
     //Ask user to input car name/s
     println("Enter the names of the cars (comma-separated):")
-
     // Read and split input into a list
     val candidates = Console.readLine()!!.split(",")
 
@@ -15,4 +14,10 @@ fun main() {
     if (candidates.isEmpty() || candidates.any { it.isEmpty() || it.length > 5 }) {
         throw IllegalArgumentException("Please provide a valid candidate name")
     }
+
+    //Ask user for number of round/s
+    println("How many rounds will be played?")
+    // Read and convert input to integer
+    val rounds = Console.readLine()!!.toInt()
+
 }
