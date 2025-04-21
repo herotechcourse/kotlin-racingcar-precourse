8ece3b163a530837660a9526b6427a197c350fd7
차정혁 <ckwjdgur1234@gmail.com>
2025-04-22 00:28:49 +0900

    feat(controller): implement race logic
    - Each round moves cars with random number

--- src/main/kotlin/racingcar/controller/GameController.kt
+++ src/main/kotlin/racingcar/controller/GameController.kt
@@ -8,5 +8,13 @@ class GameController {
     fun start() {
         val cars = Cars.from(InputView.readCarNames())
         val round = Round.from(InputView.readRound())
+
+        race(cars, round)
+    }
+
+    fun race(cars: Cars, round: Round) {
+        repeat(round.round) {
+            cars.moveAll()
+        }
     }
 }
--- src/main/kotlin/racingcar/domain/Cars.kt
+++ src/main/kotlin/racingcar/domain/Cars.kt
@@ -1,7 +1,18 @@
 package racingcar.domain
 
+import camp.nextstep.edu.missionutils.Randoms
+
 class Cars(val cars: List<Car>) {
+    fun moveAll() {
+        cars.forEach { car ->
+            car.moveIfOverThreshold(Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER))
+        }
+    }
+
     companion object {
+        private const val MIN_NUMBER = 0
+        private const val MAX_NUMBER = 9
+
         fun from(names: List<String>): Cars {
             val carList = names.map { Car(it) }
             return Cars(carList)
