6659a0159b4bbba436c2678452b4e10789fad99f
cat <myakoven@student.42berlin.de>
2025-04-21 03:20:55 +0200

    feat(error): restore error printing
    User friendly input error handling
    	- Prints the reason why the input caused an exception

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -3,11 +3,16 @@ package racingcar
 val maxRounds: Int = Int.MAX_VALUE
 
 fun main() {
-    val names: List<String> = getInputNames()
-    val cars: List<Car> = initializeCars(names)
-    val game = Game(cars)
-    val numRounds: Int = getInputRounds()
-    game.play(numRounds)
+    try {
+        val names: List<String> = getInputNames()
+        val cars: List<Car> = initializeCars(names)
+        val game = Game(cars)
+        val numRounds: Int = getInputRounds()
+        game.play(numRounds)
+    } catch (error: IllegalArgumentException) {
+        println("Error: ${error.message}")
+        throw error
+    }
 }
 
 fun initializeCars(names: List<String>): List<Car> {
