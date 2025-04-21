ba2be6f547400287998bd7909bae1e42ac19132b
CheonWoo <cjsgusdn14@naver.com>
2025-04-21 20:12:33 +0900

    style: update method notation when get score from map

--- src/main/kotlin/racingcar/domain/Car.kt
+++ src/main/kotlin/racingcar/domain/Car.kt
@@ -4,13 +4,13 @@ class Car {
     var score = mutableMapOf<String, Int>();
 
     fun addScore(carName: String): Unit {
-        score.put(carName, score.getOrDefault(carName, 0) + 1);
+        score[carName] = score.getOrDefault(carName, 0) + 1;
     }
 
     fun getScore(carName: String): Int {
-        if (score.get(carName) == null) {
+        if (score[carName] == null) {
             return 0;
         }
-        return score.get(carName)!!;
+        return score[carName]!!;
     }
 }
