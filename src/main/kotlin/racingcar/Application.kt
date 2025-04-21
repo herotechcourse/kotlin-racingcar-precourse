3e3918b5b1db4516e709fc0624d7ac2903742cb8
cat <myakoven@student.42berlin.de>
2025-04-21 02:54:43 +0200

    refactor(main): improve function names
    makeCars -> initializeCars
    
    getUserInput -> getInput

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,19 +1,15 @@
 package racingcar
 
-import camp.nextstep.edu.missionutils.Randoms
-
 val maxRounds: Int = Int.MAX_VALUE
 
 fun main() {
-    val names: List<String> = getUserInputNames()
-    val cars: List<Car> = makeCars(names)
-
+    val names: List<String> = getInputNames()
+    val cars: List<Car> = initializeCars(names)
     val game = Game(cars)
-
-    val numRounds: Int = getUserInputRounds()
+    val numRounds: Int = getInputRounds()
     game.play(numRounds)
 }
 
-fun makeCars(names: List<String>): List<Car> {
+fun initializeCars(names: List<String>): List<Car> {
     return names.map { name -> Car(name) }
 }
