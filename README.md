a3eabb74ffd560391a95c57b4fac528cf621304c
Marc Rodenbusch <87020460+MacMosby@users.noreply.github.com>
2025-04-21 16:52:00 +0200

    feat(Race): Create Race class with methods
    Implement Race class with 4 main methods and 1 utility method:
    - start(): initializes the race and runs given number of rounds
    - simulateRound(): initializes simulateRound() for each Car instance
    - printStatus(): initializes printStatus() for each Car instance
    - announceWinners(): determines the winners by calling determineWinners(), and outputs their names
    - determineWinners(): finds the max distance of a car and returns a list of all cars that have the max distance
    
    Update README.md:
    - tick off implemented features from above

--- README.md
+++ README.md
@@ -7,7 +7,7 @@
     - [x] Separate names at comma
     - [x] Validate length of car names (1 to 5 characters)
     - [x] Throw IllegalArgumentException and terminate program in case of invalid input
-- [x] Read number or rounds from user input
+- [x] Read number of rounds from user input
     - [ ] Validate data type to be a positive integer
     - [ ] Throw IllegalArgumentException and terminate program in case of invalid input
 
@@ -21,7 +21,7 @@
 
 
 ### 🏁 Race Simulation
-- [ ] Start race and simulate given number of rounds
-- [ ] Simulate round for each car
-- [ ] Output progress for each car
-- [ ] Display winner(s) (comma-separated) after given number of rounds
+- [x] Start race and simulate given number of rounds
+- [x] Simulate round for each car
+- [x] Output progress for each car
+- [x] Display winner(s) (comma-separated) after given number of rounds
---
+++ src/main/kotlin/racingcar/models/Race.kt
@@ -0,0 +1,53 @@
+package racingcar.models
+
+class Race(var cars: List<Car>, val rounds: Int) {
+
+    fun start() {
+        println()
+        println("Race Results")
+        repeat(rounds)
+        {
+            simulateRound()
+            printStatus()
+        }
+        announceWinners()
+    }
+
+    fun simulateRound()
+    {
+        for (car in cars)
+        {
+            car.simulateRound()
+        }
+    }
+
+    fun printStatus() {
+        for (car in cars)
+        {
+            car.printStatus()
+        }
+        println()
+    }
+
+    fun determineWinners(): MutableList<Car> {
+        val maxDistance = cars.maxOf {it.distance}
+        val winners: MutableList<Car> = mutableListOf()
+        for (car in cars) {
+            if (car.distance == maxDistance) {
+                winners.add(car)
+            }
+        }
+        return winners
+    }
+
+    fun announceWinners() {
+        val winners: MutableList<Car> = determineWinners()
+        var winnerMessage = "Winners : "
+        for (winner in winners) {
+            if (winnerMessage.compareTo("Winners : ") != 0)
+                winnerMessage += ", "
+            winnerMessage += winner.name
+        }
+        println(winnerMessage)
+    }
+}
