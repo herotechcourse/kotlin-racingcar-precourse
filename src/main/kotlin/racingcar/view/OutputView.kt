4a1c3b7f695c30b9bcabdbe76432925a165604b2
kkonii <119216472+kkonii@users.noreply.github.com>
2025-04-21 15:51:10 +0900

    feat: print final winners

--- src/main/kotlin/racingcar/view/OutputView.kt
+++ src/main/kotlin/racingcar/view/OutputView.kt
@@ -1,6 +1,7 @@
 package racingcar.view
 
 import racingcar.controller.response.RaceResultDto
+import racingcar.controller.response.WinnersDto
 import racingcar.domain.dto.RoundResultDto
 
 class OutputView {
@@ -17,7 +18,14 @@ class OutputView {
         println()
     }
 
+    fun printRaceWinners(raceWinners: WinnersDto) {
+        val winnerNames = raceWinners.winners.joinToString(JOIN_MARK) { it.name }
+        println("$WINNER_ARE$winnerNames")
+    }
+
     companion object {
         private const val DISTANCE_MARK = "-"
+        private const val WINNER_ARE = "Winners : "
+        private const val JOIN_MARK = ", "
     }
 }
