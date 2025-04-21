aa5805ccc3023a255448555fc52b9b370d0db8b4
minsang <minsang007@gmail.com>
2025-04-21 14:22:36 +0900

    feat(main): get car names and make car list
    Now the program asks the user to type car names with commas.
    Then it makes a Car list and shows the names.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,17 @@
 package racingcar
 
+import racingcar.model.Car
+
 fun main() {
     // TODO: Implement the program
+    print("Car names : ")
+    val input = readln()
+    val carNames = input.split(",")
+
+    val cars = mutableListOf<Car>()
+    for (name in carNames) {
+        cars.add(Car(name.trim()))
+    }
+
+    println("등록된 자동차: ${cars.map { it.name }}")
 }
