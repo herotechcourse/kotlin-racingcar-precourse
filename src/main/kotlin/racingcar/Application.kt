94fa27fcaf38ec06f926c35c71d085276d932764
USER <rhrudtn0314@gmail.com>
2025-04-21 21:36:04 +0900

    feat(inputCarNames): implement function 'inputCarNames'
    Implement function 'inputCarNames'
    - take names and split them by a specific key(,)
    - if the user inputs is invalid, terminate

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,10 +1,31 @@
 package racingcar
 
+class Car(val name: String){
+
+}
+
 class CarRacingGame {
     fun start() {   // 시작부
-
+        val cars = inputCarNames()   // 이름들 입력부. 이름들 리스트 반환받음
+//        val rounds = inputRounds()   //
 
     }
+
+    private fun inputCarNames(): List<Car> {     // 차 이름 입력부
+        print("Enter the names of the cars (comma-separated):\n")
+        val input = readln()
+        val names = input.split(',')
+        println(names)
+
+        if (names.any { it.length > 5 }) {
+            throw IllegalArgumentException("Car names must be up to 5 characters.")
+        }
+
+        return names.map { Car(it) }
+    }
+
+
+
 }
 
 
