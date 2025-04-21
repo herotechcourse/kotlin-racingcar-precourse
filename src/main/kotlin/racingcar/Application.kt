cd64b2b819d282e6ce7c5a0ee541abd78f60e1c4
FatboyBoji <jdk2016@bk.ru>
2025-04-21 04:37:12 +0200

    fix(app): simple main

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,16 +2,7 @@ package racingcar
 
 import racingcar.controller.GameController
 
-class Application {
-    companion object {
-        @JvmStatic
-        fun main(args: Array<String>) {
-            val gameController = GameController()
-            gameController.start()
-        }
-    }
-}
-
 fun main() {
-    Application.main(arrayOf())
+    val gameController = GameController()
+    gameController.start()
 }
