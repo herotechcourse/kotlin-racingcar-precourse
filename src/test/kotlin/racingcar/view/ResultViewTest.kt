3e3fdb379e0cb38bfffa4872814da7270c32634b
pyyeon <pyyeon98@gmail.com>
2025-04-21 17:31:05 +0900

    test: add unit test for ResultView output formatting

--- src/test/kotlin/racingcar/view/ResultViewTest.kt
+++ src/test/kotlin/racingcar/view/ResultViewTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 class ResultViewTest {
 
     @Test
-    fun `자동차 결과 포맷을 문자열로 반환한다`() {
+    fun car_result_is_returned_as_formatted_string() {
         val car = CarView(name = "pobi", position = 3)
         val result = ResultView.formatCarResult(car)
 
