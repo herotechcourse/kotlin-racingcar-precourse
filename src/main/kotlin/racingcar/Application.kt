fc6d0317569249f64b6d7fcab26768617477ce04
minsang <minsang007@gmail.com>
2025-04-22 02:36:31 +0900

    refactor(main): use inputView and outputView in main instead of ConsoleInput/Output
    ãUpdate main to rely on input/output interfaces rather than concrete implementations.

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -2,26 +2,30 @@ package racingcar
 
 import racingcar.factory.CarFactory
 import racingcar.input.ConsoleInput
+import racingcar.input.InputView
 import racingcar.model.Winner
 import racingcar.validation.CarNameValidator
 import racingcar.validation.InputNumberValidator
 import racingcar.view.ConsoleOutput
+import racingcar.view.OutputView
 
 fun main() {
     // TODO: Implement the program
+    val inputView: InputView = ConsoleInput
+    val outputView: OutputView = ConsoleOutput
 
-    val carNames = ConsoleInput.readCarNames()
+    val carNames = inputView.readCarNames()
     CarNameValidator.validateCarNames(carNames)
     val cars = CarFactory.createCars(carNames)
 
-    val roundInput = ConsoleInput.readRound()
+    val roundInput = inputView.readRound()
     val n = InputNumberValidator.validateInputNumber(roundInput)
 
-    ConsoleOutput.printResultPerRound(n, cars)
+    outputView.printResultPerRound(n, cars)
 
     val winner = Winner(cars)
     winner.findWinners()
 
-    ConsoleOutput.displayWinners(winner.winners)
+    outputView.displayWinners(winner.winners)
 }
 
