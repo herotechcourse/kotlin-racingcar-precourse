20b4bf841b4880b2aa658715671a47878f4909fa
minsang <minsang007@gmail.com>
2025-04-22 00:22:51 +0900

    refactor(model): move winner determinaion logic to Winner class
    Move the winner determination logic from main func into a seperate Winner class.

---
+++ src/main/kotlin/racingcar/model/Winner.kt
@@ -0,0 +1,25 @@
+package racingcar.model
+
+class Winner(val cars: List<Car>) {
+    var winners: MutableList<Car> = mutableListOf()
+
+    fun findWinners() {
+        for (car in cars) {
+            if (winners.isEmpty()) {
+                winners.add(car)
+                continue
+            }
+            if (car.position > winners.last().position) {
+                winners.clear()
+                winners.add(car)
+            } else if (car.position == winners.last().position) {
+                winners.add(car)
+            } else if (car.position < winners.last().position && winners.size > 1) {
+                winners.removeLast()
+            } else {
+                continue
+            }
+        }
+    }
+
+}
