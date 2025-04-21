74b1bd104b5108ecae2ecd24eccd43ec23a159a3
seunzu <0123suh@gmail.com>
2025-04-21 22:35:10 +0900

    feat(controller): implement game flow controller

---
+++ src/main/kotlin/racingcar/controller/RaceController.kt
@@ -0,0 +1,27 @@
+package racingcar.controller
+
+import racingcar.constants.MessageConstants
+import racingcar.model.Car
+import racingcar.model.RacingCars
+import racingcar.view.InputView
+import racingcar.view.OutputView
+
+class RaceController {
+
+    fun run() {
+        val carNames = InputView.readCarNames();
+        val roundCount = InputView.readRoundCount();
+
+        val cars = carNames.map { Car(it) }
+        val racingCars = RacingCars(cars)
+
+        println(MessageConstants.MESSAGE_RACE_RESULT)
+        repeat(roundCount) {
+            racingCars.raceOneRound { RandomUtil.shouldMove() }
+            OutputView.printRaceStatus(racingCars.getAllCars())
+        }
+
+        val winners = racingCars.findWinners().map { it.name }
+        OutputView.printWinners(winners)
+    }
+}
