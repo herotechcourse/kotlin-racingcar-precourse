5194c9e3a2c062dfb8814623671b90f38522378a
jslusark <slusark.jessica@gmail.com>
2025-04-21 13:04:20 +0200

    refactor(simulation): split Simulation in objects with own specific responsibilities to improve modularity
    - Extracted all output-related functions into a SimulationOutput so that Simulation object could focus on pure code logic
    - Added  notes in SimulationOutput explaining certains decisions and thoughts relevant to the project structure also while considering encountered restrictions

---
+++ src/main/kotlin/racingcar/simulation/Simulation.kt
@@ -0,0 +1,33 @@
+package racingcar.simulation
+
+import racingcar.Car
+
+object Simulation {
+
+  fun race(cars: List<Car>, totalRounds: Int) {
+    SimulationOutput.header()
+    start(cars, totalRounds)
+    end(cars)
+  }
+
+  private fun start(cars: List<Car>, totalRounds: Int) {
+    for (round in 1..totalRounds) {
+      runSingleRound(cars)
+    }
+  }
+
+  private fun runSingleRound(cars: List<Car>) {
+    SimulationOutput.addPadding()
+    for (car in cars) {
+      car.attemptMovement()
+      SimulationOutput.round("${car.name} : ${car.progressBar()}")
+    }
+    SimulationOutput.addPadding()
+  }
+
+  private fun end(cars: List<Car>) {
+    val longestRun = cars.maxOf { it.position }
+    val result = if (longestRun == 0) emptyList() else cars.filter { it.position == longestRun }
+    SimulationOutput.result(result)
+  }
+}
