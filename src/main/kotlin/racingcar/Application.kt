abbe7e6d29e1925e523c0201be44ab74bbec6248
beom_k <toppi6741@gmail.com>
2025-05-01 16:33:45 +0900

    feat: implement main function for running the racing game
    - Implemented main function to initialize and run the racing game
    - Reads player names and game length from user input
    - Starts the game and prints the winner after the game ends
    - Implemented input validation check for names and game length

--- src/main/kotlin/racingcar/Application.kt
+++ src/main/kotlin/racingcar/Application.kt
@@ -1,5 +1,38 @@
 package racingcar
+import camp.nextstep.edu.missionutils.Console
 
 fun main() {
-    // TODO: Implement the program
+    val names = getNamesInput()
+    val gameLength = getGameLengthInput()
+
+    val game = RaceGame(names, gameLength)
+    game.startGame()
+    game.printWinner()
+}
+
+fun getNamesInput() : List<String> {
+    println("경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)")
+    val names = Console.readLine()
+
+    val namesArray = names.split(",")
+    namesArray.ifEmpty { throw IllegalArgumentException("잘못된 입력입니다.") }
+
+    for (name in namesArray) {
+        if(name.length > 5) {
+            throw IllegalArgumentException("잘못된 입력입니다.")
+        }
+    }
+
+    return namesArray
+}
+
+fun getGameLengthInput() : Int {
+    println("시도할 회수는 몇 회인가요?")
+    val gameLength= Console.readLine()
+
+    return try {
+        gameLength.toInt()
+    } catch (e: Exception) {
+        throw IllegalArgumentException("잘못된 입력입니다.")
+    }
 }
