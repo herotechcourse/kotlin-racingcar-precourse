10465ade43f69762e1f00e63c27ca2d3972d862a
beom_k <toppi6741@gmail.com>
2025-05-01 15:58:29 +0900

    feat: implement RacingCar class with movement and state display
    - Added RacingCar class with basic movement logic and state display output
    - Includes name, distanceTraveled, thresholdToMove, distancePerStep as perperties
    - Implemented tryMove(chance) to move the car based on a random threshold check
    - Provides printState() for visualizing the car's current position with dashes ("-")

---
+++ src/main/kotlin/racingcar/RacingCar.kt
@@ -0,0 +1,25 @@
+package racingcar
+
+class RacingCar(val name: String) {
+    private var distanceTraveled: Int = 0
+    private var thresholdToMove: Int = 4
+    private var distancePerStep: Int = 1
+
+    fun tryMove(chance : Int) {
+        if (isMoveable(chance)) {
+            distanceTraveled += distancePerStep
+        }
+    }
+
+    private fun isMoveable(chance : Int) : Boolean {
+        return chance >= thresholdToMove
+    }
+
+    fun printState() {
+        println("$name : ${getStateBar()}")
+    }
+
+    private fun getStateBar() : String {
+        return "-".repeat(distanceTraveled)
+    }
+}
